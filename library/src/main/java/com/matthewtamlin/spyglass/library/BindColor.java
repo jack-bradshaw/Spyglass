af11f0b78f9cf86f7c11a42aa51e7797bfaa5d65
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-23 17:30:47 +1030

    Added @Target annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/BindColor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/BindColor.java
@@ -1,7 +1,11 @@
 package com.matthewtamlin.spyglass.library;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 import static com.matthewtamlin.spyglass.library.IgnoreMode.NEVER_IGNORE;
 
+@Target({ElementType.METHOD, ElementType.FIELD})
 public @interface BindColor {
 	int annotationId();
 
