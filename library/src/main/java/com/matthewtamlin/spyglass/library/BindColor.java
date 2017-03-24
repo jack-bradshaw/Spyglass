5d2f62278d842b3402c0e33b9a3e9251eac32abb
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-24 11:42:50 +1030

    Replaced ignore mode with boolean method

--- library/src/main/java/com/matthewtamlin/spyglass/library/BindColor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/BindColor.java
@@ -3,13 +3,12 @@ package com.matthewtamlin.spyglass.library;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-import static com.matthewtamlin.spyglass.library.IgnoreMode.NEVER_IGNORE;
 
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface BindColor {
 	int annotationId();
 
-	IgnoreMode ignoreMode() default NEVER_IGNORE;
-
+	boolean ignoreIfAttributeMissing() default false;
+	
 	int defaultValue() default 0;
 }
