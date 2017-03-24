b6d4e444046b8655d70d74edc2dd164f3017867b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-24 17:05:50 +1030

    Moved DimensionUnit to package core
    - Moved file
    - Updated references in other files

--- library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/BindDimension.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/BindDimension.java
@@ -1,11 +1,11 @@
 package com.matthewtamlin.spyglass.library.binder_annotations;
 
-import com.matthewtamlin.spyglass.library.DimensionUnit;
+import com.matthewtamlin.spyglass.library.core.DimensionUnit;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-import static com.matthewtamlin.spyglass.library.DimensionUnit.PT;
+import static com.matthewtamlin.spyglass.library.core.DimensionUnit.PT;
 
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface BindDimension {
