64c6b4e14f9122463d3f6932499459d656f136f3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-25 00:45:53 +1030

    Added retention policy

--- library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/BindColor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/BindColor.java
@@ -1,8 +1,10 @@
 package com.matthewtamlin.spyglass.library.binder_annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface BindColor {
 	int annotationId();
