78773d2a81f8d6ca3d6a0cf30427a87c550183f6
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-24 20:12:46 +1030

    Deleted ignoreIfAttributeMissing entity

--- library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/BindInteger.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/BindInteger.java
@@ -6,6 +6,4 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface BindInteger {
 	int annotationId();
-
-	boolean ignoreIfAttributeMissing() default false;
 }
