203fcd7bd64e761d1ccab9899fc1724b3fa35058
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-24 11:56:15 +1030

    Added target annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/BindDimension.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/BindDimension.java
@@ -1,7 +1,11 @@
 package com.matthewtamlin.spyglass.library;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 import static com.matthewtamlin.spyglass.library.DimensionUnit.PT;
 
+@Target({ElementType.METHOD, ElementType.FIELD})
 public @interface BindDimension {
 	int annotationId();
 
