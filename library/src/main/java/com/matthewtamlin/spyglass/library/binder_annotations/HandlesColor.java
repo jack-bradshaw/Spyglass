7beede01867daee4e9a42a7155d3b6cb5c9438fe
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-25 01:05:24 +1030

    Renamed class

--- library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/HandlesColor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/HandlesColor.java
@@ -6,6 +6,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
-public @interface BindColor {
+public @interface HandlesColor {
 	int annotationId();
 }
