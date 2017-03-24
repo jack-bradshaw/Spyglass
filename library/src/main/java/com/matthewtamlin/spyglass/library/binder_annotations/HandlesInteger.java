24f10d37cb0fabf6cdf21d9e451275a9634ffad5
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-25 01:05:45 +1030

    Renamed class

--- library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/HandlesInteger.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/HandlesInteger.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
-public @interface BindInteger {
+public @interface HandlesInteger {
 	int annotationId();
 }
