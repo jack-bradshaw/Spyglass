cbf8f2dc083d4296c86617562a5d8fa31a2282fd
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-24 11:55:59 +1030

    Added target annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/BindBoolean.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/BindBoolean.java
@@ -1,5 +1,9 @@
 package com.matthewtamlin.spyglass.library;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
 public @interface BindBoolean {
 	int annotationId();
 
