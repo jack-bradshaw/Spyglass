9e818d18e799dcf9bdcafdea871f7314dbf566ef
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-25 00:46:34 +1030

    Added retention policy

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToEnumConstant.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToEnumConstant.java
@@ -1,5 +1,9 @@
 package com.matthewtamlin.spyglass.library.default_annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DefaultToEnumConstant {
 	Class<? extends Enum> enumClass();
 
