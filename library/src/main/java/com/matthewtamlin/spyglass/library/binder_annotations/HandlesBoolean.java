83db2d806843f3aac83f118f78d7097d3fde2a97
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-25 01:05:22 +1030

    Renamed class

--- library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/HandlesBoolean.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/HandlesBoolean.java
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
-public @interface BindBoolean {
+public @interface HandlesBoolean {
 	int annotationId();
 }
