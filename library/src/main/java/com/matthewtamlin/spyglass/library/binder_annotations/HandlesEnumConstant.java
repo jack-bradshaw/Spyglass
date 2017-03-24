0333c99276fd4d9172511eb21f215c3e57171d9c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-25 01:05:35 +1030

    Renamed class

--- library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/HandlesEnumConstant.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/HandlesEnumConstant.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface BindEnumConstant {
+public @interface HandlesEnumConstant {
 	int annotationId();
 
 	Class<? extends Enum> enumClass();
