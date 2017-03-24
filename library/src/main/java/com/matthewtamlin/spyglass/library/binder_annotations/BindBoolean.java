318174b0cf679fbffefd674f45eec55fe6eb73be
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-24 18:16:48 +1030

    Rearranged entities

--- library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/BindBoolean.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/binder_annotations/BindBoolean.java
@@ -3,13 +3,14 @@ package com.matthewtamlin.spyglass.library.binder_annotations;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface BindBoolean {
 	int annotationId();
 
-	boolean ignoreIfAttributeMissing() default false;
-
 	boolean defaultValue() default false;
+
+	boolean ignoreIfAttributeMissing() default false;
 
 	int defaultResourceId() default 0;
 }
