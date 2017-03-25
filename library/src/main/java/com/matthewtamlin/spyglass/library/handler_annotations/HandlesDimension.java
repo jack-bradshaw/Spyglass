4ea801b719cadd8f34e6903fc9f6bab9267b9df0
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-25 17:39:47 +1030

    Added missing import

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesDimension.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesDimension.java
@@ -1,6 +1,7 @@
 package com.matthewtamlin.spyglass.library.handler_annotations;
 
 import com.matthewtamlin.spyglass.library.core.DimensionUnit;
+import com.matthewtamlin.spyglass.library.meta_annotations.Handles;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
