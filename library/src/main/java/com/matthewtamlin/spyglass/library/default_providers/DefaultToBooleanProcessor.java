bbddb7fc424a8c12428cbd465fdb086d3d785adc
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 15:12:40 +1030

    Reformatting

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultToBooleanProcessor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultToBooleanProcessor.java
@@ -4,8 +4,7 @@ import android.content.Context;
 
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToBoolean;
 
-public class DefaultToBooleanProcessor
-		implements DefaultProcessor<Boolean, DefaultToBoolean> {
+public class DefaultToBooleanProcessor implements DefaultProcessor<Boolean, DefaultToBoolean> {
 	@Override
 	public Boolean process(final DefaultToBoolean annotation, final Context context) {
 		return annotation.value();
