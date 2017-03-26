b787979eba465588984d605ecee292c9ce6ce899
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 15:17:14 +1030

    Created DefaultToColorResourceProcessor.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultToColorResourceProcessor.java
@@ -0,0 +1,14 @@
+package com.matthewtamlin.spyglass.library.default_providers;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+
+import com.matthewtamlin.spyglass.library.default_annotations.DefaultToColorResource;
+
+public class DefaultToColorResourceProcessor
+		implements DefaultProcessor<Integer, DefaultToColorResource> {
+	@Override
+	public Integer process(final DefaultToColorResource annotation, final Context context) {
+		return ContextCompat.getColor(context, annotation.value());
+	}
+}
