6d345cc2c0c165e92560b0d2d5e5da820158cc7d
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 10:37:50 +1030

    Changed annotationId field to attributeId

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesFraction.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesFraction.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface HandlesFraction {
-	int annotationId();
+	int attributeId();
 
 	int baseMultiplier() default 1;
 
