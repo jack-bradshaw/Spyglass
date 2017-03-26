f9a77965c9f044d7432325949f3577b7690c9d7f
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 15:09:47 +1030

    Renamed DefaultSupplier to DefaultAnnotationProcessor

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultAnnotationProcessor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultAnnotationProcessor.java
@@ -4,6 +4,6 @@ import android.content.Context;
 
 import java.lang.annotation.Annotation;
 
-public interface DefaultSupplier<T, A extends Annotation> {
+public interface DefaultAnnotationProcessor<T, A extends Annotation> {
 	public T get(Context context, A annotation);
 }
