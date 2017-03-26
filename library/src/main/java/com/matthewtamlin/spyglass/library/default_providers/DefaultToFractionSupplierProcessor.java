b4b0c22b64c98f8ad0a298ffe8da1b5edf9ed3a3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 16:20:56 +1030

    Added missing >

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultToFractionSupplierProcessor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultToFractionSupplierProcessor.java
@@ -7,7 +7,7 @@ import com.matthewtamlin.spyglass.library.default_annotations.DefaultToFractionS
 import static com.matthewtamlin.spyglass.library.core.SupplierInstantiator.instantiateSupplier;
 
 public class DefaultToFractionSupplierProcessor
-		implements DefaultProcessor<Float, DefaultToFractionSupplier {
+		implements DefaultProcessor<Float, DefaultToFractionSupplier> {
 	@Override
 	public Float process(final DefaultToFractionSupplier annotation, final Context context) {
 		return instantiateSupplier(annotation.value()).get();
