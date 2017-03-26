8d068e7a23f51e982bf93cd106e46105e9222e02
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 10:37:27 +1030

    Changed annotationId field to attributeId

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesColor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesColor.java
@@ -11,5 +11,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface HandlesColor {
-	int annotationId();
+	int attributeId();
 }
