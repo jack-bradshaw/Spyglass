4e0f27b8fa89d8542b4537b467d7287a745e955b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 10:37:43 +1030

    Changed annotationId field to attributeId

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesEnumConstant.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesEnumConstant.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface HandlesEnumConstant {
-	int annotationId();
+	int attributeId();
 
 	Class<? extends Enum> enumClass();
 
