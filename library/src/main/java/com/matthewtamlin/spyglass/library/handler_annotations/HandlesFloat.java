0a295054766cadbffecebf01d69053289a671f51
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 10:37:46 +1030

    Changed annotationId field to attributeId

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesFloat.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesFloat.java
@@ -11,5 +11,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface HandlesFloat {
-	int annotationId();
+	int attributeId();
 }
