a9b4657196a9c17c06c18e2b1ceb4600ca9153aa
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 16:34:58 +1030

    Updated @Default annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToBooleanSupplier.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToBooleanSupplier.java
@@ -1,6 +1,7 @@
 package com.matthewtamlin.spyglass.library.default_annotations;
 
 import com.matthewtamlin.spyglass.library.core.Supplier;
+import com.matthewtamlin.spyglass.library.default_processors.DefaultToBooleanSupplierProcessor;
 import com.matthewtamlin.spyglass.library.meta_annotations.Default;
 
 import java.lang.annotation.ElementType;
@@ -8,7 +9,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Default(annotationClass = DefaultToBooleanSupplier.class)
+@Default(processorClass = DefaultToBooleanSupplierProcessor.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface DefaultToBooleanSupplier {
