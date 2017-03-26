069bc240ff64a0f9768d5ba3dc885ed58e9aba92
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 16:18:32 +1030

    Changed resId type to int from float

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToFractionResource.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToFractionResource.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface DefaultToFractionResource {
-	float resId();
+	int resId();
 
 	int baseMultiplier() default 1;
 
