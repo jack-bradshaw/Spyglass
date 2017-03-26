d5e56e335261c118f986a738ebc747fce0db43c8
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 15:12:00 +1030

    Reordered method arguments

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultProcessor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultProcessor.java
@@ -5,5 +5,5 @@ import android.content.Context;
 import java.lang.annotation.Annotation;
 
 public interface DefaultProcessor<T, A extends Annotation> {
-	public T process(Context context, A annotation);
+	public T process(A annotation, Context context);
 }
