d2bc7fb1926d4bfb5291c90302b3b3b1a9dc5799
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 11:13:13 +1030

    Added mandatory element

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesBoolean.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/HandlesBoolean.java
@@ -13,4 +13,6 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface HandlesBoolean {
 	int attributeId();
+
+	boolean mandatory() default false;
 }
