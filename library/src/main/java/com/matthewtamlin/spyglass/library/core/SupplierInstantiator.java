b62069f95ecd4b3ec9bd8f05598a884a78fa9c16
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 15:26:55 +1030

    Created SupplierInstantiator.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/SupplierInstantiator.java
@@ -0,0 +1,22 @@
+package com.matthewtamlin.spyglass.library.core;
+
+public class SupplierInstantiator {
+	@SuppressWarnings("TryWithIdenticalCatches") // Can't be collapsed before API 19
+	public static <T> Supplier<T> instantiateSupplier(final Class<? extends Supplier<T>> clazz) {
+		try {
+			return clazz.newInstance();
+
+		} catch (final InstantiationException e) {
+			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
+					" class with a public no-arg constructor?";
+
+			throw new SupplierInstantiationException(String.format(message, clazz), e);
+
+		} catch (final IllegalAccessException e) {
+			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
+					" class with a public no-arg constructor?";
+
+			throw new SupplierInstantiationException(String.format(message, clazz), e);
+		}
+	}
+}
