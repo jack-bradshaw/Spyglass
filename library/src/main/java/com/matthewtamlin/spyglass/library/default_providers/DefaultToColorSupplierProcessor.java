bdfa6d14a6d723adfe82b0d002b0380a2c65cd66
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-26 15:30:58 +1030

    Refactoring

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultToColorSupplierProcessor.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_providers/DefaultToColorSupplierProcessor.java
@@ -2,16 +2,14 @@ package com.matthewtamlin.spyglass.library.default_providers;
 
 import android.content.Context;
 
-import com.matthewtamlin.spyglass.library.core.Supplier;
-import com.matthewtamlin.spyglass.library.core.SupplierInstantiator;
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToColorSupplier;
+
+import static com.matthewtamlin.spyglass.library.core.SupplierInstantiator.instantiateSupplier;
 
 public class DefaultToColorSupplierProcessor
 		implements DefaultProcessor<Integer, DefaultToColorSupplier> {
 	@Override
 	public Integer process(final DefaultToColorSupplier annotation, final Context context) {
-		Supplier<Integer> supplier = SupplierInstantiator.instantiateSupplier(annotation.value());
-
-		return supplier.get();
+		return instantiateSupplier(annotation.value()).get();
 	}
 }
