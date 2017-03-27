6d6f9c407b48809fd232cdf4a2e388a492e19780
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-27 19:22:42 +1030

    Created StringAttributeProcessor.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_processors/StringAttributeProcessor.java
@@ -0,0 +1,29 @@
+package com.matthewtamlin.spyglass.library.handler_processors;
+
+import android.content.res.TypedArray;
+
+import com.matthewtamlin.spyglass.library.handler_annotations.StringHandler;
+
+public class StringAttributeProcessor implements AttributeProcessor<String, StringHandler> {
+	@Override
+	public boolean attributeValueIsAvailable(
+			final TypedArray attrs,
+			final StringHandler annotation) {
+
+		return attrs.hasValue(annotation.attributeId());
+	}
+
+	@Override
+	public String getAttributeValue(final TypedArray attrs, final StringHandler annotation) {
+		if (attributeValueIsAvailable(attrs, annotation)) {
+			return attrs.getString(annotation.attributeId());
+		} else {
+			throw new RuntimeException("No attribute found for ID " + annotation.attributeId());
+		}
+	}
+
+	@Override
+	public boolean attributeIsMandatory(final StringHandler annotation) {
+		return false;
+	}
+}
