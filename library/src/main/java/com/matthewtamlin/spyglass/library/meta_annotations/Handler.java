25e6dbaa21139e11b832d822400b1e734163b9c0
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-27 19:38:43 +1030

    Renamed value() method to processorClass()

--- library/src/main/java/com/matthewtamlin/spyglass/library/meta_annotations/Handler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/meta_annotations/Handler.java
@@ -10,5 +10,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface Handler {
-	Class<? extends AttributeProcessor> value();
+	Class<? extends AttributeProcessor> processorClass();
 }
