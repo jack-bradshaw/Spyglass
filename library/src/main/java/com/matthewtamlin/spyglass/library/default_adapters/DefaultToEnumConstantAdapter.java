2aa0cdc80f96870a069231becbe7b003167c53d0
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-27 20:12:04 +1030

    Replaced implementation with all to EnumUtil

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToEnumConstantAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToEnumConstantAdapter.java
@@ -2,6 +2,7 @@ package com.matthewtamlin.spyglass.library.default_adapters;
 
 import android.content.Context;
 
+import com.matthewtamlin.spyglass.library.core.EnumUtil;
 import com.matthewtamlin.spyglass.library.core.MalformedEnumException;
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToEnumConstant;
 
@@ -11,20 +12,6 @@ public class DefaultToEnumConstantAdapter
 	@Override
 	public Enum<?> getDefault(final DefaultToEnumConstant annotation, final Context context) {
 		final int ordinal = annotation.ordinal();
-		final Enum[] constants = annotation.enumClass().getEnumConstants();
-
-		if (ordinal < 0 || ordinal > constants.length - 1) {
-			final String message = "Found ordinal value %1$s for enum %2$s. Ordinal must be in " +
-					"the range 0 to %3$s.";
-
-			throw new MalformedEnumException(
-					String.format(
-							message,
-							ordinal,
-							annotation.enumClass(),
-							constants.length - 1));
-		}
-
-		return constants[ordinal];
+		return EnumUtil.getEnumConstant(annotation.enumClass(), ordinal);
 	}
 }
