bcc2e36d3af30e0607ec061e31f72bd9d0746ae6
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-27 19:37:30 +1030

    Changed handler annotation argument

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/ColorHandler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/ColorHandler.java
@@ -1,5 +1,6 @@
 package com.matthewtamlin.spyglass.library.handler_annotations;
 
+import com.matthewtamlin.spyglass.library.handler_processors.ColorAttributeProcessor;
 import com.matthewtamlin.spyglass.library.meta_annotations.Handler;
 
 import java.lang.annotation.ElementType;
@@ -7,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Handler(ColorHandler.class)
+@Handler(processorClass = ColorAttributeProcessor.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface ColorHandler {
