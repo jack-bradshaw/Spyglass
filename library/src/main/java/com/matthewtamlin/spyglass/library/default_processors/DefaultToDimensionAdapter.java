aeb1bc2ea9427775afb876d316e1346659f8bea3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-27 20:02:46 +1030

    Renamed DefaultToDimensionProcessor
    - Renamed declaration
    - Updated references

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_processors/DefaultToDimensionAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_processors/DefaultToDimensionAdapter.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToDimension;
 
-public class DefaultToDimensionProcessor implements DefaultAdapter<Float, DefaultToDimension> {
+public class DefaultToDimensionAdapter implements DefaultAdapter<Float, DefaultToDimension> {
 	@Override
 	public Float process(final DefaultToDimension annotation, final Context context) {
 		return annotation.unit().convertToPx(context, annotation.value());
