110d4b39c27389b41f16c28025542955d1aa1b31
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-27 19:57:07 +1030

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_processors/HandlerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_processors/HandlerAdapter.java
@@ -4,6 +4,14 @@ import android.content.res.TypedArray;
 
 import java.lang.annotation.Annotation;
 
+/**
+ * Interfaces between a Handler annotation and a set of Android view attributes.
+ *
+ * @param <T>
+ * 		the data type the view attribute maps to
+ * @param <A>
+ * 		the type of annotation to interface with
+ */
 public interface HandlerAdapter<T, A extends Annotation> {
 	public boolean attributeValueIsAvailable(TypedArray attrs, A annotation);
 
