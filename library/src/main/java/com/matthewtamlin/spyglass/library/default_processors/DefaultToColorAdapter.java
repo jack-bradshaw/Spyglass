38ca68820cacf023c03834a0910b45985f1ca99e
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-27 20:00:57 +1030

    Renamed DefaultToColorProcessor
    - Renamed declaration
    - Updated references

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_processors/DefaultToColorAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_processors/DefaultToColorAdapter.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToColor;
 
-public class DefaultToColorProcessor implements DefaultAdapter<Integer, DefaultToColor> {
+public class DefaultToColorAdapter implements DefaultAdapter<Integer, DefaultToColor> {
 	@Override
 	public Integer process(final DefaultToColor annotation, final Context context) {
 		return annotation.value();
