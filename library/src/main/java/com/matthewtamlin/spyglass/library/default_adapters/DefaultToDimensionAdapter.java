f96e417fa8f74afb8a72e7d35379567fa6eff6f8
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 17:31:43 +1030

    Added null checks to argument

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToDimensionAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToDimensionAdapter.java
@@ -4,9 +4,14 @@ import android.content.Context;
 
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToDimension;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class DefaultToDimensionAdapter implements DefaultAdapter<Float, DefaultToDimension> {
 	@Override
 	public Float getDefault(final DefaultToDimension annotation, final Context context) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+		checkNotNull(context, "Argument \'context\' cannot be null.");
+
 		return annotation.unit().convertToPx(context, annotation.value());
 	}
 }
