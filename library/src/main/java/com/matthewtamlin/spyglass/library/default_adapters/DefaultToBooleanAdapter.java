7151f193da44d6869c998139ab992eb5081649b8
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 17:31:24 +1030

    Added null checks to argument

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToBooleanAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToBooleanAdapter.java
@@ -4,9 +4,13 @@ import android.content.Context;
 
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToBoolean;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class DefaultToBooleanAdapter implements DefaultAdapter<Boolean, DefaultToBoolean> {
 	@Override
 	public Boolean getDefault(final DefaultToBoolean annotation, final Context context) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
 		return annotation.value();
 	}
 }
