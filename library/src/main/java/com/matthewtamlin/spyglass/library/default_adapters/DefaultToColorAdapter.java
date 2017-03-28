3192ebfcc7961febf55d75bd1cbf505ac80ed1e3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 17:31:35 +1030

    Added null checks to argument

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToColorAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToColorAdapter.java
@@ -4,9 +4,13 @@ import android.content.Context;
 
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToColor;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class DefaultToColorAdapter implements DefaultAdapter<Integer, DefaultToColor> {
 	@Override
 	public Integer getDefault(final DefaultToColor annotation, final Context context) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
 		return annotation.value();
 	}
 }
