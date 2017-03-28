1ced293fffd950dcc112f0102f59811b856d4668
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 18:28:17 +1030

    Created TestDefaultAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/default_adapters/TestDefaultAdapter.java
@@ -0,0 +1,48 @@
+package com.matthewtamlin.spyglass.library_tests.default_adapters;
+
+import android.content.Context;
+
+import com.matthewtamlin.spyglass.library.default_adapters.DefaultAdapter;
+
+import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public abstract class TestDefaultAdapter<
+		D,
+		A extends Annotation,
+		P extends DefaultAdapter<D, A>> {
+
+	public abstract D getExpectedDefaultValue();
+
+	public abstract A getAnnotation();
+
+	public abstract P getAdapter();
+
+	public abstract Context getContext();
+
+	@Test
+	public void testReflectiveInstantiation() throws Exception {
+		getAdapter().getClass().newInstance();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetDefault_nullAnnotation() {
+		getAdapter().getDefault(null, getContext());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetDefault_nullContext() {
+		getAdapter().getDefault(getAnnotation(), null);
+	}
+
+	@Test
+	public void testGetDefault_validArguments() {
+		final D returnedDefault = getAdapter().getDefault(getAnnotation(), getContext());
+
+		assertThat(returnedDefault, is(getExpectedDefaultValue()));
+	}
+}
