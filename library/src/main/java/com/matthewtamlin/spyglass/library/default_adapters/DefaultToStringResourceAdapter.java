0d2b61244a206ff667f30a7eb71a0f2885270e21
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 17:32:14 +1030

    Added null checks to argument

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToStringResourceAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToStringResourceAdapter.java
@@ -4,11 +4,16 @@ import android.content.Context;
 
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToStringResource;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class DefaultToStringResourceAdapter
 		implements DefaultAdapter<String, DefaultToStringResource> {
 
 	@Override
 	public String getDefault(final DefaultToStringResource annotation, final Context context) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+		checkNotNull(context, "Argument \'context\' cannot be null.");
+
 		return context.getResources().getString(annotation.value());
 	}
 }
