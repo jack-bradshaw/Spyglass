a01ffad2eed4de83fbc74b34eac60e2ade32796d
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 17:31:59 +1030

    Added null checks to argument

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToFractionSupplierAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToFractionSupplierAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToFractionSupplier;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 import static com.matthewtamlin.spyglass.library.core.SupplierInstantiator.instantiateSupplier;
 
 public class DefaultToFractionSupplierAdapter
@@ -11,6 +12,8 @@ public class DefaultToFractionSupplierAdapter
 
 	@Override
 	public Float getDefault(final DefaultToFractionSupplier annotation, final Context context) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
 		return instantiateSupplier(annotation.value()).get();
 	}
 }
