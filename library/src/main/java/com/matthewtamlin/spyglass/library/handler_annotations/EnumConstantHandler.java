b8de1be60a639cf6a7d3d2172f90ebb39593ac9a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 22:19:14 +1030

    Fixed spelling error in comment

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/EnumConstantHandler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/EnumConstantHandler.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-//TODO this class needs significant wori
+//TODO this class needs significant work
 //@Handler(EnumConstantHandler.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
