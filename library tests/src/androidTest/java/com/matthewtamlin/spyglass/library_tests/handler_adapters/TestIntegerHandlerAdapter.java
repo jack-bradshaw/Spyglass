6ce67f4635babbd20e1fa8281820b180e68ee294
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 22:09:06 +1030

    Created TestIntegerHandlerAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestIntegerHandlerAdapter.java
@@ -0,0 +1,103 @@
+package com.matthewtamlin.spyglass.library_tests.handler_adapters;
+
+import android.content.res.TypedArray;
+
+import com.matthewtamlin.spyglass.library.handler_adapters.IntegerHandlerAdapter;
+import com.matthewtamlin.spyglass.library.handler_annotations.IntegerHandler;
+
+import org.junit.Before;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.Random;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SuppressWarnings("ResourceType")
+public class TestIntegerHandlerAdapter extends TestHandlerAdapter<Integer, IntegerHandler,
+		IntegerHandlerAdapter> {
+	private Integer expectedValue;
+
+	private TypedArray containingAttribute;
+
+	private TypedArray missingAttribute;
+
+	private IntegerHandler withMandatoryFlag;
+
+	private IntegerHandler missingMandatoryFlag;
+
+	private IntegerHandlerAdapter adapter;
+
+	@Before
+	public void setup() {
+		final int attributeId = new Random().nextInt(Integer.MAX_VALUE);
+
+		expectedValue = new Random().nextInt(Integer.MAX_VALUE);
+
+		containingAttribute = mock(TypedArray.class);
+		when(containingAttribute.hasValue(attributeId)).thenReturn(true);
+		when(containingAttribute.getInt(eq(attributeId), anyInt())).thenReturn(expectedValue);
+		when(containingAttribute.getInteger(eq(attributeId), anyInt())).thenReturn(expectedValue);
+
+		missingAttribute = mock(TypedArray.class);
+		when(missingAttribute.hasValue(attributeId)).thenReturn(false);
+		when(missingAttribute.getInt(eq(attributeId), anyInt())).thenAnswer(new Answer<Object>() {
+			@Override
+			public Object answer(final InvocationOnMock invocation) throws Throwable {
+				// Always return the second argument since is't the default
+				return invocation.getArgumentAt(1, Integer.class);
+			}
+		});
+		when(missingAttribute.getInteger(eq(attributeId), anyInt()))
+				.thenAnswer(new Answer<Object>() {
+					@Override
+					public Object answer(final InvocationOnMock invocation) throws Throwable {
+						// Always return the second argument since is't the default
+						return invocation.getArgumentAt(1, Integer.class);
+					}
+				});
+
+		withMandatoryFlag = mock(IntegerHandler.class);
+		when(withMandatoryFlag.attributeId()).thenReturn(attributeId);
+		when(withMandatoryFlag.mandatory()).thenReturn(true);
+
+		missingMandatoryFlag = mock(IntegerHandler.class);
+		when(missingMandatoryFlag.attributeId()).thenReturn(attributeId);
+		when(missingMandatoryFlag.mandatory()).thenReturn(false);
+
+		adapter = new IntegerHandlerAdapter();
+	}
+
+	@Override
+	public Integer getExpectedValue() {
+		return expectedValue;
+	}
+
+	@Override
+	public TypedArray getTypedArrayContainingAttribute() {
+		return containingAttribute;
+	}
+
+	@Override
+	public TypedArray getTypedArrayMissingAttribute() {
+		return missingAttribute;
+	}
+
+	@Override
+	public IntegerHandler getAnnotationWithMandatoryFlag() {
+		return withMandatoryFlag;
+	}
+
+	@Override
+	public IntegerHandler getAnnotationMissingMandatoryFlag() {
+		return missingMandatoryFlag;
+	}
+
+	@Override
+	public IntegerHandlerAdapter getAdapter() {
+		return adapter;
+	}
+}
