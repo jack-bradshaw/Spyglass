48f06f1dee963a12adf625b9b6846f080a160b2a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 21:40:28 +1030

    Created TestDimensionHandlerAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestDimensionHandlerAdapter.java
@@ -0,0 +1,98 @@
+package com.matthewtamlin.spyglass.library_tests.handler_adapters;
+
+import android.content.res.TypedArray;
+
+import com.matthewtamlin.spyglass.library.handler_adapters.BooleanHandlerAdapter;
+import com.matthewtamlin.spyglass.library.handler_adapters.DimensionHandlerAdapter;
+import com.matthewtamlin.spyglass.library.handler_annotations.BooleanHandler;
+import com.matthewtamlin.spyglass.library.handler_annotations.DimensionHandler;
+
+import org.junit.Before;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.Random;
+
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyFloat;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SuppressWarnings("ResourceType")
+public class TestDimensionHandlerAdapter extends TestHandlerAdapter<Float, DimensionHandler,
+		DimensionHandlerAdapter> {
+	private Float expectedValue;
+
+	private TypedArray containingAttribute;
+
+	private TypedArray missingAttribute;
+
+	private DimensionHandler withMandatoryFlag;
+
+	private DimensionHandler missingMandatoryFlag;
+
+	private DimensionHandlerAdapter adapter;
+
+	@Before
+	public void setup() {
+		final int attributeId = new Random().nextInt(Integer.MAX_VALUE);
+
+		expectedValue = Float.NEGATIVE_INFINITY;
+
+		containingAttribute = mock(TypedArray.class);
+		when(containingAttribute.getDimension(eq(attributeId), anyFloat()))
+				.thenReturn(expectedValue);
+
+		missingAttribute = mock(TypedArray.class);
+		when(missingAttribute.getDimension(eq(attributeId), anyFloat()))
+				.thenAnswer(new Answer<Object>() {
+					@Override
+					public Object answer(final InvocationOnMock invocation) throws Throwable {
+						// Always return the second argument since is't the default
+						return invocation.getArgumentAt(1, Float.class);
+					}
+				});
+
+		withMandatoryFlag = mock(DimensionHandler.class);
+		when(withMandatoryFlag.attributeId()).thenReturn(attributeId);
+		when(withMandatoryFlag.mandatory()).thenReturn(true);
+
+		missingMandatoryFlag = mock(DimensionHandler.class);
+		when(missingMandatoryFlag.attributeId()).thenReturn(attributeId);
+		when(missingMandatoryFlag.mandatory()).thenReturn(false);
+
+		adapter = new DimensionHandlerAdapter();
+	}
+
+	@Override
+	public Float getExpectedValue() {
+		return expectedValue;
+	}
+
+	@Override
+	public TypedArray getTypedArrayContainingAttribute() {
+		return containingAttribute;
+	}
+
+	@Override
+	public TypedArray getTypedArrayMissingAttribute() {
+		return missingAttribute;
+	}
+
+	@Override
+	public DimensionHandler getAnnotationWithMandatoryFlag() {
+		return withMandatoryFlag;
+	}
+
+	@Override
+	public DimensionHandler getAnnotationMissingMandatoryFlag() {
+		return missingMandatoryFlag;
+	}
+
+	@Override
+	public DimensionHandlerAdapter getAdapter() {
+		return adapter;
+	}
+}
