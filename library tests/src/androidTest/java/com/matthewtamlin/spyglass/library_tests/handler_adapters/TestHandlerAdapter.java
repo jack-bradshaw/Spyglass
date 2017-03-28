7b4fd0c4a7130e0d56ca565a96806b090e67626a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-28 21:12:06 +1030

    Created TestHandlerAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestHandlerAdapter.java
@@ -0,0 +1,84 @@
+package com.matthewtamlin.spyglass.library_tests.handler_adapters;
+
+import android.content.res.TypedArray;
+
+import com.matthewtamlin.spyglass.library.handler_adapters.HandlerAdapter;
+
+import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public abstract class TestHandlerAdapter<V,
+		A extends Annotation,
+		H extends HandlerAdapter<V, A>> {
+
+	public abstract V getExpectedValue();
+
+	public abstract TypedArray getTypedArrayContainingAttribute();
+
+	public abstract TypedArray getTypedArrayMissingAttribute();
+
+	public abstract A getAnnotationWithMandatoryFlag();
+
+	public abstract A getAnnotationMissingMandatoryFlag();
+
+	public abstract H getAdapter();
+
+	@Test
+	public void testReflectiveInstantiation() throws Exception {
+		getAdapter().getClass().newInstance();
+	}
+
+	@Test
+	public void testAttributeValueIsAvailable_valueAvailable() {
+		final boolean available = getAdapter().attributeValueIsAvailable(
+				getTypedArrayContainingAttribute(),
+				getAnnotationWithMandatoryFlag());
+
+		assertThat(available, is(true));
+	}
+
+	@Test
+	public void testAttributeValueIsAvailable_valueMissing() {
+		final boolean available = getAdapter().attributeValueIsAvailable(
+				getTypedArrayMissingAttribute(),
+				getAnnotationWithMandatoryFlag());
+
+		assertThat(available, is(false));
+	}
+
+	@Test
+	public void testGetAttributeValue_valueAvailable() {
+		final V value = getAdapter().getAttributeValue(
+				getTypedArrayContainingAttribute(),
+				getAnnotationWithMandatoryFlag());
+
+		assertThat(value, is(getExpectedValue()));
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testGetAttributeValue_valueMissing() {
+		getAdapter().getAttributeValue(
+				getTypedArrayMissingAttribute(),
+				getAnnotationWithMandatoryFlag());
+	}
+
+	@Test
+	public void testAttributeIsMandatory_mandatoryFlagPresent() {
+		final boolean mandatory = getAdapter().attributeIsMandatory(
+				getAnnotationWithMandatoryFlag());
+
+		assertThat(mandatory, is(true));
+	}
+
+	@Test
+	public void testAttributeIsMandatory_mandatoryFlagMissing() {
+		final boolean mandatory = getAdapter().attributeIsMandatory(
+				getAnnotationMissingMandatoryFlag());
+
+		assertThat(mandatory, is(false));
+	}
+}
