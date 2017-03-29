d989e5a8a27e2eeddbd2102cb7b967394583d4ac
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-29 12:59:26 +1030

    Changed adapter class declaration

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_annotations/UseLong.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_annotations/UseLong.java
@@ -1,13 +1,14 @@
 package com.matthewtamlin.spyglass.library.use_annotations;
 
 import com.matthewtamlin.spyglass.library.meta_annotations.Use;
+import com.matthewtamlin.spyglass.library.use_adapters.UseLongAdapter;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Use(adapterClass = UseLong.class)
+@Use(adapterClass = UseLongAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface UseLong {
