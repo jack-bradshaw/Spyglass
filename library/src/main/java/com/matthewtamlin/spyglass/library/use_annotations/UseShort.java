ad05d09a415482a86617d8afb46e7b2ab9803114
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-29 12:59:32 +1030

    Changed adapter class declaration

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_annotations/UseShort.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_annotations/UseShort.java
@@ -1,13 +1,14 @@
 package com.matthewtamlin.spyglass.library.use_annotations;
 
 import com.matthewtamlin.spyglass.library.meta_annotations.Use;
+import com.matthewtamlin.spyglass.library.use_adapters.UseShortAdapter;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Use(adapterClass = UseShort.class)
+@Use(adapterClass = UseShortAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface UseShort {
