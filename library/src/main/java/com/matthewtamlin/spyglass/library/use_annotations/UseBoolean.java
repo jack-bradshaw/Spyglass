3e6486fa7e61f92e33b712e7d02f96f6751b9d81
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-29 12:59:09 +1030

    Changed adapter class declaration

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_annotations/UseBoolean.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_annotations/UseBoolean.java
@@ -1,13 +1,14 @@
 package com.matthewtamlin.spyglass.library.use_annotations;
 
 import com.matthewtamlin.spyglass.library.meta_annotations.Use;
+import com.matthewtamlin.spyglass.library.use_adapters.UseBooleanAdapter;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Use(adapterClass = UseBoolean.class)
+@Use(adapterClass = UseBooleanAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface UseBoolean {
