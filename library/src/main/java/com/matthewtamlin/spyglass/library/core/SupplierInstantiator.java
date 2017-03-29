cf0d90047ba44813d5291e6b6b3753f3f07ed60f
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-03-29 16:04:10 +1030

    Added method instantiateWildcardSupplier

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/SupplierInstantiator.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/SupplierInstantiator.java
@@ -19,4 +19,25 @@ public class SupplierInstantiator {
 			throw new SupplierInstantiationException(String.format(message, clazz), e);
 		}
 	}
+
+	@SuppressWarnings("TryWithIdenticalCatches") // Can't be collapsed before API 19
+	public static Supplier<?> instantiateWildcardSupplier(
+			final Class<? extends Supplier<?>> clazz) {
+
+		try {
+			return clazz.newInstance();
+
+		} catch (final InstantiationException e) {
+			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
+					" class with a public no-arg constructor?";
+
+			throw new SupplierInstantiationException(String.format(message, clazz), e);
+
+		} catch (final IllegalAccessException e) {
+			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
+					" class with a public no-arg constructor?";
+
+			throw new SupplierInstantiationException(String.format(message, clazz), e);
+		}
+	}
 }
