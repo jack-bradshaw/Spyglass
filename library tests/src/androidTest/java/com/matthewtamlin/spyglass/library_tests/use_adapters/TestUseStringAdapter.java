6be705dea55415ee04c6ae397d3059aeb0b84085
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 00:37:59 +1030

    Created TestUseStringAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseStringAdapter.java
@@ -0,0 +1,43 @@
+package com.matthewtamlin.spyglass.library_tests.use_adapters;
+
+import com.matthewtamlin.spyglass.library.use_adapters.UseAdapter;
+import com.matthewtamlin.spyglass.library.use_adapters.UseStringAdapter;
+import com.matthewtamlin.spyglass.library.use_annotations.UseString;
+
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestUseStringAdapter extends TestUseAdapter<String, UseString> {
+	private String expectedValue;
+
+	private UseString annotation;
+
+	private UseAdapter<String, UseString> adapter;
+
+	@Before
+	public void setup() {
+		expectedValue = "This is a String...";
+
+		annotation = mock(UseString.class);
+		when(annotation.value()).thenReturn(expectedValue);
+
+		adapter = new UseStringAdapter();
+	}
+
+	@Override
+	public String getExpectedValue() {
+		return expectedValue;
+	}
+
+	@Override
+	public UseString getAnnotation() {
+		return annotation;
+	}
+
+	@Override
+	public UseAdapter<String, UseString> getAdapter() {
+		return adapter;
+	}
+}
