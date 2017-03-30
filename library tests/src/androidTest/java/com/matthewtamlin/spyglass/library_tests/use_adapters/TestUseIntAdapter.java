7bc5d836a43748c905f192524ee1b70df3455caf
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 00:37:22 +1030

    Created TestUseIntAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseIntAdapter.java
@@ -0,0 +1,43 @@
+package com.matthewtamlin.spyglass.library_tests.use_adapters;
+
+import com.matthewtamlin.spyglass.library.use_adapters.UseAdapter;
+import com.matthewtamlin.spyglass.library.use_adapters.UseIntAdapter;
+import com.matthewtamlin.spyglass.library.use_annotations.UseInt;
+
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestUseIntAdapter extends TestUseAdapter<Integer, UseInt> {
+	private Integer expectedValue;
+
+	private UseInt annotation;
+
+	private UseAdapter<Integer, UseInt> adapter;
+
+	@Before
+	public void setup() {
+		expectedValue = Integer.MAX_VALUE;
+
+		annotation = mock(UseInt.class);
+		when(annotation.value()).thenReturn(expectedValue);
+
+		adapter = new UseIntAdapter();
+	}
+
+	@Override
+	public Integer getExpectedValue() {
+		return expectedValue;
+	}
+
+	@Override
+	public UseInt getAnnotation() {
+		return annotation;
+	}
+
+	@Override
+	public UseAdapter<Integer, UseInt> getAdapter() {
+		return adapter;
+	}
+}
