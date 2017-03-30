703fd327fd994a1e9f065b894e9a60af9ee82f37
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 00:37:14 +1030

    Created TestUseFloatAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseFloatAdapter.java
@@ -0,0 +1,43 @@
+package com.matthewtamlin.spyglass.library_tests.use_adapters;
+
+import com.matthewtamlin.spyglass.library.use_adapters.UseAdapter;
+import com.matthewtamlin.spyglass.library.use_adapters.UseFloatAdapter;
+import com.matthewtamlin.spyglass.library.use_annotations.UseFloat;
+
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestUseFloatAdapter extends TestUseAdapter<Float, UseFloat> {
+	private Float expectedValue;
+
+	private UseFloat annotation;
+
+	private UseAdapter<Float, UseFloat> adapter;
+
+	@Before
+	public void setup() {
+		expectedValue = Float.POSITIVE_INFINITY;
+
+		annotation = mock(UseFloat.class);
+		when(annotation.value()).thenReturn(expectedValue);
+
+		adapter = new UseFloatAdapter();
+	}
+
+	@Override
+	public Float getExpectedValue() {
+		return expectedValue;
+	}
+
+	@Override
+	public UseFloat getAnnotation() {
+		return annotation;
+	}
+
+	@Override
+	public UseAdapter<Float, UseFloat> getAdapter() {
+		return adapter;
+	}
+}
