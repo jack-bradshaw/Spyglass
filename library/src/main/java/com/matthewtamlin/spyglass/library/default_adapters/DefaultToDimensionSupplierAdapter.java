c492d073b09726032a6067b2f581492165251b6d
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:40:19 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToDimensionSupplierAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToDimensionSupplierAdapter.java
@@ -2,12 +2,14 @@ package com.matthewtamlin.spyglass.library.default_adapters;
 
 import android.content.Context;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.core.DimensionUnit;
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToDimensionSupplier;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 import static com.matthewtamlin.spyglass.library.core.supplier.SupplierInstantiator.instantiateSupplier;
 
+@Tested(testMethod = "automated")
 public class DefaultToDimensionSupplierAdapter
 		implements DefaultAdapter<Float, DefaultToDimensionSupplier> {
 
