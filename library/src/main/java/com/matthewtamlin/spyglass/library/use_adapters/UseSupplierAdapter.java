9f29f6e4ab54bef8c13a9af184411730fc6fdb0d
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:43:52 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseSupplierAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseSupplierAdapter.java
@@ -1,10 +1,12 @@
 package com.matthewtamlin.spyglass.library.use_adapters;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.core.supplier.SupplierInstantiator;
 import com.matthewtamlin.spyglass.library.use_annotations.UseSupplier;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class UseSupplierAdapter implements UseAdapter<Object, UseSupplier> {
 	@Override
 	public Object getValue(final UseSupplier annotation) {
