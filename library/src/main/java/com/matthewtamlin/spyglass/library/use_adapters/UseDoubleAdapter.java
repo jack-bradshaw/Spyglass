a1ef19e20f8bc48ac5c79c0f82386da12e95fa68
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 18:55:09 +1030

    Added null check

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseDoubleAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseDoubleAdapter.java
@@ -2,9 +2,13 @@ package com.matthewtamlin.spyglass.library.use_adapters;
 
 import com.matthewtamlin.spyglass.library.use_annotations.UseDouble;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class UseDoubleAdapter implements UseAdapter<Double, UseDouble> {
 	@Override
 	public Double getValue(final UseDouble annotation) {
+		checkNotNull(annotation, "Argument 'annotation' cannot be null.");
+
 		return annotation.value();
 	}
 }
