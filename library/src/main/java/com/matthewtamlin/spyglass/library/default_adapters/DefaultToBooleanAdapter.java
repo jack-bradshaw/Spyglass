ba6013a79adb40acd0f4c7002f1d587e12ec0f4b
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:40:01 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToBooleanAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToBooleanAdapter.java
@@ -2,10 +2,12 @@ package com.matthewtamlin.spyglass.library.default_adapters;
 
 import android.content.Context;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToBoolean;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class DefaultToBooleanAdapter implements DefaultAdapter<Boolean, DefaultToBoolean> {
 	@Override
 	public Boolean getDefault(final DefaultToBoolean annotation, final Context context) {
