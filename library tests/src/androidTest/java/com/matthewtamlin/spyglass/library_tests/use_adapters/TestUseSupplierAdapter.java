2ac0d76c73267e7970d74402d5393c9c9b4ef867
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 10:17:30 +1030

    Added @RunWith annotation

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseSupplierAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseSupplierAdapter.java
@@ -1,15 +1,19 @@
 package com.matthewtamlin.spyglass.library_tests.use_adapters;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import com.matthewtamlin.spyglass.library.core.supplier.Supplier;
 import com.matthewtamlin.spyglass.library.use_adapters.UseAdapter;
 import com.matthewtamlin.spyglass.library.use_adapters.UseSupplierAdapter;
 import com.matthewtamlin.spyglass.library.use_annotations.UseSupplier;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
+@RunWith(AndroidJUnit4.class)
 public class TestUseSupplierAdapter extends TestUseAdapter<Object, UseSupplier> {
 	private static Object expectedValue;
 
