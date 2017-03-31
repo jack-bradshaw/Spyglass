cd75e629437545d421df63354cfd993bca9abf6c
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 18:55:23 +1030

    Added null check

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseSupplierAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseSupplierAdapter.java
@@ -3,9 +3,13 @@ package com.matthewtamlin.spyglass.library.use_adapters;
 import com.matthewtamlin.spyglass.library.core.SupplierInstantiator;
 import com.matthewtamlin.spyglass.library.use_annotations.UseSupplier;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class UseSupplierAdapter implements UseAdapter<Object, UseSupplier> {
 	@Override
 	public Object getValue(final UseSupplier annotation) {
+		checkNotNull(annotation, "Argument 'annotation' cannot be null.");
+
 		return SupplierInstantiator.instantiateWildcardSupplier(annotation.value()).get();
 	}
 }
