ac855ceca21674391054c06700951f93a021855c
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:43:32 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseBooleanAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseBooleanAdapter.java
@@ -1,9 +1,11 @@
 package com.matthewtamlin.spyglass.library.use_adapters;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.use_annotations.UseBoolean;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class UseBooleanAdapter implements UseAdapter<Boolean, UseBoolean> {
 	@Override
 	public Boolean getValue(final UseBoolean annotation) {
