08798dc26bd1938117a5c2af804c22f1c83934ea
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 10:15:18 +1030

    Added @RunWith annotation

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestColorHandlerAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestColorHandlerAdapter.java
@@ -1,11 +1,13 @@
 package com.matthewtamlin.spyglass.library_tests.handler_adapters;
 
 import android.content.res.TypedArray;
+import android.support.test.runner.AndroidJUnit4;
 
 import com.matthewtamlin.spyglass.library.handler_adapters.ColorHandlerAdapter;
 import com.matthewtamlin.spyglass.library.handler_annotations.ColorHandler;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
@@ -17,6 +19,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SuppressWarnings("ResourceType")
+@RunWith(AndroidJUnit4.class)
 public class TestColorHandlerAdapter extends TestHandlerAdapter<Integer, ColorHandler,
 		ColorHandlerAdapter> {
 	private Integer expectedValue;
