8db56e6388b27dcf79fcaeff13409ba1e310f682
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 18:55:02 +1030

    Added null check

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseBooleanAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseBooleanAdapter.java
@@ -2,9 +2,13 @@ package com.matthewtamlin.spyglass.library.use_adapters;
 
 import com.matthewtamlin.spyglass.library.use_annotations.UseBoolean;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class UseBooleanAdapter implements UseAdapter<Boolean, UseBoolean> {
 	@Override
 	public Boolean getValue(final UseBoolean annotation) {
+		checkNotNull(annotation, "Argument 'annotation' cannot be null.");
+
 		return annotation.value();
 	}
 }
