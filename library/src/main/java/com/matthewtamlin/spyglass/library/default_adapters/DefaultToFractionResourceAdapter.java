f31d1b8dbd79dc740a7597f9679687c19c842ddd
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:40:26 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToFractionResourceAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToFractionResourceAdapter.java
@@ -2,10 +2,12 @@ package com.matthewtamlin.spyglass.library.default_adapters;
 
 import android.content.Context;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToFractionResource;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class DefaultToFractionResourceAdapter
 		implements DefaultAdapter<Float, DefaultToFractionResource> {
 
