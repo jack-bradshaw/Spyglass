2064a021e61e47344f6918e326316309e9af9444
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 10:17:13 +1030

    Added @RunWith annotation

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseCharAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseCharAdapter.java
@@ -1,14 +1,18 @@
 package com.matthewtamlin.spyglass.library_tests.use_adapters;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import com.matthewtamlin.spyglass.library.use_adapters.UseAdapter;
 import com.matthewtamlin.spyglass.library.use_adapters.UseCharAdapter;
 import com.matthewtamlin.spyglass.library.use_annotations.UseChar;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(AndroidJUnit4.class)
 public class TestUseCharAdapter extends TestUseAdapter<Character, UseChar> {
 	private Character expectedValue;
 
