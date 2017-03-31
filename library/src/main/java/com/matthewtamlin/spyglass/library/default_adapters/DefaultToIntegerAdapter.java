19be860dfd26993eef3b8a039407c8ddc2b78cab
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:40:32 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToIntegerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToIntegerAdapter.java
@@ -2,10 +2,12 @@ package com.matthewtamlin.spyglass.library.default_adapters;
 
 import android.content.Context;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.default_annotations.DefaultToInteger;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class DefaultToIntegerAdapter
 		implements DefaultAdapter<Integer, DefaultToInteger> {
 
