974156e560315077742136c78ae2995aecb647a9
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 21:17:58 +1030

    Defined adapter class

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/ColorStateListHandler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/ColorStateListHandler.java
@@ -1,5 +1,6 @@
 package com.matthewtamlin.spyglass.library.handler_annotations;
 
+import com.matthewtamlin.spyglass.library.handler_adapters.ColorStateListHandlerAdapter;
 import com.matthewtamlin.spyglass.library.meta_annotations.Handler;
 
 import java.lang.annotation.ElementType;
@@ -7,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Handler(adapterClass =)
+@Handler(adapterClass = ColorStateListHandlerAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface ColorStateListHandler {
