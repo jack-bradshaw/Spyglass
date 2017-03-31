d2f69c5614d92bb18338958c3e825f4c623cb3d5
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:43:46 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseNullAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseNullAdapter.java
@@ -1,9 +1,11 @@
 package com.matthewtamlin.spyglass.library.use_adapters;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.use_annotations.UseNull;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class UseNullAdapter implements UseAdapter<Void, UseNull> {
 	@Override
 	public Void getValue(final UseNull annotation) {
