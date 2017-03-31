5c3fd140729aaada90934f51aa9e7163c6e2c0f2
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:43:38 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseDoubleAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseDoubleAdapter.java
@@ -1,9 +1,11 @@
 package com.matthewtamlin.spyglass.library.use_adapters;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.use_annotations.UseDouble;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class UseDoubleAdapter implements UseAdapter<Double, UseDouble> {
 	@Override
 	public Double getValue(final UseDouble annotation) {
