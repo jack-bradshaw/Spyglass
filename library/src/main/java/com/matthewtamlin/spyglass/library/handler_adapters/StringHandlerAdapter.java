a497c40e73601469d25ba64a5bda457ebcb5fa1c
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:41:57 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/StringHandlerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/StringHandlerAdapter.java
@@ -2,10 +2,12 @@ package com.matthewtamlin.spyglass.library.handler_adapters;
 
 import android.content.res.TypedArray;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.handler_annotations.StringHandler;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class StringHandlerAdapter implements HandlerAdapter<String, StringHandler> {
 	@Override
 	public boolean attributeValueIsAvailable(
