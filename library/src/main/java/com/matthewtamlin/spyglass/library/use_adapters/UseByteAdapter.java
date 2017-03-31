ca02632166e815bdb744390ea8a715f359c754cf
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 18:55:04 +1030

    Added null check

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseByteAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseByteAdapter.java
@@ -2,9 +2,13 @@ package com.matthewtamlin.spyglass.library.use_adapters;
 
 import com.matthewtamlin.spyglass.library.use_annotations.UseByte;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class UseByteAdapter implements UseAdapter<Byte, UseByte> {
 	@Override
 	public Byte getValue(final UseByte annotation) {
+		checkNotNull(annotation, "Argument 'annotation' cannot be null.");
+
 		return annotation.value();
 	}
 }
