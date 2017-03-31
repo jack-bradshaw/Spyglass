2c66b65a89a4071880f5272f70d07debe53b8333
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 21:57:31 +1030

    Created TextArrayHandlerAdapter.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/TextArrayHandlerAdapter.java
@@ -0,0 +1,42 @@
+package com.matthewtamlin.spyglass.library.handler_adapters;
+
+import android.content.res.TypedArray;
+
+import com.matthewtamlin.spyglass.library.handler_annotations.TextArrayHandler;
+
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
+public class TextArrayHandlerAdapter implements HandlerAdapter<CharSequence[], TextArrayHandler> {
+	@Override
+	public boolean attributeValueIsAvailable(
+			final TypedArray attrs,
+			final TextArrayHandler annotation) {
+
+		checkNotNull(attrs, "Argument \'attrs\' cannot be null.");
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
+		return attrs.getTextArray(annotation.attributeId()) != null;
+	}
+
+	@Override
+	public CharSequence[] getAttributeValue(
+			final TypedArray attrs,
+			final TextArrayHandler annotation) {
+
+		checkNotNull(attrs, "Argument \'attrs\' cannot be null.");
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
+		if (attributeValueIsAvailable(attrs, annotation)) {
+			return attrs.getTextArray(annotation.attributeId());
+		} else {
+			throw new RuntimeException("No attribute found for ID " + annotation.attributeId());
+		}
+	}
+
+	@Override
+	public boolean attributeIsMandatory(final TextArrayHandler annotation) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
+		return annotation.mandatory();
+	}
+}
