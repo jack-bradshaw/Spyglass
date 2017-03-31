729f4f6b78297c6009fa5882bc3ce7b02571b45a
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 10:17:24 +1030

    Added @RunWith annotation

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseNullAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseNullAdapter.java
@@ -1,16 +1,20 @@
 package com.matthewtamlin.spyglass.library_tests.use_adapters;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import com.matthewtamlin.spyglass.library.use_adapters.UseAdapter;
 import com.matthewtamlin.spyglass.library.use_adapters.UseNullAdapter;
 import com.matthewtamlin.spyglass.library.use_annotations.UseNull;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 
+@RunWith(AndroidJUnit4.class)
 public class TestUseNullAdapter extends TestUseAdapter<Void, UseNull> {
 	private Void expectedValue;
 
