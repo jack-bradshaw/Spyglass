2f2f6ee133b1c4c967b3ec69b88c47250e901d74
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:43:33 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseByteAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseByteAdapter.java
@@ -1,9 +1,11 @@
 package com.matthewtamlin.spyglass.library.use_adapters;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.use_annotations.UseByte;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class UseByteAdapter implements UseAdapter<Byte, UseByte> {
 	@Override
 	public Byte getValue(final UseByte annotation) {
