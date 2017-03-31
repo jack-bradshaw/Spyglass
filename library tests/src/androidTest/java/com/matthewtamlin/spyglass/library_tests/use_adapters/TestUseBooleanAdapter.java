aeff7d11ac92e6571431464bafd1880f3c46da83
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 10:17:09 +1030

    Added @RunWith annotation

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseBooleanAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/use_adapters/TestUseBooleanAdapter.java
@@ -1,16 +1,20 @@
 package com.matthewtamlin.spyglass.library_tests.use_adapters;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import com.matthewtamlin.spyglass.library.use_adapters.UseAdapter;
 import com.matthewtamlin.spyglass.library.use_adapters.UseBooleanAdapter;
 import com.matthewtamlin.spyglass.library.use_annotations.UseBoolean;
 
 import org.junit.Before;
+import org.junit.runner.RunWith;
 
 import java.lang.annotation.Annotation;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(AndroidJUnit4.class)
 public class TestUseBooleanAdapter extends TestUseAdapter {
 	private Boolean expectedValue;
 
