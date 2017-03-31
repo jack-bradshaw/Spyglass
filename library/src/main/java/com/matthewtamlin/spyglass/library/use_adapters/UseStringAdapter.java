2e51fc47e5bd55893e17a5219472e5b89e22e338
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-03-31 20:43:50 +1030

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseStringAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseStringAdapter.java
@@ -1,9 +1,11 @@
 package com.matthewtamlin.spyglass.library.use_adapters;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.use_annotations.UseString;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 public class UseStringAdapter implements UseAdapter<String, UseString> {
 	@Override
 	public String getValue(final UseString annotation) {
