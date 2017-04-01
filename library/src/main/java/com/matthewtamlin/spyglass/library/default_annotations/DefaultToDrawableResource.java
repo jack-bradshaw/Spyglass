003b4e1851abe05503aa92bc3d29d1af15eaa12a
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 10:30:02 +1030

    Created DefaultToDrawableResource.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToDrawableResource.java
@@ -0,0 +1,15 @@
+package com.matthewtamlin.spyglass.library.default_annotations;
+
+import com.matthewtamlin.spyglass.library.meta_annotations.Default;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Default(adapterClass =)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD})
+public @interface DefaultToDrawableResource {
+	int value();
+}
