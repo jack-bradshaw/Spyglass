1a5594520be3002199e0472ebd94187c5ff87e01
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 11:14:36 +1030

    Change type argument to array form of CharSequence

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToTextArrayResourceAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_adapters/DefaultToTextArrayResourceAdapter.java
@@ -7,10 +7,10 @@ import com.matthewtamlin.spyglass.library.default_annotations.DefaultToTextArray
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
 public class DefaultToTextArrayResourceAdapter
-		implements DefaultAdapter<CharSequence, DefaultToTextArrayResource> {
+		implements DefaultAdapter<CharSequence[], DefaultToTextArrayResource> {
 
 	@Override
-	public CharSequence getDefault(final DefaultToTextArrayResource annotation,
+	public CharSequence[] getDefault(final DefaultToTextArrayResource annotation,
 			final Context context) {
 		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
 		checkNotNull(context, "Argument \'context\' cannot be null.");
