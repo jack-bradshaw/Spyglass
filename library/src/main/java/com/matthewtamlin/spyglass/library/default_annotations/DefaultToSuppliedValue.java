c56758761976d0dc157cdebb16cf6caadc8a02ae
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 11:08:31 +1030

    Defined adapter class

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToSuppliedValue.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToSuppliedValue.java
@@ -1,6 +1,7 @@
 package com.matthewtamlin.spyglass.library.default_annotations;
 
 import com.matthewtamlin.spyglass.library.core.supplier.Supplier;
+import com.matthewtamlin.spyglass.library.default_adapters.DefaultToSuppliedValueAdapter;
 import com.matthewtamlin.spyglass.library.meta_annotations.Default;
 
 import java.lang.annotation.ElementType;
@@ -8,7 +9,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Default(adapterClass =)
+@Default(adapterClass = DefaultToSuppliedValueAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface DefaultToSuppliedValue {
