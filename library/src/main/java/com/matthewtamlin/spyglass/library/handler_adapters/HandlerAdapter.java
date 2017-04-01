c618dcb777ca05c4fd806f01aebb66b8c7b3fbd7
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 14:12:07 +1030

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/HandlerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/HandlerAdapter.java
@@ -14,9 +14,40 @@ import java.lang.annotation.Annotation;
  * 		the type of annotation accepted by this adapter
  */
 public interface HandlerAdapter<T, A extends Annotation> {
+	/**
+	 * Determines whether or not the supplied attributes contain a specific value. The supplied
+	 * annotation contains the data needed to access the value.
+	 *
+	 * @param attrs
+	 * 		the attributes to load data from, not null
+	 * @param annotation
+	 * 		defines what to load from the attributes, not null
+	 *
+	 * @return true if the data is available in the attribute, false otherwise
+	 */
 	public boolean attributeValueIsAvailable(TypedArray attrs, A annotation);
 
+	/**
+	 * Loads a specific value from the supplied attributes using the data in the supplied
+	 * annotation. Calling this method without first verifying that the value is available is
+	 * potentially unsafe and could cause an exception to be thrown.
+	 *
+	 * @param attrs
+	 * 		the attributes to load data from, not null
+	 * @param annotation
+	 * 		defines what to load from the attributes, not null
+	 *
+	 * @return the loaded value, may be null
+	 */
 	public T getAttributeValue(TypedArray attrs, A annotation);
 
+	/**
+	 * Returns whether or not the supplied annotation has been declared as mandatory.
+	 *
+	 * @param annotation
+	 * 		the annotation to check, not null
+	 *
+	 * @return true if the supplied annotation was declared mandatory, false otherwise
+	 */
 	public boolean attributeIsMandatory(A annotation);
 }
