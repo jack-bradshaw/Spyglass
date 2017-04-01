cf181eab26ff431f283fd3f0f76944e27403105a
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 10:39:30 +1030

    Created DefaultToSupplierValue.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToSuppliedValue.java
@@ -0,0 +1,17 @@
+package com.matthewtamlin.spyglass.library.default_annotations;
+
+import com.matthewtamlin.spyglass.library.core.supplier.Supplier;
+import com.matthewtamlin.spyglass.library.default_adapters.DefaultToColorSupplierAdapter;
+import com.matthewtamlin.spyglass.library.meta_annotations.Default;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Default(adapterClass = )
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD})
+public @interface DefaultToSuppliedValue {
+	Class<? extends Supplier<?>> value();
+}
