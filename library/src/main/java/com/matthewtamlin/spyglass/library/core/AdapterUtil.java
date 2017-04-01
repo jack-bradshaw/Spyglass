773c6b8d9a3171086be474f6c073dd90cf6f8d7b
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 14:45:21 +1030

    Rearranged methods

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/AdapterUtil.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/AdapterUtil.java
@@ -41,34 +41,6 @@ public class AdapterUtil {
 		}
 	}
 
-	public static DefaultAdapter<?, Annotation> getDefaultAdapter(
-			final Field field) {
-
-		checkNotNull(field, "Argument \'field\' cannot be null.");
-
-		final Annotation defaultAnnotation = AnnotationUtil.getDefaultAnnotation(field);
-
-		checkNotNull(defaultAnnotation, "The supplied field does not have a default annotation.");
-
-		final Class<? extends DefaultAdapter> clazz = defaultAnnotation
-				.annotationType()
-				.getAnnotation(Default.class)
-				.adapterClass();
-
-		final String exceptionMessage = String.format(
-				"Could not instantiate DefaultAdapter from class %1$s. Does the class have a " +
-						"public no-arg constructor?",
-				clazz.getName());
-
-		try {
-			return clazz.newInstance();
-		} catch (final InstantiationException e) {
-			throw new RuntimeException(exceptionMessage, e);
-		} catch (final IllegalAccessException e) {
-			throw new RuntimeException(exceptionMessage, e);
-		}
-	}
-
 	public static HandlerAdapter<?, Annotation> getHandlerAdapter(
 			final Method method) {
 
@@ -85,6 +57,34 @@ public class AdapterUtil {
 
 		final String exceptionMessage = String.format(
 				"Could not instantiate HandlerAdapter from class %1$s. Does the class have a " +
+						"public no-arg constructor?",
+				clazz.getName());
+
+		try {
+			return clazz.newInstance();
+		} catch (final InstantiationException e) {
+			throw new RuntimeException(exceptionMessage, e);
+		} catch (final IllegalAccessException e) {
+			throw new RuntimeException(exceptionMessage, e);
+		}
+	}
+
+	public static DefaultAdapter<?, Annotation> getDefaultAdapter(
+			final Field field) {
+
+		checkNotNull(field, "Argument \'field\' cannot be null.");
+
+		final Annotation defaultAnnotation = AnnotationUtil.getDefaultAnnotation(field);
+
+		checkNotNull(defaultAnnotation, "The supplied field does not have a default annotation.");
+
+		final Class<? extends DefaultAdapter> clazz = defaultAnnotation
+				.annotationType()
+				.getAnnotation(Default.class)
+				.adapterClass();
+
+		final String exceptionMessage = String.format(
+				"Could not instantiate DefaultAdapter from class %1$s. Does the class have a " +
 						"public no-arg constructor?",
 				clazz.getName());
 
