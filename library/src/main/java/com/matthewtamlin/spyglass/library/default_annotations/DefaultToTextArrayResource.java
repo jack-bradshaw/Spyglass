41156120d30a18da52f0a90ec8a826b3f55a8528
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 11:11:23 +1030

    Defined adapter class

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToTextArrayResource.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToTextArrayResource.java
@@ -1,5 +1,6 @@
 package com.matthewtamlin.spyglass.library.default_annotations;
 
+import com.matthewtamlin.spyglass.library.default_adapters.DefaultToTextArrayResourceAdapter;
 import com.matthewtamlin.spyglass.library.meta_annotations.Default;
 
 import java.lang.annotation.ElementType;
@@ -7,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Default(adapterClass =)
+@Default(adapterClass = DefaultToTextArrayResourceAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface DefaultToTextArrayResource {
