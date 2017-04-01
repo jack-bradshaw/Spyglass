ed428c3c5219d7aa081c05b5cf0ddd70b56b1207
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 14:15:57 +1030

    Fixed incorrect spelling of it's

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestFractionHandlerAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestFractionHandlerAdapter.java
@@ -63,7 +63,7 @@ public class TestFractionHandlerAdapter extends TestHandlerAdapter<Float, Fracti
 				.thenAnswer(new Answer<Object>() {
 					@Override
 					public Object answer(final InvocationOnMock invocation) throws Throwable {
-						// Always return the second argument since is't the default
+						// Always return the second argument since it's the default
 						return invocation.getArgumentAt(3, Float.class);
 					}
 				});
