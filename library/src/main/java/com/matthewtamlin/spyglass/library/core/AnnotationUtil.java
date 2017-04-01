893c8595dac6bd95518f444d2b3aa6ed9cd642e0
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 14:30:39 +1030

    Created AnnotationUtil.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/AnnotationUtil.java
@@ -0,0 +1,50 @@
+package com.matthewtamlin.spyglass.library.core;
+
+import com.matthewtamlin.spyglass.library.meta_annotations.Default;
+import com.matthewtamlin.spyglass.library.meta_annotations.Handler;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class AnnotationUtil {
+	public static Annotation getHandlerAnnotation(final Field field) {
+		for (final Annotation a : field.getDeclaredAnnotations()) {
+			if (a.annotationType().isAnnotationPresent(Handler.class)) {
+				return a;
+			}
+		}
+
+		return null;
+	}
+
+	public static Annotation getDefaultAnnotation(final Field field) {
+		for (final Annotation a : field.getDeclaredAnnotations()) {
+			if (a.annotationType().isAnnotationPresent(Default.class)) {
+				return a;
+			}
+		}
+
+		return null;
+	}
+
+	public static Annotation getHandlerAnnotation(final Method method) {
+		for (final Annotation a : method.getDeclaredAnnotations()) {
+			if (a.annotationType().isAnnotationPresent(Handler.class)) {
+				return a;
+			}
+		}
+
+		return null;
+	}
+
+	public static Annotation getDefaultAnnotation(final Method method) {
+		for (final Annotation a : method.getDeclaredAnnotations()) {
+			if (a.annotationType().isAnnotationPresent(Default.class)) {
+				return a;
+			}
+		}
+
+		return null;
+	}
+}
