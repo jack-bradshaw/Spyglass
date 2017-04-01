fc67ecbe22e0a4573469647ab3c50ae174c6109b
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 11:43:12 +1030

    Created TestDefaultToFloatAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/default_adapters/TestDefaultToFloatAdapter.java
@@ -0,0 +1,57 @@
+package com.matthewtamlin.spyglass.library_tests.default_adapters;
+
+import android.content.Context;
+
+import com.matthewtamlin.spyglass.library.default_adapters.DefaultToFloatAdapter;
+import com.matthewtamlin.spyglass.library.default_adapters.DefaultToIntegerAdapter;
+import com.matthewtamlin.spyglass.library.default_annotations.DefaultToFloat;
+import com.matthewtamlin.spyglass.library.default_annotations.DefaultToInteger;
+
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestDefaultToFloatAdapter extends TestDefaultAdapter<
+		Float,
+		DefaultToFloat,
+		DefaultToFloatAdapter> {
+
+	private Context context;
+
+	private Float expectedDefaultValue;
+
+	private DefaultToFloatAdapter adapter;
+
+	private DefaultToFloat annotation;
+
+	@Before
+	public void setup() {
+		context = mock(Context.class);
+		expectedDefaultValue = Float.MAX_VALUE;
+		adapter = new DefaultToFloatAdapter();
+
+		annotation = mock(DefaultToFloat.class);
+		when(annotation.value()).thenReturn(expectedDefaultValue);
+	}
+
+	@Override
+	public Context getContext() {
+		return context;
+	}
+
+	@Override
+	public Float getExpectedDefaultValue() {
+		return expectedDefaultValue;
+	}
+
+	@Override
+	public DefaultToFloatAdapter getAdapter() {
+		return adapter;
+	}
+
+	@Override
+	public DefaultToFloat getAnnotation() {
+		return annotation;
+	}
+}
