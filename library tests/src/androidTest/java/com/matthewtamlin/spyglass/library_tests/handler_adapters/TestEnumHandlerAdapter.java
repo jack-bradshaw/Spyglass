721f1c122176d9dca93cfe2acfc9be7e5ac91695
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 14:15:53 +1030

    Fixed incorrect spelling of it's

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestEnumHandlerAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestEnumHandlerAdapter.java
@@ -56,7 +56,7 @@ public class TestEnumHandlerAdapter {
 				.thenAnswer(new Answer<Object>() {
 					@Override
 					public Object answer(final InvocationOnMock invocation) throws Throwable {
-						// Always return the second argument since is't the default
+						// Always return the second argument since it's the default
 						return invocation.getArgumentAt(1, Integer.class);
 					}
 				});
@@ -64,7 +64,7 @@ public class TestEnumHandlerAdapter {
 				.thenAnswer(new Answer<Object>() {
 					@Override
 					public Object answer(final InvocationOnMock invocation) throws Throwable {
-						// Always return the second argument since is't the default
+						// Always return the second argument since it's the default
 						return invocation.getArgumentAt(1, Integer.class);
 					}
 				});
