9607ce7a8bd74eaceb47802c0f0d82bc319f3aee
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 14:15:44 +1030

    Fixed incorrect spelling of it's

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestBooleanHandlerAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestBooleanHandlerAdapter.java
@@ -54,7 +54,7 @@ public class TestBooleanHandlerAdapter extends TestHandlerAdapter<
 				.thenAnswer(new Answer<Object>() {
 					@Override
 					public Object answer(final InvocationOnMock invocation) throws Throwable {
-						// Always return the second argument since is't the default
+						// Always return the second argument since it's the default
 						return invocation.getArgumentAt(1, Boolean.class);
 					}
 				});
