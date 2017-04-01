a3a481228cf1a9326ffc8b75d0bf627600ecad72
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-01 14:39:34 +1030

    Created 'method' methods to reflect 'field' methods

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/AdapterUtil.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/AdapterUtil.java
@@ -7,6 +7,7 @@ import com.matthewtamlin.spyglass.library.meta_annotations.Handler;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
@@ -48,6 +49,62 @@ public class AdapterUtil {
 		final Annotation defaultAnnotation = AnnotationUtil.getDefaultAnnotation(field);
 
 		checkNotNull(defaultAnnotation, "The supplied field does not have a default annotation.");
+
+		final Class<? extends DefaultAdapter> clazz = defaultAnnotation
+				.annotationType()
+				.getAnnotation(Default.class)
+				.adapterClass();
+
+		final String exceptionMessage = String.format(
+				"Could not instantiate DefaultAdapter from class %1$s. Does the class have a " +
+						"public no-arg constructor?",
+				clazz.getName());
+
+		try {
+			return clazz.newInstance();
+		} catch (final InstantiationException e) {
+			throw new RuntimeException(exceptionMessage, e);
+		} catch (final IllegalAccessException e) {
+			throw new RuntimeException(exceptionMessage, e);
+		}
+	}
+
+	public static HandlerAdapter<?, Annotation> getHandlerAdapter(
+			final Method method) {
+
+		checkNotNull(method, "Argument \'method\' cannot be null.");
+
+		final Annotation handlerAnnotation = AnnotationUtil.getHandlerAnnotation(method);
+
+		checkNotNull(handlerAnnotation, "The supplied method does not have a handler annotation.");
+
+		final Class<? extends HandlerAdapter> clazz = handlerAnnotation
+				.annotationType()
+				.getAnnotation(Handler.class)
+				.adapterClass();
+
+		final String exceptionMessage = String.format(
+				"Could not instantiate HandlerAdapter from class %1$s. Does the class have a " +
+						"public no-arg constructor?",
+				clazz.getName());
+
+		try {
+			return clazz.newInstance();
+		} catch (final InstantiationException e) {
+			throw new RuntimeException(exceptionMessage, e);
+		} catch (final IllegalAccessException e) {
+			throw new RuntimeException(exceptionMessage, e);
+		}
+	}
+
+	public static DefaultAdapter<?, Annotation> getDefaultAdapter(
+			final Method method) {
+
+		checkNotNull(method, "Argument \'method\' cannot be null.");
+
+		final Annotation defaultAnnotation = AnnotationUtil.getDefaultAnnotation(method);
+
+		checkNotNull(defaultAnnotation, "The supplied method does not have a default annotation.");
 
 		final Class<? extends DefaultAdapter> clazz = defaultAnnotation
 				.annotationType()
