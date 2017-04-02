df916cb9cd1a59598744d1ef6704e1e803420280
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-02 11:55:58 +0930

    Implemented method countArguments()

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/ArgumentUtil.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/ArgumentUtil.java
@@ -3,6 +3,8 @@ package com.matthewtamlin.spyglass.library.core;
 import java.lang.reflect.Method;
 import java.util.Map;
 
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class ArgumentUtil {
 	public static Map<Integer, Object> getArgsFromUseAnnotations(final Method method) {
 		//TODO
@@ -12,5 +14,11 @@ public class ArgumentUtil {
 	public static Integer getIndexOfUnannotatedArg(final Method method) {
 		//TODO
 		return null;
+	}
+
+	public static Integer countArguments(final Method method) {
+		checkNotNull(method, "Argument \'method\' cannot be null.");
+
+		return method.getParameterTypes().length;
 	}
 }
