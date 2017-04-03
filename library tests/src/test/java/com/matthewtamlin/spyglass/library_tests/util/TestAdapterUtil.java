ef72bae0bbf37c95f7a657177648d2bf514e897e
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-03 23:15:30 +0930

    Added facility for getting test field and methods

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAdapterUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAdapterUtil.java
@@ -6,8 +6,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 @RunWith(JUnit4.class)
 public class TestAdapterUtil {
@@ -18,7 +24,9 @@ public class TestAdapterUtil {
 
 	@Test
 	public void testGetHandlerAdapter_fieldVariant_noHandlerAnnotations() {
+		final Field[] field = TestClass.class.getDeclaredFields();
 
+		AdapterUtil.getHandlerAdapter()
 	}
 
 	@Test
@@ -99,5 +107,45 @@ public class TestAdapterUtil {
 	@Test
 	public void testGetUseAdapters_threeArguments_threeUseAnnotations() {
 
+	}
+
+	private Field getFieldWithTag(final int tagValue) {
+		for (final Field f : TestClass.class.getDeclaredFields()) {
+			final FieldTag tag = f.getAnnotation(FieldTag.class);
+
+			if (tag != null && tag.value() == tagValue) {
+				return f;
+			}
+		}
+
+		throw new RuntimeException("No field found with tag index " + tagValue);
+	}
+
+	private Method getMethodWithTag(final int tagValue) {
+		for (final Method m : TestClass.class.getDeclaredMethods()) {
+			final MethodTag tag = m.getAnnotation(MethodTag.class);
+
+			if (tag != null && tag.value() == tagValue) {
+				return m;
+			}
+		}
+
+		throw new RuntimeException("No method found with tag index " + tagValue);
+	}
+
+	private static class TestClass {
+		
+	}
+
+	@Target(ElementType.FIELD)
+	@Retention(RetentionPolicy.RUNTIME)
+	private @interface FieldTag {
+		int value();
+	}
+
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.RUNTIME)
+	private @interface MethodTag {
+		int value();
 	}
 }
