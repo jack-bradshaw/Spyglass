a555885d97c2aec5a2d484086ab2c1e1ddb77e7c
Matthew Tamlin <m.d.tamlin@gmail.com>
2017-04-03 22:52:44 +0930

    Created TestAdapterUtil.java

---
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAdapterUtil.java
@@ -0,0 +1,9 @@
+package com.matthewtamlin.spyglass.library_tests.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestAdapterUtil {
+
+}
