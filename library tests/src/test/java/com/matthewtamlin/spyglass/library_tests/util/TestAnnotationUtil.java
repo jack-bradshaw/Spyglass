013d75e3acaaaad9fde83bce2983b349c932f79c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-05 13:23:10 +0930

    Created TestClass with field/method access methods

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAnnotationUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAnnotationUtil.java
@@ -2,10 +2,17 @@ package com.matthewtamlin.spyglass.library_tests.util;
 
 import org.junit.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class TestAnnotationUtil {
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetHandlerAnnotation_fieldVariant_nullField() {
-		
+
 	}
 
 	@Test
@@ -91,5 +98,47 @@ public class TestAnnotationUtil {
 	@Test
 	public void testGetUseAnnotations_threeArgs_threeAnnotations() {
 
+	}
+
+
+	private Field getFieldWithTag(final int tagValue) {
+		for (final Field f : TestClass.class.getDeclaredFields()) {
+			final FieldTag tag = f.getAnnotation(FieldTag.class);
+
+			if (tag != null && tag.value() == tagValue) {
+				return f;
+			}
+		}
+
+		throw new RuntimeException("No field found with tag index " + tagValue);
+	}
+
+	private Method getMethodWithTag(final int tagValue) {
+		for (final Method m : TestClass.class.getDeclaredMethods()) {
+			final MethodTag tag = m.getAnnotation(MethodTag.class);
+
+			if (tag != null && tag.value() == tagValue) {
+				return m;
+			}
+		}
+
+		throw new RuntimeException("No method found with tag index " + tagValue);
+	}
+
+	@SuppressWarnings("unused")
+	private static class TestClass {
+		
+	}
+
+	@Target(ElementType.FIELD)
+	@Retention(RetentionPolicy.RUNTIME)
+	private @interface FieldTag {
+		int value();
+	}
+
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.RUNTIME)
+	private @interface MethodTag {
+		int value();
 	}
 }
