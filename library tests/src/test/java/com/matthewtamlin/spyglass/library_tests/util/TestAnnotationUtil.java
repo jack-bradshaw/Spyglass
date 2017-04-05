cd80dc99c4c2430096f72c616b439ca5cdc87e82
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-05 13:29:11 +0930

    Implemented test case

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAnnotationUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAnnotationUtil.java
@@ -43,7 +43,7 @@ public class TestAnnotationUtil {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetHandlerAnnotation_methodVariant_nullMethod() {
-
+		getHandlerAnnotation((Method) null);
 	}
 
 	@Test
