824482b9fdc5ef7257145c142acd52e846783dc9
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-07 17:36:34 +0930

    Created test skeleton

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestEnumUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestEnumUtil.java
@@ -3,5 +3,33 @@ package com.matthewtamlin.spyglass.library_tests.util;
 import org.junit.Test;
 
 public class TestEnumUtil {
-	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetEnumConstant_nullClassSupplied() {
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetEnumConstant_nonEnumClassSupplied() {
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetEnumConstant_ordinalIsNegativeOne() {
+
+	}
+
+	@Test
+	public void testGetEnumConstant_ordinalIsZero() {
+
+	}
+
+	@Test
+	public void testGetEnumConstant_ordinalIsMax() {
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetEnumConstant_ordinalExceedsMax() {
+
+	}
 }
