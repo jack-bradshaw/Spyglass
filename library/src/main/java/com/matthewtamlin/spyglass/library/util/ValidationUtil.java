e2d1496a0456e9e12067b36bc824d230735baa49
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-08 17:12:45 +0930

    Created skeleton logic

--- library/src/main/java/com/matthewtamlin/spyglass/library/util/ValidationUtil.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/util/ValidationUtil.java
@@ -2,13 +2,44 @@ package com.matthewtamlin.spyglass.library.util;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ValidationUtil {
+	private static Set<FieldRule> fieldRules = new HashSet<>();
+
+	private static Set<MethodRule> methodRules = new HashSet<>();
+
+	static {
+		createFieldRules();
+		createMethodRules();
+	}
+
 	public static void validateAnnotations(final Field field) throws SpyglassValidationException {
-		//TODO
+		for (final FieldRule rule : fieldRules) {
+			rule.checkField(field);
+		}
 	}
 
 	public static void validateAnnotations(final Method method) throws SpyglassValidationException {
-		//TODO
+		for (final MethodRule rule : methodRules) {
+			rule.checkMethod(method);
+		}
+	}
+
+	private static void createFieldRules() {
+
+	}
+
+	private static void createMethodRules() {
+
+	}
+
+	private interface FieldRule {
+		public void checkField(Field field);
+	}
+
+	private interface MethodRule {
+		public void checkMethod(Method method);
 	}
 }
