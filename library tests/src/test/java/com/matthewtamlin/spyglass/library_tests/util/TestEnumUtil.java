ad82f41983b3c7cda061be5c502823e4cde0dd3d
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-08 15:35:29 +0930

    Replaced constant references with expressions

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestEnumUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestEnumUtil.java
@@ -5,6 +5,7 @@ import com.matthewtamlin.spyglass.library.util.EnumUtil;
 import org.junit.Test;
 
 import static com.matthewtamlin.spyglass.library_tests.util.TestEnumUtil.TestEnum.CONST1;
+import static com.matthewtamlin.spyglass.library_tests.util.TestEnumUtil.TestEnum.CONST4;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -25,14 +26,14 @@ public class TestEnumUtil {
 	public void testGetEnumConstant_ordinalIsZero() {
 		final TestEnum returnedValue = EnumUtil.getEnumConstant(TestEnum.class, 0);
 
-		assertThat(returnedValue, is(CONST1));
+		assertThat(returnedValue, is(TestEnum.values()[0]));
 	}
 
 	@Test
 	public void testGetEnumConstant_ordinalIsMax() {
 		final TestEnum returnedValue = EnumUtil.getEnumConstant(TestEnum.class, MAX_ORDINAL);
 
-		assertThat(returnedValue, is(CONST1));
+		assertThat(returnedValue, is(TestEnum.values()[MAX_ORDINAL]));
 	}
 
 	@Test(expected = IllegalArgumentException.class)
