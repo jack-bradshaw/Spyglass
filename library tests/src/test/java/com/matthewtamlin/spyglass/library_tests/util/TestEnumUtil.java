213fe5d7ffb775b353a7b8281fc737c58418640d
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-08 15:29:54 +0930

    Implemented test case

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestEnumUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestEnumUtil.java
@@ -1,16 +1,18 @@
 package com.matthewtamlin.spyglass.library_tests.util;
 
+import com.matthewtamlin.spyglass.library.util.EnumUtil;
+
 import org.junit.Test;
 
 public class TestEnumUtil {
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetEnumConstant_nullClassSupplied() {
-		
+		EnumUtil.getEnumConstant(null, 0);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetEnumConstant_nonEnumClassSupplied() {
-
+		
 	}
 
 	@Test(expected = IllegalArgumentException.class)
