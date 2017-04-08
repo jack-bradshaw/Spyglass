86d0e5cf5d16e5eb99e0c83a8d8cb0df6e2bb580
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-08 15:22:24 +0930

    Created TestEnum

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestEnumUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestEnumUtil.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TestEnumUtil {
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetEnumConstant_nullClassSupplied() {
-
+		
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -31,5 +31,12 @@ public class TestEnumUtil {
 	@Test(expected = IllegalArgumentException.class)
 	public void testGetEnumConstant_ordinalExceedsMax() {
 
+	}
+
+	private enum TestEnum {
+		CONST1,
+		CONST2,
+		CONST3,
+		CONST4
 	}
 }
