572a1a4eef815b55d059fbcc8109ba64f22e8260
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-09 18:41:44 +0930

    Implemented getAttributeId()

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/StringHandlerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/StringHandlerAdapter.java
@@ -3,6 +3,7 @@ package com.matthewtamlin.spyglass.library.handler_adapters;
 import android.content.res.TypedArray;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
+import com.matthewtamlin.spyglass.library.handler_annotations.BooleanHandler;
 import com.matthewtamlin.spyglass.library.handler_annotations.StringHandler;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
@@ -33,6 +34,13 @@ public class StringHandlerAdapter implements HandlerAdapter<String, StringHandle
 				}
 			}
 		};
+	}
+
+	@Override
+	public int getAttributeId(final StringHandler annotation) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
+		return annotation.attributeId();
 	}
 
 	@Override
