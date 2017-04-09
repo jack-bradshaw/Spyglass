97f5ca6ec2239c90a2bb0a6b4911b9206bc03e86
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-09 18:40:06 +0930

    Implemented getAttributeId()

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/DrawableHandlerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_adapters/DrawableHandlerAdapter.java
@@ -4,6 +4,7 @@ import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
+import com.matthewtamlin.spyglass.library.handler_annotations.BooleanHandler;
 import com.matthewtamlin.spyglass.library.handler_annotations.DrawableHandler;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
@@ -34,6 +35,13 @@ public class DrawableHandlerAdapter implements HandlerAdapter<Drawable, Drawable
 				}
 			}
 		};
+	}
+
+	@Override
+	public int getAttributeId(final DrawableHandler annotation) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
+		return annotation.attributeId();
 	}
 
 	@Override
