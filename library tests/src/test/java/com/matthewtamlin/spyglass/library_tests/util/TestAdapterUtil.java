243572bf48e38e0ea8cfe421ec9ee52118367783
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-09 13:52:56 +0930

    Changed order of use annotations in test method

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAdapterUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestAdapterUtil.java
@@ -254,7 +254,7 @@ public class TestAdapterUtil {
 		private void method8(String s, boolean b, float f) {}
 
 		@MethodTag(9)
-		private void method8(@UseBoolean(false) int i, @UseFloat(0F) char c, String s) {}
+		private void method8(@UseBoolean(false) int i, char c, @UseFloat(0F) String s) {}
 
 		@MethodTag(10)
 		private void method9(@UseLong(0L) long l, @UseChar(0) char c, @UseString("s") String s) {}
