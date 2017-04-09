d96788b79f77f8c8f338f8e5679adfdcfeafb638
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-09 16:26:05 +0930

    Added method and field tags

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestValidationUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestValidationUtil.java
@@ -27,6 +27,8 @@ import com.matthewtamlin.spyglass.library.use_annotations.UseInt;
 import com.matthewtamlin.spyglass.library.use_annotations.UseLong;
 import com.matthewtamlin.spyglass.library.util.SpyglassValidationException;
 import com.matthewtamlin.spyglass.library.util.ValidationUtil;
+import com.matthewtamlin.spyglass.library_tests.util.FieldHelper.FieldTag;
+import com.matthewtamlin.spyglass.library_tests.util.MethodHelper.MethodTag;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -190,115 +192,145 @@ public class TestValidationUtil {
 
 	@SuppressWarnings("unused")
 	public class TestClass {
+		@FieldTag(1)
 		private Object field1;
 
+		@FieldTag(2)
 		@BooleanHandler(attributeId = 2)
 		private Object field2;
 
+		@FieldTag(3)
 		@FloatHandler(attributeId = 3)
 		@DimensionHandler(attributeId = 3)
 		private Object field3;
 
+		@FieldTag(4)
 		@StringHandler(attributeId = 4)
 		@IntegerHandler(attributeId = 4)
 		@FractionHandler(attributeId = 4)
 		private Object field4;
 
+		@FieldTag(5)
 		@BooleanHandler(attributeId = 5)
 		@DefaultToBoolean(true)
 		private Object field5;
 
+		@FieldTag(6)
 		@DimensionHandler(attributeId = 6)
 		@DefaultToInteger(6)
 		@DefaultToDrawableResource(6)
 		private Object field6;
 
+		@FieldTag(7)
 		@StringHandler(attributeId = 7)
 		@DefaultToEnumConstant(enumClass = TestEnum.class, ordinal = 0)
 		@DefaultToBooleanResource(7)
 		@DefaultToStringResource(7)
 		private Object field7;
 
+		@FieldTag(8)
 		@DefaultToColorStateListResource(1)
 		private Object field8;
 
+		@MethodTag(1)
 		private void method1() {}
 
+		@MethodTag(2)
 		@BooleanHandler(attributeId = 2)
 		private void method2(Object o) {}
 
+		@MethodTag(3)
 		@StringHandler(attributeId = 3)
 		@FloatHandler(attributeId = 3)
 		private void method3(Object o) {}
 
+		@MethodTag(4)
 		@FractionHandler(attributeId = 4)
 		@DimensionHandler(attributeId = 4)
 		@IntegerHandler(attributeId = 4)
 		private void method4(Object o) {}
 
+		@MethodTag(5)
 		@FractionHandler(attributeId = 5)
 		@DefaultToFloat(5)
 		private void method5(Object o) {}
 
+		@MethodTag(6)
 		@BooleanHandler(attributeId = 6)
 		@DefaultToColorResource(6)
 		@DefaultToNull
 		private void method6(Object o) {}
 
+		@MethodTag(7)
 		@BooleanHandler(attributeId = 6)
 		@DefaultToColorResource(6)
 		@DefaultToDimensionResource(6)
 		@DefaultToDimension(value = 10, unit = DP)
 		private void method7(Object o) {}
 
+		@MethodTag(8)
 		@DefaultToString("something")
 		private void method8(Object o) {}
 
+		@MethodTag(9)
 		@IntegerHandler(attributeId = 9)
 		private void method9() {}
 
+		@MethodTag(10)
 		@DimensionHandler(attributeId = 10)
 		private void method10(Object o) {}
 
+		@MethodTag(11)
 		@FractionHandler(attributeId = 11)
 		private void method11(@UseBoolean(true) Object o) {}
 
+		@MethodTag(12)
 		@StringHandler(attributeId = 12)
 		private void method12(Object o1, Object o2, Object o3) {}
 
+		@MethodTag(13)
 		@BooleanHandler(attributeId = 13)
 		private void method13(@UseChar('A') Object o1, Object o2, @UseBoolean(true) Object o3) {}
 
+		@MethodTag(14)
 		@EnumConstantHandler(attributeId = 14, enumClass = TestEnum.class, ordinal = 0)
 		private void method14(
 				@UseLong(0L) Object o1,
 				@UseInt(1) Object o2,
 				@UseByte(0) Object o3) {}
 
+		@MethodTag(15)
 		@EnumConstantHandler(attributeId = 15, enumClass = TestEnum.class, ordinal = 0)
 		private void method15() {}
 
+		@MethodTag(16)
 		@EnumConstantHandler(attributeId = 16, enumClass = TestEnum.class, ordinal = 0)
 		private void method16(Object o) {}
 
+		@MethodTag(17)
 		@EnumConstantHandler(attributeId = 17, enumClass = TestEnum.class, ordinal = 0)
 		private void method17(@UseBoolean(true) Object o) {}
 
+		@MethodTag(18)
 		@EnumConstantHandler(attributeId = 18, enumClass = TestEnum.class, ordinal = 0)
 		private void method18(Object o1, Object o2, Object o3) {}
 
+		@MethodTag(19)
 		@EnumConstantHandler(attributeId = 19, enumClass = TestEnum.class, ordinal = 0)
 		private void method19(@UseChar('A') Object o1, Object o2, @UseBoolean(true) Object o3) {}
 
+		@MethodTag(20)
 		@EnumConstantHandler(attributeId = 20, enumClass = TestEnum.class, ordinal = 0)
 		private void method20(
 				@UseLong(0L) Object o1,
 				@UseInt(1) Object o2,
 				@UseByte(0) Object o3) {}
 
+		@MethodTag(21)
 		@BooleanHandler(attributeId = 21)
 		private void method21(@UseLong(0L) @UseBoolean(true) Object o) {}
 
+		@MethodTag(22)
 		private void method22(@UseBoolean(true) Object o) {}
 	}
 
