6fe9c1306400cad38173c80c0558d63d1e17ce55
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-10 20:56:06 +0930

    Added spacing

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/Spyglass.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/Spyglass.java
@@ -84,11 +84,13 @@ public class Spyglass {
 
 			if (accessor.valueExistsInArray(attrSource)) {
 				bindDataToField(field, accessor.getValueFromArray(attrSource));
+
 			} else if (getDefaultAnnotation(field) != null) {
 				final DefaultAdapter<?, Annotation> defaultAdapter = getDefaultAdapter(field);
 				bindDataToField(
 						field,
 						defaultAdapter.getDefault(getDefaultAnnotation(field), context));
+
 			} else {
 				final String message = "Missing mandatory attribute %1$s in view %2$s.";
 
