18488a5edf06bc709520bdaf721b05148d8c4f6e
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-11 08:55:54 +0930

    Made return match return type in getUseAdapters()

--- library/src/main/java/com/matthewtamlin/spyglass/library/util/AdapterUtil.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/util/AdapterUtil.java
@@ -116,7 +116,7 @@ public class AdapterUtil {
 	public static Map<Integer, UseAdapter<?, Annotation>> getUseAdapters(final Method method)  {
 		checkNotNull(method, "Argument \'method \' cannot be null.");
 
-		final Map<Integer, UseAdapter> adapters = new HashMap<>();
+		final Map<Integer, UseAdapter<?, Annotation>> adapters = new HashMap<>();
 
 		final Map<Integer, Annotation> useAnnotations = getUseAnnotations(method);
 
