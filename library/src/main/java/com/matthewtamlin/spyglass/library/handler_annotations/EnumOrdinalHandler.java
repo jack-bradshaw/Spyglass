64e5157c0f51b1f5412f8771255e85b62b28d89f
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-12 09:03:55 +0930

    Added @Handler annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/EnumOrdinalHandler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/EnumOrdinalHandler.java
@@ -1,5 +1,10 @@
 package com.matthewtamlin.spyglass.library.handler_annotations;
 
+import com.matthewtamlin.spyglass.library.handler_adapters.EnumOrdinalHandlerAdapter;
+import com.matthewtamlin.spyglass.library.meta_annotations.Handler;
+
+
+@Handler(adapterClass = EnumOrdinalHandlerAdapter.class)
 public @interface EnumOrdinalHandler {
 	int attributeId();
 
