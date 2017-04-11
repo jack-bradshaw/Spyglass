beffc1a2e4b31431be212c48eeaa737e8d52bbac
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-11 20:05:18 +0930

    Implemented setup

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestFlagHandlerAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestFlagHandlerAdapter.java
@@ -3,12 +3,22 @@ package com.matthewtamlin.spyglass.library_tests.handler_adapters;
 import android.content.res.TypedArray;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.matthewtamlin.spyglass.library.handler_adapters.EnumConstantHandlerAdapter;
 import com.matthewtamlin.spyglass.library.handler_adapters.FlagHandlerAdapter;
 import com.matthewtamlin.spyglass.library.handler_annotations.FlagHandler;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SuppressWarnings("ResourceType")
 @RunWith(AndroidJUnit4.class)
@@ -35,7 +45,49 @@ public class TestFlagHandlerAdapter {
 
 	@Before
 	public void setup() {
+		missingAttribute = mock(TypedArray.class);
+		when(missingAttribute.hasValue(ATTRIBUTE_ID)).thenReturn(false);
+		when(missingAttribute.getInt(eq(ATTRIBUTE_ID), anyInt())).thenAnswer(new Answer<Object>() {
+			@Override
+			public Object answer(final InvocationOnMock invocation) throws Throwable {
+				// Always return the second argument since it's the default
+				return invocation.getArgumentAt(1, Integer.class);
+			}
+		});
+		when(missingAttribute.getInteger(eq(ATTRIBUTE_ID), anyInt()))
+				.thenAnswer(new Answer<Object>() {
+					@Override
+					public Object answer(final InvocationOnMock invocation) throws Throwable {
+						// Always return the second argument since it's the default
+						return invocation.getArgumentAt(1, Integer.class);
+					}
+				});
 
+		containingFlag1 = mock(TypedArray.class);
+		when(containingFlag1.hasValue(ATTRIBUTE_ID)).thenReturn(true);
+		when(containingFlag1.getInt(eq(ATTRIBUTE_ID), anyInt())).thenReturn(FLAG_1);
+		when(containingFlag1.getInteger(eq(ATTRIBUTE_ID), anyInt())).thenReturn(FLAG_1);
+
+		containingFlag2 = mock(TypedArray.class);
+		when(containingFlag2.hasValue(ATTRIBUTE_ID)).thenReturn(true);
+		when(containingFlag2.getInt(eq(ATTRIBUTE_ID), anyInt())).thenReturn(FLAG_2);
+		when(containingFlag2.getInteger(eq(ATTRIBUTE_ID), anyInt())).thenReturn(FLAG_2);
+
+		containingFlags1And2 = mock(TypedArray.class);
+		when(containingFlags1And2.hasValue(ATTRIBUTE_ID)).thenReturn(true);
+		when(containingFlags1And2.getInt(eq(ATTRIBUTE_ID), anyInt())).thenReturn(FLAG_1 | FLAG_2);
+		when(containingFlags1And2.getInteger(eq(ATTRIBUTE_ID), anyInt()))
+				.thenReturn(FLAG_1 | FLAG_2);
+
+		handlesFlag1 = mock(FlagHandler.class);
+		when(handlesFlag1.attributeId()).thenReturn(ATTRIBUTE_ID);
+		when(handlesFlag1.handledFlags()).thenReturn(FLAG_1);
+
+		handlesFlag1And2 = mock(FlagHandler.class);
+		when(handlesFlag1And2.attributeId()).thenReturn(ATTRIBUTE_ID);
+		when(handlesFlag1And2.handledFlags()).thenReturn(FLAG_1 | FLAG_2);
+
+		adapter = new FlagHandlerAdapter();
 	}
 
 	@Test
