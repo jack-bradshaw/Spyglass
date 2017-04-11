3cb0577087209b9ab1b195dc2b3a34cc04258e01
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-12 09:04:21 +0930

    Added @Handler annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/FlagHandler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/handler_annotations/FlagHandler.java
@@ -1,5 +1,9 @@
 package com.matthewtamlin.spyglass.library.handler_annotations;
 
+import com.matthewtamlin.spyglass.library.handler_adapters.FlagHandlerAdapter;
+import com.matthewtamlin.spyglass.library.meta_annotations.Handler;
+
+@Handler(adapterClass = FlagHandlerAdapter.class)
 public @interface FlagHandler {
 	int attributeId();
 
