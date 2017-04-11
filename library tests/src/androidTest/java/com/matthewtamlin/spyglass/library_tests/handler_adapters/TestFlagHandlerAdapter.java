9d90bbe3955192e5498f36ddb9ef2a0120edb331
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-11 20:04:58 +0930

    Created TestFlagHandlerAdapter.java

---
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/handler_adapters/TestFlagHandlerAdapter.java
@@ -0,0 +1,11 @@
+package com.matthewtamlin.spyglass.library_tests.handler_adapters;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@SuppressWarnings("ResourceType")
+@RunWith(AndroidJUnit4.class)
+public class TestFlagHandlerAdapter {
+	
+}
