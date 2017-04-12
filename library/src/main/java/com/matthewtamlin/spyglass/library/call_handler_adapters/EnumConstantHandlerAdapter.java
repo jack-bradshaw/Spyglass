8aaaa051d5a0c68d9e52fe5d3f762b15955d004d
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-12 17:50:52 +0930

    Implemented interface

--- library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_adapters/EnumConstantHandlerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_adapters/EnumConstantHandlerAdapter.java
@@ -8,5 +8,33 @@ import com.matthewtamlin.spyglass.library.call_handler_annotations.EnumConstantH
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
 public class EnumConstantHandlerAdapter implements CallHandlerAdapter<EnumConstantHandler> {
+	@Override
+	public boolean shouldCallMethod(final EnumConstantHandler annotation, final TypedArray attrs) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+		checkNotNull(attrs, "Argument \'attrs\' cannot be null.");
 
+		if (arrayContainsValue(annotation, attrs)) {
+			return attrs.getInt(annotation.attributeId(), 0) == annotation.ordinal();
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int getAttributeId(final EnumConstantHandler annotation) {
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
+
+		return annotation.attributeId();
+	}
+
+	private static boolean arrayContainsValue(
+			final EnumConstantHandler annotation,
+			final TypedArray attrs) {
+
+		// Compare two different results to see if the default is consistently returned
+		final int reading1 = attrs.getInt(annotation.attributeId(), 0);
+		final int reading2 = attrs.getInt(annotation.attributeId(), 1);
+
+		return !((reading1 == 0) && (reading2 == 1));
+	}
 }
