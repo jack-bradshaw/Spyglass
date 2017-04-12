31fe6449c0cd1649da2f488622acccf8b3e3e52c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-12 20:35:12 +0930

    Renamed class

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/call_handler_adapters/TestSpecificEnumHandlerAdapter.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/call_handler_adapters/TestSpecificEnumHandlerAdapter.java
@@ -22,7 +22,7 @@ import static org.mockito.Mockito.when;
 
 @SuppressWarnings("ResourceType")
 @RunWith(AndroidJUnit4.class)
-public class TestEnumConstantHandlerAdapter {
+public class TestSpecificEnumHandlerAdapter {
 	private static final int ATTRIBUTE_ID = 5927;
 
 	private TypedArray withCorrectOrdinal;
