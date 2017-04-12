4f5720564a1ce62c8510c7bd1a0b9b995284e81f
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-12 17:19:50 +0930

    Created EnumConstantHandler.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_annotations/EnumConstantHandler.java
@@ -0,0 +1,22 @@
+package com.matthewtamlin.spyglass.library.call_handler_annotations;
+
+import com.matthewtamlin.spyglass.library.meta_annotations.CallHandler;
+import com.matthewtamlin.spyglass.library.value_handler_adapters.EnumConstantHandlerAdapter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@CallHandler(adapterClass = EnumConstantHandlerAdapter.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface EnumConstantHandler {
+	int attributeId();
+
+	Class<? extends Enum> enumClass();
+
+	int ordinal();
+
+	boolean mandatory() default false;
+}
