0bb850aff40a841d5bfdd45719653a0eebb8092d
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-12 13:21:19 +0930

    Defined adapter class

--- library/src/main/java/com/matthewtamlin/spyglass/library/meta_annotations/CallHandler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/meta_annotations/CallHandler.java
@@ -1,5 +1,7 @@
 package com.matthewtamlin.spyglass.library.meta_annotations;
 
+import com.matthewtamlin.spyglass.library.call_handler_adapters.CallHandlerAdapter;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,5 +10,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface CallHandler {
-
+	Class<? extends CallHandlerAdapter> adapterClass();
 }
