6365e9c8f5205f471842759997f53c6b6c18d664
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-12 17:34:09 +0930

    Deleted unused imports

--- library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_adapters/FlagHandlerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_adapters/FlagHandlerAdapter.java
@@ -1,10 +1,6 @@
 package com.matthewtamlin.spyglass.library.call_handler_adapters;
 
-import android.content.res.TypedArray;
-
 import com.matthewtamlin.spyglass.library.call_handler_annotations.FlagHandler;
-
-import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
 public class FlagHandlerAdapter implements CallHandlerAdapter<FlagHandler> {
 	
