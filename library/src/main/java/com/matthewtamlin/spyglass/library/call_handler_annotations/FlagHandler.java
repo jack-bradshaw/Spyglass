3c67ab15b78a6b8e169cc74a94b293a11ef09a79
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-12 17:19:56 +0930

    Created FlagHandler.java

---
+++ library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_annotations/FlagHandler.java
@@ -0,0 +1,19 @@
+package com.matthewtamlin.spyglass.library.call_handler_annotations;
+
+import com.matthewtamlin.spyglass.library.meta_annotations.CallHandler;
+import com.matthewtamlin.spyglass.library.meta_annotations.ValueHandler;
+import com.matthewtamlin.spyglass.library.value_handler_adapters.FlagHandlerAdapter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@CallHandler(adapterClass = FlagHandlerAdapter.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface FlagHandler {
+	int attributeId();
+
+	int handledFlags();
+}
