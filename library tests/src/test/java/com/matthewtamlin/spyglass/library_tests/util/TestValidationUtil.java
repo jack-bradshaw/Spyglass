653bc551591ec2e717807039f588b3b8d55a9f6d
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-13 18:21:33 +0930

    Deleted all method tags

--- library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestValidationUtil.java
+++ library tests/src/test/java/com/matthewtamlin/spyglass/library_tests/util/TestValidationUtil.java
@@ -123,99 +123,77 @@ public class TestValidationUtil {
 		@DefaultToColorStateListResource(1)
 		private Object field8;
 
-		@MethodTag(1)
 		private void method1() {}
 
-		@MethodTag(2)
 		@BooleanHandler(attributeId = 2)
 		private void method2(Object o) {}
 
-		@MethodTag(3)
 		@StringHandler(attributeId = 3)
 		@FloatHandler(attributeId = 3)
 		private void method3(Object o) {}
 
-		@MethodTag(4)
 		@FractionHandler(attributeId = 4)
 		@DimensionHandler(attributeId = 4)
 		@IntegerHandler(attributeId = 4)
 		private void method4(Object o) {}
 
-		@MethodTag(5)
 		@FractionHandler(attributeId = 5)
 		@DefaultToFloat(5)
 		private void method5(Object o) {}
 
-		@MethodTag(6)
 		@BooleanHandler(attributeId = 6)
 		@DefaultToColorResource(6)
 		@DefaultToNull
 		private void method6(Object o) {}
 
-		@MethodTag(7)
 		@BooleanHandler(attributeId = 6)
 		@DefaultToColorResource(6)
 		@DefaultToDimensionResource(6)
 		@DefaultToDimension(value = 10, unit = DP)
 		private void method7(Object o) {}
 
-		@MethodTag(8)
 		@DefaultToString("something")
 		private void method8(Object o) {}
 
-		@MethodTag(9)
 		@IntegerHandler(attributeId = 9)
 		private void method9() {}
 
-		@MethodTag(10)
 		@DimensionHandler(attributeId = 10)
 		private void method10(Object o) {}
 
-		@MethodTag(11)
 		@FractionHandler(attributeId = 11)
 		private void method11(@UseBoolean(true) Object o) {}
 
-		@MethodTag(12)
 		@StringHandler(attributeId = 12)
 		private void method12(Object o1, Object o2, Object o3) {}
 
-		@MethodTag(13)
 		@BooleanHandler(attributeId = 13)
 		private void method13(@UseChar('A') Object o1, Object o2, @UseBoolean(true) Object o3) {}
 
-		@MethodTag(14)
 		@FractionHandler(attributeId = 14)
 		private void method14(
 				@UseLong(0L) Object o1,
 				@UseInt(1) Object o2,
 				@UseByte(0) Object o3) {}
 
-		@MethodTag(15)
 		private void method15() {}
 
-		@MethodTag(16)
 		private void method16(Object o) {}
 
-		@MethodTag(17)
 		private void method17(@UseBoolean(true) Object o) {}
 
-		@MethodTag(18)
 		private void method18(Object o1, Object o2, Object o3) {}
 
-		@MethodTag(19)
 		private void method19(@UseChar('A') Object o1, Object o2, @UseBoolean(true) Object o3) {}
 
-		@MethodTag(20)
 		private void method20(
 				@UseLong(0L) Object o1,
 				@UseInt(1) Object o2,
 				@UseByte(0) Object o3) {}
 
-		@MethodTag(21)
 		@BooleanHandler(attributeId = 21)
 		private void method21(@UseLong(0L) @UseBoolean(true) Object o) {}
 
-		@MethodTag(22)
 		private void method22(@UseBoolean(true) Object o) {}
 	}
 
