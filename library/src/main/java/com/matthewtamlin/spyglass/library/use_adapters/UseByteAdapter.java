3494e8376808ec81c66a14193ce2286478bd5c00
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-15 12:04:24 +0930

    Added character escapes

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseByteAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseByteAdapter.java
@@ -9,7 +9,7 @@ import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull
 public class UseByteAdapter implements UseAdapter<Byte, UseByte> {
 	@Override
 	public Byte getValue(final UseByte annotation) {
-		checkNotNull(annotation, "Argument 'annotation' cannot be null.");
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
 
 		return annotation.value();
 	}
