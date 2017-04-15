290962578397e4c0b7aee55859c4ba0766f5f716
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-15 12:04:39 +0930

    Added character escapes

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseCharAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_adapters/UseCharAdapter.java
@@ -9,7 +9,7 @@ import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull
 public class UseCharAdapter implements UseAdapter<Character, UseChar> {
 	@Override
 	public Character getValue(final UseChar annotation) {
-		checkNotNull(annotation, "Argument 'annotation' cannot be null.");
+		checkNotNull(annotation, "Argument \'annotation\' cannot be null.");
 
 		return annotation.value();
 	}
