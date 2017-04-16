2528393fb7b8522feb42e0c7c329d875563fc5e7
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-16 11:25:31 +0930

    Replaced derived context with explicit context

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/Spyglass.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/Spyglass.java
@@ -44,7 +44,7 @@ public class Spyglass {
 
 		this.context = builder.context;
 
-		this.attrSource = view.getContext().obtainStyledAttributes(
+		this.attrSource = context.obtainStyledAttributes(
 				builder.attributeSet,
 				builder.styleableRes,
 				builder.defStyleAttr,
