95137f57c5ef79250588ea19a9480617457dfe62
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-17 20:58:06 +0930

    Added mandatory check

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/Spyglass.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/Spyglass.java
@@ -95,7 +95,7 @@ public class Spyglass {
 
 				bindDataToField(field, defaultValue);
 
-			} else {
+			} else if (handlerAdapter.isMandatory(handlerAnnotation)) {
 				final String message = "Missing mandatory attribute in view \"%1$s\".";
 				throw new MandatoryAttributeMissingException(String.format(message, view));
 			}
