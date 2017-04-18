92b40e1834b7d2373804c1a2c771b2efc88ab170
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-18 19:03:41 +0930

    Created FlagHandler.java

--- library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_annotations/FlagHandler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_annotations/FlagHandler.java
@@ -8,11 +8,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Methods tagged with this annotation will be called by Spyglasses to handle flag attributes.
+ * Tagged methods will only be called if the spyglass contains the supplied attribute ID, and the
+ * attribute ID is mapped to at least one of the supplied flags.
+ * <p>
+ * Methods tagged with this annotation must not have default annotations, and all parameters must
+ * have a use annotation.
+ */
 @CallHandler(adapterClass = FlagHandlerAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface FlagHandler {
+	/**
+	 * @return the attribute handled by the tagged method
+	 */
 	int attributeId();
 
+	/**
+	 * @return the flags handled by the tagged method
+	 */
 	int handledFlags();
 }
