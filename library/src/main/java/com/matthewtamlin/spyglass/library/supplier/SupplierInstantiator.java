77883039227705aa48818ed7b5e8fa974b898aa9
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-20 19:29:59 +0930

    Extracted common message

--- library/src/main/java/com/matthewtamlin/spyglass/library/supplier/SupplierInstantiator.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/supplier/SupplierInstantiator.java
@@ -2,15 +2,15 @@ package com.matthewtamlin.spyglass.library.supplier;
 
 @SuppressWarnings("TryWithIdenticalCatches") // Can't be collapsed before API 19
 public class SupplierInstantiator {
+	private static final String MESSAGE = "Unable to instantiate supplier of type %1$s. Is it a " +
+			"concrete class with a public no-arg constructor?";
+
 	public static <T> Supplier<T> instantiateSupplier(final Class<? extends Supplier<T>> clazz) {
 		try {
 			return clazz.newInstance();
 
 		} catch (final Exception e) {
-			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
-					" class with a public no-arg constructor?";
-
-			throw new SupplierInstantiationException(String.format(message, clazz), e);
+			throw new SupplierInstantiationException(String.format(MESSAGE, clazz), e);
 		}
 	}
 
@@ -19,12 +19,9 @@ public class SupplierInstantiator {
 
 		try {
 			return clazz.newInstance();
-
+			
 		} catch (final Exception e) {
-			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
-					" class with a public no-arg constructor?";
-
-			throw new SupplierInstantiationException(String.format(message, clazz), e);
+			throw new SupplierInstantiationException(String.format(MESSAGE, clazz), e);
 		}
 	}
 }
