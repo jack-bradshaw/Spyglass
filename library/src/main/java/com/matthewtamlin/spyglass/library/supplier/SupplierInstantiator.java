90946caca3dc98a7383d0ff655516cf7de1e66aa
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-21 00:55:09 +0930

    Added @Tested annotation

--- library/src/main/java/com/matthewtamlin/spyglass/library/supplier/SupplierInstantiator.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/supplier/SupplierInstantiator.java
@@ -1,7 +1,10 @@
 package com.matthewtamlin.spyglass.library.supplier;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
+
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
+@Tested(testMethod = "automated")
 @SuppressWarnings("TryWithIdenticalCatches") // Can't be collapsed before API 19
 public class SupplierInstantiator {
 	private static final String MESSAGE = "Unable to instantiate supplier of type %1$s. Is it a " +
