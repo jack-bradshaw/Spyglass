84bb5ec8e9e5b89a30a09117297ca4128f5b100f
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-20 19:29:04 +0930

    Collapsed catch blocks

--- library/src/main/java/com/matthewtamlin/spyglass/library/supplier/SupplierInstantiator.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/supplier/SupplierInstantiator.java
@@ -6,13 +6,7 @@ public class SupplierInstantiator {
 		try {
 			return clazz.newInstance();
 
-		} catch (final InstantiationException e) {
-			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
-					" class with a public no-arg constructor?";
-
-			throw new SupplierInstantiationException(String.format(message, clazz), e);
-
-		} catch (final IllegalAccessException e) {
+		} catch (final Exception e) {
 			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
 					" class with a public no-arg constructor?";
 
@@ -26,13 +20,7 @@ public class SupplierInstantiator {
 		try {
 			return clazz.newInstance();
 
-		} catch (final InstantiationException e) {
-			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
-					" class with a public no-arg constructor?";
-
-			throw new SupplierInstantiationException(String.format(message, clazz), e);
-
-		} catch (final IllegalAccessException e) {
+		} catch (final Exception e) {
 			final String message = "Unable to instantiate supplier of type %1$s. Is it a concrete" +
 					" class with a public no-arg constructor?";
 
