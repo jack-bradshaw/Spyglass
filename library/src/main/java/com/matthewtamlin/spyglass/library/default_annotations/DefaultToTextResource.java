aa398991be078be6301e5e21e8a004544d9e75e2
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-21 01:36:07 +0930

    Removed Field type from targets

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToTextResource.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToTextResource.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToTextResourceAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target({ElementType.METHOD})
 public @interface DefaultToTextResource {
 	int value();
 }
