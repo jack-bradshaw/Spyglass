471d91bef6f5bb43874b376206e2793e95b4dc5b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-21 10:46:19 +0930

    Renamed methods and variables in TargetViews
    - Updated declarations
    - Updated references

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/core/TargetViews.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/core/TargetViews.java
@@ -16,24 +16,24 @@ public class TargetViews {
 	public static final byte USE_BYTE_VALUE = 54;
 
 	public static class SpyglassTestView extends View {
-		private Object[] argsFromLastSpyglassMethodInvocation;
+		private Object[] args;
 
 		public SpyglassTestView(final Context context) {
 			super(context);
 		}
 
-		public Object[] getArgsFromLastSpyglassMethodInvocation() {
-			return argsFromLastSpyglassMethodInvocation;
+		public Object[] getArgs() {
+			return args;
 		}
 
-		public void setArgsFromLastSpyglassMethodInvocation(final Object[] args) {
-			this.argsFromLastSpyglassMethodInvocation = args;
+		public void setArgs(final Object[] args) {
+			this.args = args;
 		}
 	}
 
 	public static class NoAnnotations extends SpyglassTestView {
 		public void spyglassMethod(final String arg1, byte arg2) {
-			setArgsFromLastSpyglassMethodInvocation(new Object[]{arg1, arg2});
+			setArgs(new Object[]{arg1, arg2});
 		}
 
 		public NoAnnotations(final Context context) {
@@ -44,7 +44,7 @@ public class TargetViews {
 	public static class OnlyHandlerPresent extends SpyglassTestView {
 		@StringHandler(attributeId = SpyglassTestView_string_attr)
 		public void spyglassMethod(final String arg1, @UseByte(USE_BYTE_VALUE) byte arg2) {
-			setArgsFromLastSpyglassMethodInvocation(new Object[]{arg1, arg2});
+			setArgs(new Object[]{arg1, arg2});
 		}
 
 		public OnlyHandlerPresent(final Context context) {
@@ -56,7 +56,7 @@ public class TargetViews {
 		@StringHandler(attributeId = SpyglassTestView_string_attr)
 		@DefaultToString(DEFAULT_STRING)
 		public void spyglassMethod(final String arg1, @UseByte(USE_BYTE_VALUE) byte arg2) {
-			setArgsFromLastSpyglassMethodInvocation(new Object[]{arg1, arg2});
+			setArgs(new Object[]{arg1, arg2});
 		}
 
 		public HandlerAndDefaultPresent(final Context context) {
@@ -67,7 +67,7 @@ public class TargetViews {
 	public static class HandlerTypeMismatch extends SpyglassTestView {
 		@StringHandler(attributeId = SpyglassTestView_string_attr)
 		public void spyglassMethod(final boolean arg1, @UseByte(USE_BYTE_VALUE) byte arg2) {
-			setArgsFromLastSpyglassMethodInvocation(new Object[]{arg1, arg2});
+			setArgs(new Object[]{arg1, arg2});
 		}
 
 		public HandlerTypeMismatch(final Context context) {
@@ -79,7 +79,7 @@ public class TargetViews {
 		@StringHandler(attributeId = SpyglassTestView_string_attr)
 		@DefaultToBoolean(false)
 		public void spyglassMethod(final String arg1, @UseByte(USE_BYTE_VALUE) byte arg2) {
-			setArgsFromLastSpyglassMethodInvocation(new Object[]{arg1, arg2});
+			setArgs(new Object[]{arg1, arg2});
 		}
 
 		public DefaultTypeMismatch(final Context context) {
@@ -90,7 +90,7 @@ public class TargetViews {
 	public static class UseTypeMismatch extends SpyglassTestView {
 		@StringHandler(attributeId = SpyglassTestView_string_attr)
 		public void spyglassMethod(final String arg1, @UseByte(USE_BYTE_VALUE) boolean arg2) {
-			setArgsFromLastSpyglassMethodInvocation(new Object[]{arg1, arg2});
+			setArgs(new Object[]{arg1, arg2});
 		}
 
 		public UseTypeMismatch(final Context context) {
