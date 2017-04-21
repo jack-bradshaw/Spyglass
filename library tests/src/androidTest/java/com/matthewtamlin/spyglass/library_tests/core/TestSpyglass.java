e854d70d15300de3563e73c87adb85e10ef01873
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-22 00:26:02 +0930

    Deleted unused imports

--- library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/core/TestSpyglass.java
+++ library tests/src/androidTest/java/com/matthewtamlin/spyglass/library_tests/core/TestSpyglass.java
@@ -1,7 +1,6 @@
 package com.matthewtamlin.spyglass.library_tests.core;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.util.AttributeSet;
