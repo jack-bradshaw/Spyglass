95c289672844c9c6421a2cc92cf7342e101efcb1
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-22 13:15:49 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToBoolean.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToBoolean.java
@@ -9,10 +9,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Declares a default value to the Spyglass framework. This annotation should only be applied to
+ * methods with handler annotations, and should not be used in conjunction with other defaults.
+ */
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToBooleanAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DefaultToBoolean {
+	/**
+	 * @return the default value
+	 */
 	boolean value();
 }
