d8b47c3c17e96905bcfc4bd643e8bf30a1912d2c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-22 13:52:30 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToDimension.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToDimension.java
@@ -10,12 +10,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Declares a default value to the Spyglass framework. This annotation should only be applied to
+ * methods which accept integers and have handler annotations, and it should not be used in
+ * conjunction with other defaults. The supplied value is converted to units of pixels before
+ * being passed to the method.
+ */
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToDimensionAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DefaultToDimension {
+	/**
+	 * @return the default value
+	 */
 	float value();
 
+	/**
+	 * @return the units of {@link #value()}
+	 */
 	DimensionUnit unit();
 }
