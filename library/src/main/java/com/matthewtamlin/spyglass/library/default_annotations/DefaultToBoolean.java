6c1ed44ade64c79cc57adc2a57c7b5aefd6168ec
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-22 13:56:23 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToBoolean.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToBoolean.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 
 /**
  * Declares a default value to the Spyglass framework. This annotation should only be applied to
- * methods which accept booleans and have handler annotations, and it should not be used in
+ * methods which accept a boolean and have a handler annotation, and it should not be used in
  * conjunction with other defaults.
  */
 @Tested(testMethod = "automated")
