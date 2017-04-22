0df881ed3ccc17a28b24aabcfb2003d8afa68d15
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-22 10:43:52 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/InvalidBuilderStateException.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/InvalidBuilderStateException.java
@@ -1,5 +1,9 @@
 package com.matthewtamlin.spyglass.library.core;
 
+/**
+ * Exception to indicate that an attempt was made to instantiate an object via a builder with an
+ * invalid state. The exact nature of the invalid state is defined by the builder.
+ */
 public class InvalidBuilderStateException extends RuntimeException {
 	public InvalidBuilderStateException() {
 		super();
