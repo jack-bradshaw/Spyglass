5f861d677b8a0c5d6bddd48d182a02a62cf3baf7
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-22 18:28:46 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToEnumConstant.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToEnumConstant.java
@@ -9,12 +9,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Declares a default value to the Spyglass framework. The default value is obtained by getting
+ * the constant from the specified enum class which matches the specified ordinal. This
+ * annotation should only be applied to methods which have a handler annotation and accept an enum
+ * constant of the specified type. Furthermore, it should not be used in conjunction with other
+ * defaults.
+ */
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToEnumConstantAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DefaultToEnumConstant {
+	/**
+	 * @return the class of the default
+	 */
 	Class<? extends Enum> enumClass();
 
+	/**
+	 * @return the ordinal of the default
+	 */
 	int ordinal();
 }
