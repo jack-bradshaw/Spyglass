101e6d685fd84f815e2e4c235ebc339f30cd2dde
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 11:20:34 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToInteger.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToInteger.java
@@ -10,9 +10,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Declares a default value to the Spyglass framework. This annotation should only be applied to
- * methods which accept an integer and have a handler annotation, and it should not be used in
- * conjunction with other defaults.
+ * Defines a default for the annotated method, so that the Spyglass framework can invoke the
+ * method if its handler annotation is not satisfied. This annotation should only be applied to
+ * methods which satisfy all of the following criteria:
+ * <ul>
+ * <li>The method has a handler annotation.</li>
+ * <li>The method has no other default annotations.</li>
+ * <li>The method has at least one integer parameter.</li>
+ * <li>Except for one integer parameter, every parameter has a use annotation.</li>
+ * </ul>
  */
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToIntegerAdapter.class)
