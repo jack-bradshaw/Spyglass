db65a3f6e361b4ed000124ec516b24d87a38b018
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 10:26:36 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToBoolean.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToBoolean.java
@@ -16,7 +16,8 @@ import java.lang.annotation.Target;
  * <ul>
  * <li>The method has a handler annotation.</li>
  * <li>The method has no other default annotations.</li>
- * <li>The method has at least one boolean parameter without a use annotation.</li>
+ * <li>The method has at least one boolean parameter.</li>
+ * <li>One boolean parameter has no use annotation.</li>
  * <li>Every other parameter has a use annotation.</li>
  * </ul>
  */
