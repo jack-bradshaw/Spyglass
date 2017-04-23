e22e5d5648f8a91563a54c945ba63760f9ee22ab
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 11:18:06 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToColorStateListResource.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToColorStateListResource.java
@@ -17,8 +17,7 @@ import java.lang.annotation.Target;
  * <li>The method has a handler annotation.</li>
  * <li>The method has no other default annotations.</li>
  * <li>The method has at least one integer array parameter.</li>
- * <li>One integer array parameter has no use annotation.</li>
- * <li>Every other parameter has a use annotation.</li>
+ * <li>Except for one integer array parameter, every parameter has a use annotation.</li>
  * </ul>
  */
 @Tested(testMethod = "automated")
