0b133c3bb83deed54d838eedcc109a985506fde5
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 11:01:42 +0930

    Deleted old Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToEnumConstant.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToEnumConstant.java
@@ -10,14 +10,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Declares a default value to the Spyglass framework. The default value is obtained by getting
- * the constant from the specified enum class which matches the specified ordinal. This
- * annotation should only be applied to methods which have a handler annotation and accept an enum
- * constant of the specified type. Furthermore, it should not be used in conjunction with other
- * defaults.
- */
-
-/**
  * Defines a default for the annotated method, so that the Spyglass framework can invoke the
  * method if its handler annotation is not satisfied. The default value is obtained by using
  * the values supplied to {@code enumClass()} and {@code ordinal()} to get a reference to a
