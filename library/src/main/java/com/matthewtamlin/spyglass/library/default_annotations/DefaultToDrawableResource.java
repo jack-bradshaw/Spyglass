30e605ee1ea635d96ca7dcc908ae9bfd4a3a7fa0
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 10:48:39 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToDrawableResource.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToDrawableResource.java
@@ -14,13 +14,26 @@ import java.lang.annotation.Target;
  * applied to methods which accept a Drawable and have a handler annotation, and it should not be
  * used in conjunction with other defaults.
  */
+
+/**
+ * Defines a default for the annotated method, so that the Spyglass framework can invoke the
+ * method if its handler annotation is not satisfied. This annotation should only be applied to
+ * methods which satisfy all of the following criteria:
+ * <ul>
+ * <li>The method has a handler annotation.</li>
+ * <li>The method has no other default annotations.</li>
+ * <li>The method has at least one Drawable parameter.</li>
+ * <li>One Drawable parameter has no use annotation.</li>
+ * <li>Every other parameter has a use annotation.</li>
+ * </ul>
+ */
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToDrawableResourceAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DefaultToDrawableResource {
 	/**
-	 * @return the resource ID of the default value, must resolve to a drawable
+	 * @return the resource ID of the default value, must resolve to a drawable resource
 	 */
 	int value();
 }
