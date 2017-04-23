c520694ff9998e1e75b6abc59521512bb0d4c925
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 11:43:06 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToSuppliedValue.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToSuppliedValue.java
@@ -11,10 +11,17 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Declares a default to the Spyglass framework. The default value is obtained by reflectively
- * instantiating a supplier and getting a value from it. This annotation should only be applied
- * to methods which accept the type returned by the supplier and have a handler annotation.
- * Furthermore, it should not be used in conjunction with other defaults.
+ * Defines a default for the annotated method, so that the Spyglass framework can invoke the
+ * method if its handler annotation is not satisfied. The default value is obtained by reflectively
+ * instantiating the specified supplier and calling its {@link Supplier#get()} method. This
+ * annotation should only be applied to methods which satisfy all of the following criteria:
+ * <ul>
+ * <li>The method has a handler annotation.</li>
+ * <li>The method has no other default annotations.</li>
+ * <li>The method has at least one parameter of the type supplied by the specified supplier.</li>
+ * <li>Every parameter has a use annotation except for one parameter of the type supplied by the
+ * specified supplier.</li>
+ * </ul>
  */
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToSuppliedValueAdapter.class)
@@ -22,8 +29,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface DefaultToSuppliedValue {
 	/**
-	 * Defines the supplier to instantiate and get the default value from. The class must expose
-	 * a public no-arg constructor, and it must be static if nested in another class.
+	 * Defines the class of supplier to instantiate and get the default value from. The class must
+	 * expose a public no-arg constructor, and the class must be static if nested in another class.
 	 *
 	 * @return the supplier class
 	 */
