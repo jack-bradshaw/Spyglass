b7d442a53e924cf8901d0c980e1901835dea1083
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 10:39:01 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToDimension.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToDimension.java
@@ -1,9 +1,9 @@
 package com.matthewtamlin.spyglass.library.default_annotations;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
-import com.matthewtamlin.spyglass.library.units.DimensionUnit;
 import com.matthewtamlin.spyglass.library.default_adapters.DefaultToDimensionAdapter;
 import com.matthewtamlin.spyglass.library.meta_annotations.Default;
+import com.matthewtamlin.spyglass.library.units.DimensionUnit;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,10 +11,18 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Declares a default value to the Spyglass framework. This annotation should only be applied to
- * methods which accept an integer and have a handler annotation, and it should not be used in
- * conjunction with other defaults. The supplied value is converted to units of pixels before
- * being passed to the method.
+ * Defines a default for the annotated method, so that the Spyglass framework can invoke the
+ * method if its handler annotation is not satisfied. This annotation should only be applied to
+ * methods which satisfy all of the following criteria:
+ * <ul>
+ * <li>The method has a handler annotation.</li>
+ * <li>The method has no other default annotations.</li>
+ * <li>The method has at least one integer parameter.</li>
+ * <li>One integer parameter has no use annotation.</li>
+ * <li>Every other parameter has a use annotation.</li>
+ * </ul>
+ * <p>
+ * The dimension value is converted to units of pixels before being passed to the method.
  */
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToDimensionAdapter.class)
@@ -22,12 +30,13 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface DefaultToDimension {
 	/**
-	 * @return the default value
+	 * @return the numerical value of the default dimension, measured in the units passed to {@code
+	 * unit()}
 	 */
 	float value();
 
 	/**
-	 * @return the units of {@link #value()}
+	 * @return the units of the value passed to {@code value()}
 	 */
 	DimensionUnit unit();
 }
