c01291e970203e10b4dcf0fe8bfb116dce52a6e4
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 11:22:56 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToTextArrayResource.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToTextArrayResource.java
@@ -10,9 +10,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Declares a default value resource to the Spyglass framework. This annotation should only be
- * applied to methods which accept a CharSequence array and have a handler annotation, and it should
- * not be used in conjunction with other defaults.
+ * Defines a default for the annotated method, so that the Spyglass framework can invoke the
+ * method if its handler annotation is not satisfied. This annotation should only be applied to
+ * methods which satisfy all of the following criteria:
+ * <ul>
+ * <li>The method has a handler annotation.</li>
+ * <li>The method has no other default annotations.</li>
+ * <li>The method has at least one CharSequence array parameter.</li>
+ * <li>Except for one CharSequence array parameter, every parameter has a use annotation.</li>
+ * </ul>
  */
 @Tested(testMethod = "automated")
 @Default(adapterClass = DefaultToTextArrayResourceAdapter.class)
