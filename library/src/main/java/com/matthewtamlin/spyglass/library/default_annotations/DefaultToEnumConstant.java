9477d293c4dc998e6e6695af20df63a3a8202a28
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-23 11:12:35 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToEnumConstant.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/default_annotations/DefaultToEnumConstant.java
@@ -19,8 +19,8 @@ import java.lang.annotation.Target;
  * <li>The method has a handler annotation.</li>
  * <li>The method has no other default annotations.</li>
  * <li>The method has at least one parameter of the type passed to {@code enumClass()}.</li>
- * <li>One enum parameter has no use annotation.</li>
- * <li>Every other parameter has a use annotation.</li>
+ * <li>Except for one parameter of the type passed to {@code enumClass()}, every parameter has a
+ * use annotation.</li>
  * </ul>
  */
 @Tested(testMethod = "automated")
