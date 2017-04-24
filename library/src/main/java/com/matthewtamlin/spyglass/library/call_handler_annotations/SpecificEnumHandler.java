80c86839fb0b57426653e489f0e5f7edf4413e17
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-24 10:51:58 +0930

    Changed Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_annotations/SpecificEnumHandler.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_annotations/SpecificEnumHandler.java
@@ -10,8 +10,15 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Declares a method capable of handling specific enum attributes. Methods tagged with this
- * annotation must not have default annotations, and all parameters must have use annotations.
+ * Declares a method capable of handling a specific enum attribute. The method will only
+ * be invoked if the Spyglass framework finds the specified ordinal mapped to the specified
+ * attribute. This annotation should only be applied to methods which satisfy all of the
+ * following criteria:
+ * <ul>
+ * <li>The method has no other handler annotations.</li>
+ * <li>The method has no default annotation.</li>
+ * <li>Every parameter has a use annotation.</li>
+ * </ul>
  */
 @Tested(testMethod = "automated")
 @CallHandler(adapterClass = SpecificEnumHandlerAdapter.class)
@@ -19,12 +26,12 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface SpecificEnumHandler {
 	/**
-	 * @return the resource ID of the attribute handled by the method
+	 * @return the resource ID of the handled attribute
 	 */
 	int attributeId();
 
 	/**
-	 * @return the ordinal of the specific enum constant handled by the method
+	 * @return the ordinal of the handled enum constant
 	 */
 	int ordinal();
 }
