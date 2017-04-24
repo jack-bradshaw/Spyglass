2de552708b85afbcf8bc9c681977b0bbb550e10b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-24 11:39:11 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_adapters/CallHandlerAdapter.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/call_handler_adapters/CallHandlerAdapter.java
@@ -4,7 +4,23 @@ import android.content.res.TypedArray;
 
 import java.lang.annotation.Annotation;
 
+/**
+ * Common interface for interacting with call handler annotations.
+ *
+ * @param <A>
+ * 		the type of annotation this adapter interfaces with
+ */
 public interface CallHandlerAdapter<A extends Annotation> {
+	/**
+	 * Determines whether or not a method annotated with a call handler should be called.
+	 *
+	 * @param annotation
+	 * 		the annotation to evaluate, not null
+	 * @param attrs
+	 * 		a set of attributes to base the evaluation on
+	 *
+	 * @return true if the method should be called, false otherwise
+	 */
 	boolean shouldCallMethod(A annotation, TypedArray attrs);
 
 	public int getAttributeId(A annotation);
