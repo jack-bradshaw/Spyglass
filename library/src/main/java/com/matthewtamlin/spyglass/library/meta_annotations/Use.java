270bee0572b616c26de568ba63d6182c3a888c00
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-24 11:20:31 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/meta_annotations/Use.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/meta_annotations/Use.java
@@ -7,8 +7,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Meta annotation for all use annotations. Use annotations define the values to pass to non-data
+ * method parameters when invoked by the Spyglass framework.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
 public @interface Use {
+	/**
+	 * @return the class of adapter which can interpret the target annotation
+	 */
 	Class<? extends UseAdapter> adapterClass();
 }
