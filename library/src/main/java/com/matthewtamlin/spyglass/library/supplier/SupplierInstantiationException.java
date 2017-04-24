5924cdc5f28d82c6643c30901d08d2cf79446f7b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-24 13:19:42 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/supplier/SupplierInstantiationException.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/supplier/SupplierInstantiationException.java
@@ -7,18 +7,30 @@ import android.support.annotation.RequiresApi;
  * Exception to indicate that a {@link Supplier} could not be reflectively instantiated.
  */
 public class SupplierInstantiationException extends RuntimeException {
+	/**
+	 * Constructs a new SupplierInstantiationException with no cause or message. The
+	 */
 	public SupplierInstantiationException() {
 		super();
 	}
 
+	/**
+	 * Constructs a new SupplierInstantiationException with a message but no cause.
+	 */
 	public SupplierInstantiationException(final String message) {
 		super(message);
 	}
 
+	/**
+	 * Constructs a new SupplierInstantiationException with a message and a cause.
+	 */
 	public SupplierInstantiationException(final String message, final Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * Constructs a new SupplierInstantiationException with a cause but no method.
+	 */
 	public SupplierInstantiationException(final Throwable cause) {
 		super(cause);
 	}
