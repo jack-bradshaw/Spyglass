43b2a3829189ba9db0f3886436cd5d92d3fb8617
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-24 11:59:42 +0930

    Added Javadoc

--- library/src/main/java/com/matthewtamlin/spyglass/library/core/InvalidBuilderStateException.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/core/InvalidBuilderStateException.java
@@ -5,18 +5,30 @@ package com.matthewtamlin.spyglass.library.core;
  * invalid state. The exact nature of the invalid state is defined by the builder.
  */
 public class InvalidBuilderStateException extends RuntimeException {
+	/**
+	 * Constructs a new InvalidBuilderStateException with no cause or message. The
+	 */
 	public InvalidBuilderStateException() {
 		super();
 	}
 
+	/**
+	 * Constructs a new InvalidBuilderStateException with a message but no cause.
+	 */
 	public InvalidBuilderStateException(final String message) {
 		super(message);
 	}
 
+	/**
+	 * Constructs a new InvalidBuilderStateException with a message and a cause.
+	 */
 	public InvalidBuilderStateException(final String message, final Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * Constructs a new InvalidBuilderStateException with a cause but no method.
+	 */
 	public InvalidBuilderStateException(final Throwable cause) {
 		super(cause);
 	}
