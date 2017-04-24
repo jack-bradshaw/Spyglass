03910d84b59f90e738932a78cb4291a3a04d0836
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-24 14:28:52 +0930

    Added Javadoc to all use annotations

--- library/src/main/java/com/matthewtamlin/spyglass/library/use_annotations/UseString.java
+++ library/src/main/java/com/matthewtamlin/spyglass/library/use_annotations/UseString.java
@@ -9,10 +9,23 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Defines the value to supply to the annotated parameter when the Spyglass framework invokes the
+ * method. This annotation should only be applied to parameters which satisfy all of the following
+ * criteria:
+ * <ul>
+ * <li>The parameter is part of a method which has a handler annotation.</li>
+ * <li>The parameter has no other use annotations.</li>
+ * <li>The parameter accepts a String value.</li>
+ * </ul>
+ */
 @Tested(testMethod = "automated")
 @Use(adapterClass = UseStringAdapter.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface UseString {
+	/**
+	 * @return the value to pass
+	 */
 	String value();
 }
