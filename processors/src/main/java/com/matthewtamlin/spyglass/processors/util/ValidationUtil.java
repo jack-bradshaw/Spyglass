37120e484bd1ced58b225322d7e253d364e2a73a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-25 11:30:02 +0930

    Moved ValidationUtil to 'processor' module

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/util/ValidationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/util/ValidationUtil.java
@@ -1,4 +1,4 @@
-package com.matthewtamlin.spyglass.library.util;
+package com.matthewtamlin.spyglass.processors.util;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
 import com.matthewtamlin.spyglass.library.meta_annotations.CallHandler;
