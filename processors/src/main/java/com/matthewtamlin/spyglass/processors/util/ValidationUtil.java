2186ef460165a24937c38f23be3357420d077163
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-25 11:30:52 +0930

    Fixed broken imports

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/util/ValidationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/util/ValidationUtil.java
@@ -1,10 +1,9 @@
 package com.matthewtamlin.spyglass.processors.util;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
-import com.matthewtamlin.spyglass.library.meta_annotations.CallHandler;
-import com.matthewtamlin.spyglass.library.meta_annotations.Default;
-import com.matthewtamlin.spyglass.library.meta_annotations.Use;
-import com.matthewtamlin.spyglass.library.meta_annotations.ValueHandler;
+import com.matthewtamlin.spyglass.annotations.meta_annotations.CallHandler;
+import com.matthewtamlin.spyglass.annotations.meta_annotations.Default;
+import com.matthewtamlin.spyglass.annotations.meta_annotations.ValueHandler;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
