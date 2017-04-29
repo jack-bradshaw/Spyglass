c94e511769bcbcdff21d223494fbd65cdf458544
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-04-29 13:24:36 +0930

    Renamed class

---
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/validation/ValidationException.java
@@ -0,0 +1,35 @@
+package com.matthewtamlin.spyglass.processors.validation;
+
+/**
+ * Exception to indicate that the Spyglass framework detected a method with an illegal
+ * combination of annotations.
+ */
+public class ValidationException extends RuntimeException {
+	/**
+	 * Constructs a new ValidationException with no cause or message. The
+	 */
+	public ValidationException() {
+		super();
+	}
+
+	/**
+	 * Constructs a new ValidationException with a message but no cause.
+	 */
+	public ValidationException(final String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructs a new ValidationException with a message and a cause.
+	 */
+	public ValidationException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * Constructs a new ValidationException with a cause but no method.
+	 */
+	public ValidationException(final Throwable cause) {
+		super(cause);
+	}
+}
