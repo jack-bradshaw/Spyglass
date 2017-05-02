345a38de85ce38138bb712ea881d58095f540ef9
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-02 18:09:53 +0930

    Created AndroidClassDefinitions.java

---
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/AndroidClassDefinitions.java
@@ -0,0 +1,9 @@
+package com.matthewtamlin.spyglass.processors.code_generation;
+
+import com.squareup.javapoet.ClassName;
+
+public class AndroidClassDefinitions {
+	public static final ClassName CONTEXT = ClassName.get("android.content", "Context");
+
+	public static final ClassName TYPED_ARRAY = ClassName.get("android.content.res", "TypedArray");
+}
