6daa2e8731178f3cc8373a7f0dbc31d564cbf23b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-02 18:33:17 +0930

    Deleted constructor for Java 1.8

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/supplier/SupplierInstantiationException.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/supplier/SupplierInstantiationException.java
@@ -1,8 +1,5 @@
 package com.matthewtamlin.spyglass.processors.supplier;
 
-import android.annotation.TargetApi;
-import android.support.annotation.RequiresApi;
-
 import com.matthewtamlin.spyglass.annotations.supplier.Supplier;
 
 /**
@@ -35,20 +32,5 @@ public class SupplierInstantiationException extends RuntimeException {
 	 */
 	public SupplierInstantiationException(final Throwable cause) {
 		super(cause);
-	}
-
-	/**
-	 * Constructs a new SupplierInstantiationException with a message and a cause. This constructor
-	 * allows the exception to be suppressed, and the stack trace can be made writable.
-	 */
-	@RequiresApi(24) // For caller
-	@TargetApi(24) // For lint
-	protected SupplierInstantiationException(
-			final String message,
-			final Throwable cause,
-			final boolean enableSuppression,
-			final boolean writableStackTrace) {
-		
-		super(message, cause, enableSuppression, writableStackTrace);
 	}
 }
