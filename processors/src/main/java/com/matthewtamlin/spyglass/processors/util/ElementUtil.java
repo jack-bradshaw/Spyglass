016ed4989d1acf13a1f1428facc4657047e2c84b
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-03 22:48:28 +0930

    Created ElementUtil.java

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/util/ElementUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/util/ElementUtil.java
@@ -52,4 +52,29 @@ public class ElementUtil {
 
 		throw new IllegalArgumentException("All parameters of argument \'e\' have use annotations.");
 	}
+
+	public static String getPackageOfType(final TypeElement e) {
+		if (!e.getQualifiedName().contentEquals(".")) {
+			return null;
+		} else {
+			final String[] splitByPeriod = e.getQualifiedName().toString().split(".");
+
+			final StringBuilder packageBuilder = new StringBuilder();
+
+			for (int i = 0; i < splitByPeriod.length - 1; i++) {
+				packageBuilder.append(splitByPeriod[i]);
+			}
+
+			return packageBuilder.toString();
+		}
+	}
+
+	public static String getSimpleNameOfType(final TypeElement e) {
+		if (!e.getQualifiedName().contentEquals(".")) {
+			return e.getQualifiedName().toString();
+		} else {
+			final String[] splitByPeriod = e.getQualifiedName().toString().split(".");
+			return splitByPeriod[splitByPeriod.length - 1];
+		}
+	}
 }
