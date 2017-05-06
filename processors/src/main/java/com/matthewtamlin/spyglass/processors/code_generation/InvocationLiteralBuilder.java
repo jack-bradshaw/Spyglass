0f8a31076b6547cc9a463505f1fedf74a967e356
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-07 08:45:28 +0930

    Significant refactoring and restructuring

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/InvocationLiteralBuilder.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/InvocationLiteralBuilder.java
@@ -1,8 +1,8 @@
 package com.matthewtamlin.spyglass.processors.code_generation;
 
 import com.matthewtamlin.spyglass.annotations.use_annotations.UseBoolean;
+import com.matthewtamlin.spyglass.processors.annotation_utils.UseAnnotationUtil;
 import com.matthewtamlin.spyglass.processors.functional.ParametrisedSupplier;
-import com.matthewtamlin.spyglass.processors.util.AnnotationUtil;
 
 import java.lang.annotation.Annotation;
 import java.util.HashMap;
@@ -28,53 +28,41 @@ public class InvocationLiteralBuilder {
 				});
 	}
 
-	public static String buildFor(final ExecutableElement e) {
-		return assembleLiteral(e, getLiteralsFromUseAnnotations(e));
+	public static String buildInvocationLiteralFor(final ExecutableElement e) {
+		// A method name such as 'doSomeTask'
+		final String methodName = e.getSimpleName().toString();
+
+		// A map of argument literals by index, such as '[0: "hello", 1: 10L, 2: false, 3: 0b11]'
+		final Map<Integer, String> argLiterals = getArgLiteralsFromUseAnnotations(e);
+
+		// The actual method call literal such as 'doSomeTask("hello", 10L, false, 0b11)'
+		return methodName + combineArgLiterals(argLiterals);
 	}
 
-	public static String buildFor(
+	public static String buildInvocationLiteralFor(
 			final ExecutableElement e,
 			final String nonUseArgLiteral) {
 
-		final Map<Integer, String> argumentLiterals = getLiteralsFromUseAnnotations(e);
+		// A method name such as 'doSomeTask'
+		final String methodName = e.getSimpleName().toString();
 
-		for (int i = 0; i < argumentLiterals.size(); i++) {
-			if (!argumentLiterals.containsKey(i)) {
-				argumentLiterals.put(i, nonUseArgLiteral);
+		// A map of argument literals by index, such as '[0: "hello", 1: 10L, 2: false, 3: 0b11]'
+		final Map<Integer, String> argLiterals = getArgLiteralsFromUseAnnotations(e);
+
+		// Find the first index which is not mapped to a literal and map it to the nonUseArgLiteral
+		for (int i = 0; i < argLiterals.size(); i++) {
+			if (!argLiterals.containsKey(i)) {
+				argLiterals.put(i, nonUseArgLiteral);
 				break;
 			}
 		}
 
-		return assembleLiteral(e, argumentLiterals);
+		// The actual method call literal such as 'doSomeTask("hello", 10L, false, 0b11, "some non use arg")'
+		return e.getSimpleName() + combineArgLiterals(argLiterals);
 	}
 
-	private static String assembleLiteral(final ExecutableElement e, final Map<Integer, String> argumentLiterals) {
-		final StringBuilder fullInvocationLiteralBuilder = new StringBuilder();
-
-		// "someMethod"
-		final String methodName = e.getSimpleName().toString();
-		fullInvocationLiteralBuilder.append(methodName);
-
-		// "someMethod("
-		fullInvocationLiteralBuilder.append("(");
-
-		// "someMethod(1, "something", "true", 3L, 1.4F
-		for (int i = 0; i < argumentLiterals.size(); i++) {
-			fullInvocationLiteralBuilder.append(argumentLiterals.get(i));
-
-			if (i < argumentLiterals.size() - 1) {
-				fullInvocationLiteralBuilder.append(", ");
-			}
-		}
-
-		// "someMethod(1, "something", "true", 3L, 1.4F)
-		fullInvocationLiteralBuilder.append(")");
-
-		return fullInvocationLiteralBuilder.toString();
-	}
-
-	private static Map<Integer, String> getLiteralsFromUseAnnotations(final ExecutableElement e) {
-		final Map<Integer, Annotation> useAnnotations = AnnotationUtil.getUseAnnotations(e);
+	private static Map<Integer, String> getArgLiteralsFromUseAnnotations(final ExecutableElement e) {
+		final Map<Integer, Annotation> useAnnotations = UseAnnotationUtil.getUseAnnotations(e);
 		final Map<Integer, String> argumentLiterals = new HashMap<>();
 
 		for (final Integer i : useAnnotations.keySet()) {
@@ -85,5 +73,26 @@ public class InvocationLiteralBuilder {
 		}
 
 		return argumentLiterals;
+	}
+
+	private static String combineArgLiterals(final Map<Integer, String> argumentLiterals) {
+		final StringBuilder fullInvocationLiteralBuilder = new StringBuilder();
+
+		// Start with the opening bracket: '('
+		fullInvocationLiteralBuilder.append("(");
+
+		// Add the argument literals one by one using comma separation: '(1, "something", "true", 3L, 1.4F'
+		for (int i = 0; i < argumentLiterals.size(); i++) {
+			fullInvocationLiteralBuilder.append(argumentLiterals.get(i));
+
+			if (i < argumentLiterals.size() - 1) {
+				fullInvocationLiteralBuilder.append(", ");
+			}
+		}
+
+		// Add the closing bracket: '(1, "something", "true", 3L, 1.4F)'
+		fullInvocationLiteralBuilder.append(")");
+
+		return fullInvocationLiteralBuilder.toString();
 	}
 }
