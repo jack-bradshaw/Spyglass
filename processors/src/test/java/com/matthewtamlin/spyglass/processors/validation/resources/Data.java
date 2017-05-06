af06985a5e1d91973f52846171d782449892d80e
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-06 13:51:53 +0930

    Deleted 'failure message' element in @Target
    - Deleted declaration
    - Deleted implementations

--- processors/src/test/java/com/matthewtamlin/spyglass/processors/validation/resources/Data.java
+++ processors/src/test/java/com/matthewtamlin/spyglass/processors/validation/resources/Data.java
@@ -22,263 +22,172 @@ import com.matthewtamlin.spyglass.annotations.value_handler_annotations.FloatHan
 import com.matthewtamlin.spyglass.annotations.value_handler_annotations.StringHandler;
 
 public class Data {
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with no annotations should pass.")
+	@Target(isValid = true)
 	private void method1() {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with just one value handler should pass.")
+	@Target(isValid = true)
 	@StringHandler(attributeId = 1)
 	private void method2(Object o) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with multiple value handlers should fail.")
+	@Target(isValid = false)
 	@StringHandler(attributeId = 1)
 	@BooleanHandler(attributeId = 1)
 	private void method3(Object o) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with multiple value handlers should fail.")
+	@Target(isValid = false)
 	@StringHandler(attributeId = 1)
 	@BooleanHandler(attributeId = 1)
 	@FloatHandler(attributeId = 1)
 	private void method4(Object o) {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with just one call handler should pass.")
+	@Target(isValid = true)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method5() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with multiple call handlers should fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	@SpecificEnumHandler(attributeId = 1, ordinal = 1)
 	private void method6() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler and a value handler should fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	@StringHandler(attributeId = 1)
 	private void method7() {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with a value handler and one default should pass.")
+	@Target(isValid = true)
 	@BooleanHandler(attributeId = 1)
 	@DefaultToBoolean(false)
 	private void method8(Object o) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a value handler and multiple defaults should fail.")
+	@Target(isValid = false)
 	@StringHandler(attributeId = 1)
 	@DefaultToString("something")
 	@DefaultToStringResource(1)
 	private void method9(Object o) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a value handler and multiple defaults should fail.")
+	@Target(isValid = false)
 	@StringHandler(attributeId = 1)
 	@DefaultToString("something")
 	@DefaultToStringResource(1)
 	@DefaultToBoolean(false)
 	private void method10(Object o) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler and one default should fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	@DefaultToBoolean(false)
 	private void method11() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler and multiple defaults should fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	@DefaultToString("something")
 	@DefaultToStringResource(1)
 	private void method12() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler and multiple defaults should fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	@DefaultToString("something")
 	@DefaultToStringResource(1)
 	@DefaultToBoolean(false)
 	private void method13() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with one default and no handler should fail.")
+	@Target(isValid = false)
 	@DefaultToBoolean(true)
 	private void method14() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with multiple defaults and no handler should fail.")
+	@Target(isValid = false)
 	@DefaultToString("something")
 	@DefaultToNull
 	private void method15() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with multiple defaults and no handler should fail.")
+	@Target(isValid = false)
 	@DefaultToString("something")
 	@DefaultToNull
 	@DefaultToEnumConstant(enumClass = Enum.class, ordinal = 1)
 	private void method16() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a value handler and no arguments should fail.")
+	@Target(isValid = false)
 	@DimensionHandler(attributeId = 1)
 	private void method17() {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with a value handler and one non-use argument should " +
-					"pass.")
+	@Target(isValid = true)
 	@FloatHandler(attributeId = 1)
 	private void method18(Object o) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a value handler and one use argument should fail.")
+	@Target(isValid = false)
 	@BooleanHandler(attributeId = 1)
 	private void method19(@UseBoolean(false) Object o) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a value handler and two non-use arguments should " +
-					"fail.")
+	@Target(isValid = false)
 	@BooleanHandler(attributeId = 1)
 	private void method20(Object o1, Object o2) {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with a value handler, one non-use argument and one use " +
-					"argument should pass.")
+	@Target(isValid = true)
 	@BooleanHandler(attributeId = 1)
 	private void method21(@UseByte(1) Object o1, Object o2) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a value handler and two use arguments should fail.")
+	@Target(isValid = false)
 	@BooleanHandler(attributeId = 1)
 	private void method22(@UseByte(1) Object o1, @UseLong(1) Object o2) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a value handler, two non-use arguments and one " +
-					"use argument should fail.")
+	@Target(isValid = false)
 	@BooleanHandler(attributeId = 1)
 	private void method23(Object o1, @UseDouble(1) Object o2, Object o3) {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with a value handler, one non-use arguments and two " +
-					"use arguments should pass.")
+	@Target(isValid = true)
 	@BooleanHandler(attributeId = 1)
 	private void method24(Object o1, @UseFloat(1F) Object o2, @UseChar('A') Object o3) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a value handler and three use arguments should " +
-					"fail.")
+	@Target(isValid = false)
 	@BooleanHandler(attributeId = 1)
 	private void method25(@UseNull Object o1, @UseChar('A') Object o2, @UseInt(1) Object o3) {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with a call handler and no arguments should pass.")
+	@Target(isValid = true)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method26() {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler and one non-use argument should " +
-					"fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method27(Object o) {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with a call handler and one use argument should pass.")
+	@Target(isValid = true)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method28(@UseBoolean(false) Object o) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler and two non-use arguments should " +
-					"fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method29(Object o1, Object o2) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler, one non-use argument and one use " +
-					"argument should fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method30(@UseByte(1) Object o1, Object o2) {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with a call handler and two use arguments should pass.")
+	@Target(isValid = true)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method31(@UseByte(1) Object o1, @UseLong(1) Object o2) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler, two non-use arguments and one " +
-					"use argument should fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method32(Object o1, @UseDouble(2.0) Object o2, Object o3) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with a call handler, one non-use argument and two " +
-					"use arguments should fail.")
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method33(Object o1, @UseFloat(2F) Object o2, @UseChar('A') Object o3) {}
 
-	@Target(
-			isValid = true,
-			failureMessage = "Methods with a call handler and three use arguments should pass.")
+	@Target(isValid = true)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method34(@UseNull Object o1, @UseChar('A') Object o2, @UseInt(1) Object o3) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with one use argument but no handler should fail.")
+	@Target(isValid = false)
 	private void method35(@UseLong(1) Object o1, Object o2) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with multiple use arguments but no handler should fail.")
+	@Target(isValid = false)
 	private void method36(@UseBoolean(false) Object o1, @UseString("something") Object o2) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with multiple use annotations on a single parameter " +
-					"should fail."
-	)
+	@Target(isValid = false)
 	@BooleanHandler(attributeId = 1)
 	private void method37(@UseInt(1) @UseBoolean(true) Object o1, Object o2) {}
 
-	@Target(
-			isValid = false,
-			failureMessage = "Methods with multiple use annotations on a single parameter " +
-					"should fail."
-	)
+	@Target(isValid = false)
 	@SpecificFlagHandler(attributeId = 1, handledFlags = 1)
 	private void method38(@UseInt(1) @UseBoolean(true) @UseString("something") Object o1) {}
 }
--- processors/src/test/java/com/matthewtamlin/spyglass/processors/validation/resources/Target.java
+++ processors/src/test/java/com/matthewtamlin/spyglass/processors/validation/resources/Target.java
@@ -3,5 +3,4 @@ package com.matthewtamlin.spyglass.processors.validation.resources;
 public @interface Target {
 	boolean isValid();
 
-	String failureMessage();
 }
