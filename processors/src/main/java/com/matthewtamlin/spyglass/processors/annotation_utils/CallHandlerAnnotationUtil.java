7eee6b8426c244ab1c5e8142e3067748b64cf832
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-22 19:05:02 +0930

    Added @Tested annotation

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/CallHandlerAnnotationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/CallHandlerAnnotationUtil.java
@@ -1,5 +1,7 @@
 package com.matthewtamlin.spyglass.processors.annotation_utils;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
+
 import java.lang.annotation.Annotation;
 
 import javax.lang.model.element.Element;
@@ -7,6 +9,7 @@ import javax.lang.model.element.Element;
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.CALL_HANDLER_ANNOTATIONS;
 
+@Tested(testMethod = "automated")
 public class CallHandlerAnnotationUtil {
 	public static Annotation getCallHandlerAnnotation(final Element method) {
 		checkNotNull(method, "Argument \'method \' cannot be null.");
