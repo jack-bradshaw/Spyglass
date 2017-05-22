71e2a56dc4aedddd02e2844242c754e623cf5865
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-22 21:57:12 +0930

    Created tests for DefaultAnnotationUtil class

---
+++ processors/src/test/java/com/matthewtamlin/spyglass/processors/annotation_utils/default_annotation_util/Data.java
@@ -0,0 +1,128 @@
+package com.matthewtamlin.spyglass.processors.annotation_utils.default_annotation_util;
+
+import com.matthewtamlin.java_compiler_utilities.element_supplier.ElementId;
+import com.matthewtamlin.spyglass.annotations.call_handler_annotations.SpecificEnumHandler;
+import com.matthewtamlin.spyglass.annotations.call_handler_annotations.SpecificFlagHandler;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToBoolean;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToBooleanResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToColorResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToColorStateListResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToDimension;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToDimensionResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToDrawableResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToEnumConstant;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToFloat;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToFractionResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToInteger;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToIntegerResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToNull;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToString;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToStringResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToTextArrayResource;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToTextResource;
+import com.matthewtamlin.spyglass.annotations.units.DimensionUnit;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.BooleanHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.ColorHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.ColorStateListHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.DimensionHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.DrawableHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.EnumConstantHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.EnumOrdinalHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.FloatHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.FractionHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.IntegerHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.StringHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.TextArrayHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.TextHandler;
+
+import static com.matthewtamlin.spyglass.annotations.units.DimensionUnit.DP;
+
+public class Data {
+	@ElementId("boolean")
+	@DefaultToBoolean(true)
+	public void method1() {}
+
+	@ElementId("boolean resource")
+	@DefaultToBooleanResource(0)
+	public void method2() {}
+
+	@ElementId("color resource")
+	@DefaultToColorResource(0)
+	public void method3() {}
+
+	@ElementId("color state list resource")
+	@DefaultToColorStateListResource(0)
+	public void method4() {}
+
+	@ElementId("dimension")
+	@DefaultToDimension(value = 0, unit = DP)
+	public void method5() {}
+
+	@ElementId("dimension resource")
+	@DefaultToDimensionResource(0)
+	public void method6() {}
+
+	@ElementId("drawable resource")
+	@DefaultToDrawableResource(0)
+	public void method7() {}
+
+	@ElementId("enum constant")
+	@DefaultToEnumConstant(enumClass = PlaceholderEnum.class, ordinal = 0)
+	public void method8() {}
+
+	@ElementId("float")
+	@DefaultToFloat(0)
+	public void method9() {}
+
+	@ElementId("fraction resource")
+	@DefaultToFractionResource(resId = 0, baseMultiplier = 0, parentMultiplier = 0)
+	public void method10() {}
+
+	@ElementId("integer")
+	@DefaultToInteger(0)
+	public void method11() {}
+
+	@ElementId("integer resource")
+	@DefaultToIntegerResource(0)
+	public void method12() {}
+
+	@ElementId("null")
+	@DefaultToNull()
+	public void method13() {}
+
+	@ElementId("string")
+	@DefaultToString("hello world")
+	public void method14() {}
+
+	@ElementId("string resource")
+	@DefaultToStringResource(0)
+	public void method15() {}
+
+	@ElementId("text array resource")
+	@DefaultToTextArrayResource(0)
+	public void method16() {}
+
+	@ElementId("text resource")
+	@DefaultToTextResource(0)
+	public void method17() {}
+
+	@ElementId("no default annotation")
+	@BooleanHandler(attributeId = 0)
+	@ColorHandler(attributeId = 0)
+	@ColorStateListHandler(attributeId = 0)
+	@DimensionHandler(attributeId = 0)
+	@DrawableHandler(attributeId = 0)
+	@EnumConstantHandler(attributeId = 0, enumClass = PlaceholderEnum.class)
+	@EnumOrdinalHandler(attributeId = 0)
+	@FloatHandler(attributeId = 0)
+	@FractionHandler(attributeId = 0)
+	@IntegerHandler(attributeId = 0)
+	@StringHandler(attributeId = 0)
+	@TextArrayHandler(attributeId = 0)
+	@TextHandler(attributeId = 0)
+	@SpecificEnumHandler(attributeId = 0, ordinal = 0)
+	@SpecificFlagHandler(attributeId = 0, handledFlags = 0)
+	public void method18() {}
+
+	private enum PlaceholderEnum {}
+}
