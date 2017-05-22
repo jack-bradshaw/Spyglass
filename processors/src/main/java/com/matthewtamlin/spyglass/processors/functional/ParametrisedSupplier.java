5bee7c8cc8471ba8e8dcf6f4a24aeb2dce3ee077
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-22 12:40:19 +0930

    Reformatted Javadoc

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/functional/ParametrisedSupplier.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/functional/ParametrisedSupplier.java
@@ -3,8 +3,10 @@ package com.matthewtamlin.spyglass.processors.functional;
 /**
  * Accepts an object and returns something.
  *
- * @param <A> the type of object to accept
- * @param <R> the type of object to return
+ * @param <A>
+ * 		the type of object to accept
+ * @param <R>
+ * 		the type of object to return
  */
 public interface ParametrisedSupplier<A, R> {
 	public R supplyFor(A object);
