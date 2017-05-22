843b006519c5a68703cf3819554af2f61ecd58cd
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-22 21:58:49 +0930

    Added @Tested annotation

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/DefaultAnnotationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/DefaultAnnotationUtil.java
@@ -1,5 +1,7 @@
 package com.matthewtamlin.spyglass.processors.annotation_utils;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
+
 import java.lang.annotation.Annotation;
 
 import javax.lang.model.element.Element;
@@ -7,6 +9,7 @@ import javax.lang.model.element.Element;
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.DEFAULT_ANNOTATIONS;
 
+@Tested(testMethod = "automated")
 public class DefaultAnnotationUtil {
 	public static Annotation getDefaultAnnotation(final Element element) {
 		checkNotNull(element, "Argument \'element \' cannot be null.");
