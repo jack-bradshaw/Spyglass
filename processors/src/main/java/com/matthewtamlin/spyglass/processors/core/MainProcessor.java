474ec9fc78447a4d1f2553062005b31628a03462
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-22 14:09:37 +0930

    Updated imports for moved classes

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/core/MainProcessor.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/core/MainProcessor.java
@@ -23,7 +23,7 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 
-import static com.matthewtamlin.spyglass.processors.core.Grouper.groupByEnclosingClass;
+import static com.matthewtamlin.spyglass.processors.grouper.TypeGrouper.groupByEnclosingClass;
 import static javax.tools.Diagnostic.Kind.ERROR;
 
 public class MainProcessor extends AbstractProcessor {
