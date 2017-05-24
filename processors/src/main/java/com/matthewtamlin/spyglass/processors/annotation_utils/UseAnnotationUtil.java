e098a807c14de0e51e11f67a22d827c09cc1cf7a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-24 14:42:28 +0930

    Deleted method getUseAnnotations

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/UseAnnotationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/UseAnnotationUtil.java
@@ -1,37 +1,13 @@
 package com.matthewtamlin.spyglass.processors.annotation_utils;
 
 import java.lang.annotation.Annotation;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
-import javax.lang.model.element.Element;
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.VariableElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.USE_ANNOTATIONS;
 
 public class UseAnnotationUtil {
-	public static Map<Integer, Annotation> getUseAnnotations(final ExecutableElement element) {
-		checkNotNull(element, "Argument \'element \' cannot be null.");
-
-		final Map<Integer, Annotation> useAnnotationsByIndex = new HashMap<>();
-		final List<? extends VariableElement> params = element.getParameters();
-
-		for (int i = 0; i < params.size(); i++) {
-			for (final Class<? extends Annotation> a : USE_ANNOTATIONS) {
-				final Annotation foundAnnotation = params.get(i).getAnnotation(a);
-
-				if (foundAnnotation != null) {
-					useAnnotationsByIndex.put(i, foundAnnotation);
-				}
-			}
-		}
-
-		return useAnnotationsByIndex;
-	}
-
 	public static boolean isUseAnnotation(final Annotation anno) {
 		checkNotNull(anno, "Argument \'anno\' cannot be null.");
 
