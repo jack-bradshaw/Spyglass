6b733e11a7e2f672927d3a2cecc101ab724a432a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-24 15:27:10 +0930

    Added method getValueHandlerAnnotationMirror

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/ValueHandlerAnnotationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/ValueHandlerAnnotationUtil.java
@@ -3,16 +3,28 @@ package com.matthewtamlin.spyglass.processors.annotation_utils;
 import java.lang.annotation.Annotation;
 
 import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
-import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.DEFAULT_ANNOTATIONS;
 import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.VALUE_HANDLER_ANNOTATIONS;
 
 public class ValueHandlerAnnotationUtil {
+	public static AnnotationMirror getValueHandlerAnnotationMirror(final ExecutableElement element) {
+		checkNotNull(element, "Argument \'element\' cannot be null.");
+
+		for (final Class<? extends Annotation> annotationClass : VALUE_HANDLER_ANNOTATIONS) {
+			final AnnotationMirror mirror = AnnotationMirrorUtil.getAnnotationMirror(element, annotationClass);
+
+			if (mirror != null) {
+				return mirror;
+			}
+		}
+
+		return null;
+	}
+
 	public static boolean hasValueAnnotation(final ExecutableElement element) {
-		return getValueHandlerAnnotation(element) != null;
+		return getValueHandlerAnnotationMirror(element) != null;
 	}
 
 	private ValueHandlerAnnotationUtil() {
