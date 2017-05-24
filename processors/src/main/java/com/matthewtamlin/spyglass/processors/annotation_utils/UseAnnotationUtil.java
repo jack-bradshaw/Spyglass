c29271228410cde0b1c4c62864f96b1519d1044f
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-24 14:42:53 +0930

    Added method getUseAnnotation

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/UseAnnotationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/UseAnnotationUtil.java
@@ -1,13 +1,29 @@
 package com.matthewtamlin.spyglass.processors.annotation_utils;
 
 import java.lang.annotation.Annotation;
+import java.util.Map;
 
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.VariableElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.USE_ANNOTATIONS;
 
 public class UseAnnotationUtil {
+	public static AnnotationMirror getUseAnnotation(final VariableElement element) {
+		checkNotNull(element, "Argument \'element \' cannot be null.");
+
+		for (final Class<? extends Annotation> useAnnotationClass : USE_ANNOTATIONS) {
+			final AnnotationMirror mirror = AnnotationMirrorUtil.getAnnotationMirror(element, useAnnotationClass);
+
+			if (mirror != null) {
+				return mirror;
+			}
+		}
+
+		return null;
+	}
+
 	public static boolean isUseAnnotation(final Annotation anno) {
 		checkNotNull(anno, "Argument \'anno\' cannot be null.");
 
