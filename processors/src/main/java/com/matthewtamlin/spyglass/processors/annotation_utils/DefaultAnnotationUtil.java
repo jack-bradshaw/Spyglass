1ee0ec0fcc39049cd260b7513a35a73068151f75
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-24 15:19:49 +0930

    Added method getDefaultAnnotationMirror

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/DefaultAnnotationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/DefaultAnnotationUtil.java
@@ -4,6 +4,7 @@ import com.matthewtamlin.java_utilities.testing.Tested;
 
 import java.lang.annotation.Annotation;
 
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
@@ -11,6 +12,20 @@ import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.DEFA
 
 @Tested(testMethod = "automated")
 public class DefaultAnnotationUtil {
+	public static AnnotationMirror getDefaultAnnotationMirror(final Element element) {
+		checkNotNull(element, "Argument \'element \' cannot be null.");
+
+		for (final Class<? extends Annotation> annotationClass : DEFAULT_ANNOTATIONS) {
+			final AnnotationMirror mirror = AnnotationMirrorUtil.getAnnotationMirror(element, annotationClass);
+
+			if (mirror != null) {
+				return mirror;
+			}
+		}
+
+		return null;
+	}
+
 	public static boolean hasDefaultAnnotation(final Element element) {
 		checkNotNull(element, "Argument \'element \' cannot be null.");
 
