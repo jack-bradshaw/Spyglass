c4c0b3b61f739ce8924c836c5136dbed49ee9128
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-24 15:25:43 +0930

    Changed parameter types

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/ValueHandlerAnnotationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/ValueHandlerAnnotationUtil.java
@@ -3,12 +3,13 @@ package com.matthewtamlin.spyglass.processors.annotation_utils;
 import java.lang.annotation.Annotation;
 
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.VALUE_HANDLER_ANNOTATIONS;
 
 public class ValueHandlerAnnotationUtil {
-	public static Annotation getValueHandlerAnnotation(final Element element) {
+	public static Annotation getValueHandlerAnnotation(final ExecutableElement element) {
 		checkNotNull(element, "Argument \'element\' cannot be null.");
 
 		for (final Class<? extends Annotation> a : VALUE_HANDLER_ANNOTATIONS) {
@@ -20,7 +21,7 @@ public class ValueHandlerAnnotationUtil {
 		return null;
 	}
 
-	public static boolean hasValueAnnotation(final Element element) {
+	public static boolean hasValueAnnotation(final ExecutableElement element) {
 		for (final Class<? extends Annotation> a : VALUE_HANDLER_ANNOTATIONS) {
 			if (element.getAnnotation(a) != null) {
 				return true;
