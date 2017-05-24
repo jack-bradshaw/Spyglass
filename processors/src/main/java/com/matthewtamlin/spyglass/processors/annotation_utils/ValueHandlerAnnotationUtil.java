2b6a12943940b9a3cb7a4edece4ea7aaed4fdde3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-24 15:26:35 +0930

    Deleted method getValueHandlerAnnotation

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/ValueHandlerAnnotationUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/annotation_utils/ValueHandlerAnnotationUtil.java
@@ -2,25 +2,15 @@ package com.matthewtamlin.spyglass.processors.annotation_utils;
 
 import java.lang.annotation.Annotation;
 
+import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.DEFAULT_ANNOTATIONS;
 import static com.matthewtamlin.spyglass.processors.core.AnnotationRegistry.VALUE_HANDLER_ANNOTATIONS;
 
 public class ValueHandlerAnnotationUtil {
-	public static Annotation getValueHandlerAnnotation(final ExecutableElement element) {
-		checkNotNull(element, "Argument \'element\' cannot be null.");
-
-		for (final Class<? extends Annotation> a : VALUE_HANDLER_ANNOTATIONS) {
-			if (element.getAnnotation(a) != null) {
-				return element.getAnnotation(a);
-			}
-		}
-
-		return null;
-	}
-
 	public static boolean hasValueAnnotation(final ExecutableElement element) {
 		return getValueHandlerAnnotation(element) != null;
 	}
