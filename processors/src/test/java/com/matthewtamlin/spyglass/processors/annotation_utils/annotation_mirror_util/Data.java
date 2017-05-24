ea89853ac77dd2f7cf395c95755db8b57af14dac
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-24 20:14:56 +0930

    Added new fields

--- processors/src/test/java/com/matthewtamlin/spyglass/processors/annotation_utils/annotation_mirror_util/Data.java
+++ processors/src/test/java/com/matthewtamlin/spyglass/processors/annotation_utils/annotation_mirror_util/Data.java
@@ -17,4 +17,12 @@ public class Data {
 	@ElementId("get annotation value, value provided")
 	@AnnotationWithValues(value ="specified value")
 	public Object field5;
+
+	@ElementId("get annotation value with defaults: no value provided")
+	@AnnotationWithValues()
+	public Object field6;
+
+	@ElementId("get annotation value with defaults: value provided")
+	@AnnotationWithValues(value ="specified value")
+	public Object field7;
 }
