84ffc3ae3c7cb2b4dc0c7552256f937284950683
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-25 14:04:15 +0930

    Created tests for TypeElementWrapper

---
+++ processors/src/test/java/com/matthewtamlin/spyglass/processors/grouper/type_element_wrapper/Data.java
@@ -0,0 +1,11 @@
+package com.matthewtamlin.spyglass.processors.grouper.type_element_wrapper;
+
+import com.matthewtamlin.java_compiler_utilities.element_supplier.ElementId;
+
+public class Data {
+	@ElementId("class 1")
+	public static class Class1 {}
+
+	@ElementId("class 2")
+	public static class Class2 {}
+}
