08a8b82748f6052e81e5c7d842a5193968cdc03a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-26 11:48:28 +0930

    Generalised literal generation

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/InvocationLiteralGenerator.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/InvocationLiteralGenerator.java
@@ -1,28 +1,14 @@
 package com.matthewtamlin.spyglass.processors.code_generation;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseBoolean;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseByte;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseChar;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseDouble;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseFloat;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseInt;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseLong;
 import com.matthewtamlin.spyglass.annotations.use_annotations.UseNull;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseShort;
-import com.matthewtamlin.spyglass.annotations.use_annotations.UseString;
 import com.matthewtamlin.spyglass.processors.annotation_utils.AnnotationMirrorUtil;
 import com.matthewtamlin.spyglass.processors.annotation_utils.UseAnnotationUtil;
-import com.matthewtamlin.spyglass.processors.functional.ParametrisedSupplier;
-import com.squareup.javapoet.CodeBlock;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.util.Elements;
@@ -31,160 +17,7 @@ import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull
 
 @Tested(testMethod = "automated")
 public class InvocationLiteralGenerator {
-	private final Map<String, ParametrisedSupplier<AnnotationMirror, String>> argLiteralSuppliers = new HashMap<>();
-
 	private Elements elementsUtil;
-
-	{
-		argLiteralSuppliers.put(
-				UseBoolean.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						return value.toString();
-					}
-				});
-
-		argLiteralSuppliers.put(
-				UseByte.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						return "(byte)" + value.toString();
-					}
-				}
-		);
-
-		argLiteralSuppliers.put(
-				UseChar.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						return "(char)" + value.toString();
-					}
-				}
-		);
-
-		argLiteralSuppliers.put(
-				UseDouble.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						return value.toString();
-					}
-				}
-		);
-
-		argLiteralSuppliers.put(
-				UseFloat.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						return value.toString();
-					}
-				}
-		);
-
-		argLiteralSuppliers.put(
-				UseInt.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						return value.toString();
-					}
-				}
-		);
-
-		argLiteralSuppliers.put(
-				UseLong.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						return value.toString();
-					}
-				}
-		);
-
-		argLiteralSuppliers.put(
-				UseNull.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						return "null";
-					}
-				}
-		);
-
-		argLiteralSuppliers.put(
-				UseShort.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						return "(short)" + value.toString();
-					}
-				}
-		);
-
-		argLiteralSuppliers.put(
-				UseString.class.getName(),
-				new ParametrisedSupplier<AnnotationMirror, String>() {
-					@Override
-					public String supplyFor(final AnnotationMirror object) {
-						final AnnotationValue value = AnnotationMirrorUtil.getAnnotationValueWithDefaults(
-								object,
-								"value",
-								elementsUtil);
-
-						// Use the String escaping tools of the JavaPoet library
-						return CodeBlock
-								.builder()
-								.add("$S", value.toString())
-								.build()
-								.toString();
-					}
-				}
-		);
-	}
 
 	public InvocationLiteralGenerator(final Elements elementsUtil) {
 		this.elementsUtil = checkNotNull(elementsUtil, "Argument \'elementsUtil\' cannot be null.");
@@ -221,12 +54,20 @@ public class InvocationLiteralGenerator {
 				argLiterals.add(null);
 
 			} else {
-				final String annotationTypeName = useAnnotationMirror.getAnnotationType().toString();
-				argLiterals.add(argLiteralSuppliers.get(annotationTypeName).supplyFor(useAnnotationMirror));
+				argLiterals.add(getLiteralFromAnnotationMirror(useAnnotationMirror));
 			}
 		}
 
 		return argLiterals;
+	}
+
+	private String getLiteralFromAnnotationMirror(final AnnotationMirror mirror) {
+		if (mirror.getAnnotationType().toString().equals(UseNull.class.getName())) {
+			return "null";
+		} else {
+			// the toString method of AnnotationValue produces a source code representation
+			return AnnotationMirrorUtil.getAnnotationValueWithDefaults(mirror, "value", elementsUtil).toString();
+		}
 	}
 
 	private static String listToCommaSeparatedString(final List<String> list) {
