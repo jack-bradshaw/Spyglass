b1117b5b1de0ce462d815758913cd7724648f1bb
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-28 18:33:31 +0930

    Added CONTEXT_COMPAT constant

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/AndroidClassNames.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/AndroidClassNames.java
@@ -6,4 +6,6 @@ public class AndroidClassNames {
 	public static final ClassName CONTEXT = ClassName.get("android.content", "Context");
 
 	public static final ClassName TYPED_ARRAY = ClassName.get("android.content.res", "TypedArray");
+
+	public static final ClassName CONTEXT_COMPAT = ClassName.get("android.support.v4.content", "ContextCompat");
 }
