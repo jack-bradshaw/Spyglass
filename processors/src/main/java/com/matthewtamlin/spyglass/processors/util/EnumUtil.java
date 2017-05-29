a35fb9fd81b70bcf151aef690f47f776f7989d78
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-05-29 10:24:45 +0930

    Refactoring

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/util/EnumUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/util/EnumUtil.java
@@ -15,8 +15,10 @@ public class EnumUtil {
 		final T[] constants = clazz.getEnumConstants();
 
 		if (ordinal < 0 || ordinal > constants.length - 1) {
-			final String message = "Ordinal must be between 0 and %1$s for enum class %2$s.";
-			throw new IllegalArgumentException(String.format(message, constants.length - 1, clazz));
+			throw new IllegalArgumentException(String.format(
+					"Ordinal must be between 0 and %1$s for enum class %2$s.",
+					constants.length - 1,
+					clazz));
 		}
 
 		return constants[ordinal];
