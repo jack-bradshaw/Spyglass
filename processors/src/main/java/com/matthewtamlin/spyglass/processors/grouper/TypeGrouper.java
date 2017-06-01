4d932df28ead2d685fc898826eee87204cce34e3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-01 14:00:14 +0930

    Externalised method call
    - Deleted method getEnclosingClass
    - Changed calls to getEnclosingClass to use TypeUtil.getEnclosingType() instead

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/grouper/TypeGrouper.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/grouper/TypeGrouper.java
@@ -1,6 +1,7 @@
 package com.matthewtamlin.spyglass.processors.grouper;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
+import com.matthewtamlin.spyglass.processors.util.TypeUtil;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -8,11 +9,9 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.lang.model.element.Element;
-import javax.lang.model.element.TypeElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkEachElementIsNotNull;
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
-import static javax.lang.model.element.ElementKind.CLASS;
 
 @Tested(testMethod = "automated")
 public class TypeGrouper {
@@ -23,7 +22,7 @@ public class TypeGrouper {
 		final Map<TypeElementWrapper, Set<T>> groups = new HashMap<>();
 
 		for (final T element : elements) {
-			final TypeElementWrapper parentWrapper = new TypeElementWrapper(getEnclosingClass(element));
+			final TypeElementWrapper parentWrapper = new TypeElementWrapper(TypeUtil.getEnclosingType(element));
 
 			if (!groups.containsKey(parentWrapper)) {
 				groups.put(parentWrapper, new HashSet<T>());
@@ -33,38 +32,6 @@ public class TypeGrouper {
 		}
 
 		return groups;
-	}
-
-	/**
-	 * Recursively searches up the element model until the enclosing class is found or the top of
-	 * the model is reached. The enclosing class may be an anonymous inner class, a nested class,
-	 * or a top level class. If the element has no enclosing class, null is returned.
-	 *
-	 * @param element
-	 * 		the element to find the enclosing class of, not null
-	 *
-	 * @return the enclosing class as an element, null if none exists
-	 *
-	 * @throws IllegalArgumentException
-	 * 		if {@code element} is null
-	 */
-	private static TypeElement getEnclosingClass(final Element element) {
-		checkNotNull(element, "Argument \'element\' cannot be null.");
-
-		final Element enclosingElement = element.getEnclosingElement();
-
-		if (enclosingElement == null) {
-			// No enclosing class exists
-			return null;
-
-		} else if (enclosingElement.getKind() == CLASS) {
-			// The enclosing class has been found
-			return (TypeElement) enclosingElement;
-
-		} else {
-			// Keep searching up
-			return getEnclosingClass(enclosingElement);
-		}
 	}
 
 	private TypeGrouper() {
