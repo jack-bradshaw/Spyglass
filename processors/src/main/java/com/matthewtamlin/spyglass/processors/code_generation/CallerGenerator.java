d9e1cad00d84f69236dc640812b935d55fc36c12
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-02 15:39:00 +0930

    Made method generateCallerSpec public

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/CallerGenerator.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/code_generation/CallerGenerator.java
@@ -40,7 +40,7 @@ public class CallerGenerator {
 		invocationLiteralGenerator = new InvocationLiteralGenerator(elementUtil);
 	}
 
-	private TypeSpec generateCallerSpec(final ExecutableElement method) {
+	public TypeSpec generateCallerSpec(final ExecutableElement method) {
 		if (hasCallHandlerAnnotation(method)) {
 			return generateCallerForCallHandlerCase(method);
 
