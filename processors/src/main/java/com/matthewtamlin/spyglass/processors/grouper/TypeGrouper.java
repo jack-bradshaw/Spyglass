425cb2c81e08c966c995cf7b890e34e9ea9863a1
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-03 12:14:58 +0930

    Added exception throw if not child of TypeElement

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/grouper/TypeGrouper.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/grouper/TypeGrouper.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkEachElementIsNotNull;
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
@@ -22,13 +23,20 @@ public class TypeGrouper {
 		final Map<TypeElementWrapper, Set<T>> groups = new HashMap<>();
 
 		for (final T element : elements) {
-			final TypeElementWrapper parentWrapper = new TypeElementWrapper(TypeUtil.getEnclosingClass(element));
+			final Element parent = element.getEnclosingElement();
 
-			if (!groups.containsKey(parentWrapper)) {
-				groups.put(parentWrapper, new HashSet<T>());
+			if (parent instanceof TypeElement) {
+				final TypeElementWrapper parentWrapper = new TypeElementWrapper((TypeElement) parent);
+
+				if (!groups.containsKey(parentWrapper)) {
+					groups.put(parentWrapper, new HashSet<T>());
+				}
+
+				groups.get(parentWrapper).add(element);
+			} else {
+				throw new IllegalArgumentException("Argument \'elements\' contains an element which is not the " +
+						"immediate child of a TypeElement.");
 			}
-
-			groups.get(parentWrapper).add(element);
 		}
 
 		return groups;
