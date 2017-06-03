fae68e7a3c934e471cd8b925f52c1dcb6a593b9a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-03 14:17:05 +0930

    Created tests for SetUtil class

---
+++ processors/src/test/java/com/matthewtamlin/spyglass/processors/util/TestSetUtil.java
@@ -0,0 +1,25 @@
+package com.matthewtamlin.spyglass.processors.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(JUnit4.class)
+public class TestSetUtil {
+	@Test
+	public void testImmutableSetOf_combinationOfValues() {
+		final Set<Object> set = SetUtil.<Object>immutableSetOf(null, null, "hello", "world", "world", "world", 1);
+
+		assertThat(set, hasSize(4));
+		assertThat(set.contains(null), is(true));
+		assertThat(set.contains("hello"), is(true));
+		assertThat(set.contains("world"), is(true));
+		assertThat(set.contains(1), is(true));
+	}
+}
