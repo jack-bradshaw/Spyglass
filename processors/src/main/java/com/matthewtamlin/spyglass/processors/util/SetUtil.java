9f4a27ca283c457adb09a593440ce22474b1b637
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-03 14:17:32 +0930

    Added @Tested annotation to class

--- processors/src/main/java/com/matthewtamlin/spyglass/processors/util/SetUtil.java
+++ processors/src/main/java/com/matthewtamlin/spyglass/processors/util/SetUtil.java
@@ -1,9 +1,12 @@
 package com.matthewtamlin.spyglass.processors.util;
 
+import com.matthewtamlin.java_utilities.testing.Tested;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+@Tested(testMethod = "automated")
 public class SetUtil {
 	@SafeVarargs
 	public static <T> Set<T> immutableSetOf(T... objects) {
