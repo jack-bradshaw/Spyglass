405ecffe795ef5152d73d53d3e33daf7e5f097c1
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-06 19:08:23 +0930

    Created tests for CallerGenerator class

---
+++ processor/src/test/java/com/matthewtamlin/spyglass/processors/code_generation/caller_generator/Data.java
@@ -0,0 +1,25 @@
+package com.matthewtamlin.spyglass.processors.code_generation.caller_generator;
+
+import com.matthewtamlin.avatar.element_supplier.ElementId;
+import com.matthewtamlin.spyglass.annotations.call_handler_annotations.SpecificEnumHandler;
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToBoolean;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.BooleanHandler;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.StringHandler;
+
+public class Data {
+	@ElementId("no handler")
+	public void noHandler() {}
+
+	@ElementId("value handler with default")
+	@BooleanHandler(attributeId = 1)
+	@DefaultToBoolean(false)
+	public void valueHandlerAndDefault(boolean b) {}
+
+	@ElementId("value handler no default")
+	@StringHandler(attributeId = 1)
+	public void valueHandlerOnly(String s) {}
+
+	@ElementId("call handler")
+	@SpecificEnumHandler(attributeId = 1, ordinal = 1)
+	public void callHandler() {}
+}
