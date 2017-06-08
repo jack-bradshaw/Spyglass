9c42fc5d7a5abc57259da948ea4f44a75cd44674
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-08 17:24:32 +0930

    Deleted TestSpyglassBuilder

--- consumer tests/src/androidTest/java/com/matthewtamlin/spyglass/consumer_tests/TestSpyglassBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.matthewtamlin.spyglass.consumer_tests;
-
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.runner.RunWith;
-
-@RunWith(AndroidJUnit4.class)
-public class TestSpyglassBuilder {}
