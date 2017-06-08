6c07f4b18e3bea9148263607d8ba91e7c0d9fe94
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-08 20:25:55 +0930

    Added method activateCallersWasInvoked

--- consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/HasNormalCompanion_SpyglassCompanion.java
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/HasNormalCompanion_SpyglassCompanion.java
@@ -1,7 +1,13 @@
 package com.matthewtamlin.spyglass.consumer_tests;
 
 public class HasNormalCompanion_SpyglassCompanion {
-	public static void activateCallers() {
+	private static boolean activateCallersWasInvoked = false;
 
+	public static void activateCallers() {
+		activateCallersWasInvoked = true;
+	}
+
+	public static boolean activateCallersWasInvoked() {
+		return activateCallersWasInvoked;
 	}
 }
