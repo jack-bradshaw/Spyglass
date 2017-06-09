c0bccad534999e5777605c9d77646a62093b7424
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-09 09:46:18 +0930

    Created invalid views and companions

---
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithExceptionThrowingCompanion.java
@@ -0,0 +1,10 @@
+package com.matthewtamlin.spyglass.consumer_tests;
+
+import android.content.Context;
+import android.view.View;
+
+public class ViewWithExceptionThrowingCompanion extends View {
+	public ViewWithExceptionThrowingCompanion(final Context context) {
+		super(context);
+	}
+}
---
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithIncompleteCompanion.java
@@ -0,0 +1,10 @@
+package com.matthewtamlin.spyglass.consumer_tests;
+
+import android.content.Context;
+import android.view.View;
+
+public class ViewWithIncompleteCompanion extends View {
+	public ViewWithIncompleteCompanion(final Context context) {
+		super(context);
+	}
+}
