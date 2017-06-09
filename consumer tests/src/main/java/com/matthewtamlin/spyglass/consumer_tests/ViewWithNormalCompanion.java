ce85cc29455395f1f79c117e4f95c03a986f7ced
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-09 09:47:57 +0930

    Renamed view and companion

--- consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithNormalCompanion.java
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithNormalCompanion.java
@@ -3,8 +3,8 @@ package com.matthewtamlin.spyglass.consumer_tests;
 import android.content.Context;
 import android.view.View;
 
-public class HasNormalCompanion extends View {
-	public HasNormalCompanion(final Context context) {
+public class ViewWithNormalCompanion extends View {
+	public ViewWithNormalCompanion(final Context context) {
 		super(context);
 	}
 }
--- consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithNormalCompanion_SpyglassCompanion.java
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithNormalCompanion_SpyglassCompanion.java
@@ -3,10 +3,10 @@ package com.matthewtamlin.spyglass.consumer_tests;
 import android.content.Context;
 import android.content.res.TypedArray;
 
-public class HasNormalCompanion_SpyglassCompanion {
+public class ViewWithNormalCompanion_SpyglassCompanion {
 	private static boolean activateCallersWasInvoked = false;
 
-	public static void activateCallers(final HasNormalCompanion target, final Context context, final TypedArray attrs) {
+	public static void activateCallers(final ViewWithNormalCompanion target, final Context context, final TypedArray attrs) {
 		activateCallersWasInvoked = true;
 	}
 
