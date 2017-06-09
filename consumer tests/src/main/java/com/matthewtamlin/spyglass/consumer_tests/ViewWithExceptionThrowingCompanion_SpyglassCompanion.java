c956676ad2fec7d5ce77579387a26ed99680639a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-09 15:00:50 +0930

    Renamed incorrectly named file

--- consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithExceptionThrowingCompanion_SpyglassCompanion.java
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithExceptionThrowingCompanion_SpyglassCompanion.java
@@ -3,7 +3,7 @@ package com.matthewtamlin.spyglass.consumer_tests;
 import android.content.Context;
 import android.content.res.TypedArray;
 
-public class ViewWithExceptionThrowningCompanion_SpyglassCompanion {
+public class ViewWithExceptionThrowingCompanion_SpyglassCompanion {
 	public void activateCallers(
 			final ViewWithExceptionThrowingCompanion target,
 			final Context context,
