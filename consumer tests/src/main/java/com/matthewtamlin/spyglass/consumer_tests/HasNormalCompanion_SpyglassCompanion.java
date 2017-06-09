dce3701d8d64fa131e62721d4dc31dc657b0903c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-09 09:47:04 +0930

    Added parameters to method

--- consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/HasNormalCompanion_SpyglassCompanion.java
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/HasNormalCompanion_SpyglassCompanion.java
@@ -1,9 +1,12 @@
 package com.matthewtamlin.spyglass.consumer_tests;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+
 public class HasNormalCompanion_SpyglassCompanion {
 	private static boolean activateCallersWasInvoked = false;
 
-	public static void activateCallers() {
+	public static void activateCallers(final HasNormalCompanion target, final Context context, final TypedArray attrs) {
 		activateCallersWasInvoked = true;
 	}
 
