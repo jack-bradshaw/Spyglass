bd42b9732238dba95b2425e196d2eb05ca41c7d3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-10 13:01:29 +0930

    Created SubclassOfViewWithNormalCompanion

---
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/SubclassOfViewWithNormalCompanion.java
@@ -0,0 +1,9 @@
+package com.matthewtamlin.spyglass.consumer_tests;
+
+import android.content.Context;
+
+public class SubclassOfViewWithNormalCompanion extends ViewWithNormalCompanion {
+	public SubclassOfViewWithNormalCompanion(final Context context) {
+		super(context);
+	}
+}
