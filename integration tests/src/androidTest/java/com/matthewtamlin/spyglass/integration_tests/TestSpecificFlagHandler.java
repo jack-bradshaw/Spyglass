bca3d6d407e647439b7f890a065e8e5f3c1dbfa9
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-14 14:56:47 +0930

    Created tests for SpecificFlagHandler annotation

---
+++ integration tests/src/androidTest/java/com/matthewtamlin/spyglass/integration_tests/TestSpecificFlagHandler.java
@@ -0,0 +1,87 @@
+package com.matthewtamlin.spyglass.integration_tests;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.rule.UiThreadTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.AttributeSet;
+
+import com.matthewtamlin.spyglass.integration_tests.testing_utilities.AttributeSetSupplier;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(AndroidJUnit4.class)
+public class TestSpecificFlagHandler {
+	@Rule
+	public final UiThreadTestRule uiThreadTestRule = new UiThreadTestRule();
+
+	private Context context;
+
+	@Before
+	public void setup() {
+		context = InstrumentationRegistry.getTargetContext();
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassCallsMethod_attributePresentWithMatchingFlagOnly() {
+		final AttributeSet attrs = AttributeSetSupplier.fromXml(
+				context,
+				R.xml.specific_flag_handler_with_flag_1);
+
+		final SpecificFlagHandlerTestTarget target = new SpecificFlagHandlerTestTarget(context, attrs);
+
+		assertThat(target.wasHandlerCalled(), is(true));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassCallsMethod_attributePresentWithMatchingFlagAndOthers() {
+		final AttributeSet attrs = AttributeSetSupplier.fromXml(
+				context,
+				R.xml.specific_flag_handler_with_both_flags);
+
+		final SpecificFlagHandlerTestTarget target = new SpecificFlagHandlerTestTarget(context, attrs);
+
+		assertThat(target.wasHandlerCalled(), is(true));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassNeverCallsMethod_attributePresentWithoutMatchingFlag() {
+		final AttributeSet attrs = AttributeSetSupplier.fromXml(
+				context,
+				R.xml.specific_flag_handler_with_flag_2);
+
+		final SpecificFlagHandlerTestTarget target = new SpecificFlagHandlerTestTarget(context, attrs);
+
+		assertThat(target.wasHandlerCalled(), is(false));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassNeverCallsMethod_attributeMissing() {
+		final AttributeSet attrs = AttributeSetSupplier.fromXml(
+				context,
+				R.xml.specific_enum_handler_without_attr);
+
+		final SpecificFlagHandlerTestTarget target = new SpecificFlagHandlerTestTarget(context, attrs);
+
+		assertThat(target.wasHandlerCalled(), is(false));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassNeverCallsMethod_noAttributesSupplied() {
+		final SpecificFlagHandlerTestTarget target = new SpecificFlagHandlerTestTarget(context);
+
+		assertThat(target.wasHandlerCalled(), is(false));
+	}
+}
