270f43b6cf8e0b79ccf2db24b888c6c460e86b3c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-16 11:35:01 +0930

    Created tests for DimensionHandler annotation

---
+++ integration tests/src/androidTest/java/com/matthewtamlin/spyglass/integration_tests/TestDimensionHandler.java
@@ -0,0 +1,250 @@
+package com.matthewtamlin.spyglass.integration_tests;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.rule.UiThreadTestRule;
+import android.util.AttributeSet;
+
+import com.matthewtamlin.android_utilities.library.helpers.DimensionHelper;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.DimensionHandlerTestTargetBase;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToDpDimension;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToDpDimensionResource;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToInDimension;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToInDimensionResource;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToMmDimension;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToMmDimensionResource;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToPtDimension;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToPtDimensionResource;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToPxDimension;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToPxDimensionResource;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToSpDimension;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithDefaultToSpDimensionResource;
+import com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target.WithNoDefault;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.matthewtamlin.spyglass.integration_tests.testing_utilities.AttributeSetSupplier.fromXml;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class TestDimensionHandler {
+	@Rule
+	public final UiThreadTestRule uiThreadTestRule = new UiThreadTestRule();
+
+	private Context context;
+
+	@Before
+	public void setup() {
+		context = InstrumentationRegistry.getTargetContext();
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributePresent_attributeEquals10dp() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_with_attr_equals_10dp);
+
+		final DimensionHandlerTestTargetBase target = new WithNoDefault(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.dpToPx(context, 10);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributePresent_attributeEquals10in() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_with_attr_equals_10in);
+
+		final DimensionHandlerTestTargetBase target = new WithNoDefault(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.inToPx(context, 10);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributePresent_attributeEquals10mm() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_with_attr_equals_10mm);
+
+		final DimensionHandlerTestTargetBase target = new WithNoDefault(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.mmToPx(context, 10);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributePresent_attributeEquals10pt() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_with_attr_equals_10pt);
+
+		final DimensionHandlerTestTargetBase target = new WithNoDefault(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.ptToPx(context, 10);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributePresent_attributeEquals10px() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_with_attr_equals_10px);
+
+		final DimensionHandlerTestTargetBase target = new WithNoDefault(context, attrs);
+
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(10)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributePresent_attributeEquals10sp() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_with_attr_equals_10sp);
+
+		final DimensionHandlerTestTargetBase target = new WithNoDefault(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.spToPx(context, 10);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassNeverCallsMethod_attributeMissing_noDefaultPresent() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithNoDefault(context, attrs);
+
+		assertThat(target.getReceivedValue(), is(ReceivedValue.<Integer>none()));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionPresent_usingDpUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToDpDimension(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.dpToPx(context, WithDefaultToDpDimension.DEFAULT_VALUE_DP);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionPresent_usingInUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToInDimension(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.inToPx(context, WithDefaultToInDimension.DEFAULT_VALUE_IN);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionPresent_usingMmUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToMmDimension(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.mmToPx(context, WithDefaultToMmDimension.DEFAULT_VALUE_MM);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionPresent_usingPtUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToPtDimension(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.ptToPx(context, WithDefaultToPtDimension.DEFAULT_VALUE_PT);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionPresent_usingPxUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToPxDimension(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.dpToPx(context, WithDefaultToPxDimension.DEFAULT_VALUE_PX);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionPresent_usingSpUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToSpDimension(context, attrs);
+
+		final int expectedValue = (int) DimensionHelper.spToPx(context, WithDefaultToSpDimension.DEFAULT_VALUE_SP);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionResourcePresent_usingDpUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToDpDimensionResource(context, attrs);
+
+		final int expectedValue = (int) context.getResources().getDimension(R.dimen.DimensionForTestingDp);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionResourcePresent_usingInUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToInDimensionResource(context, attrs);
+
+		final int expectedValue = (int) context.getResources().getDimension(R.dimen.DimensionForTestingIn);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionResourcePresent_usingMmUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToMmDimensionResource(context, attrs);
+
+		final int expectedValue = (int) context.getResources().getDimension(R.dimen.DimensionForTestingMm);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionResourcePresent_usingPtUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToPtDimensionResource(context, attrs);
+
+		final int expectedValue = (int) context.getResources().getDimension(R.dimen.DimensionForTestingPt);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionResourcePresent_usingPxUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToPxDimensionResource(context, attrs);
+
+		final int expectedValue = (int) context.getResources().getDimension(R.dimen.DimensionForTestingPx);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+
+	@Test
+	@UiThreadTest
+	public void testSpyglassPassesCorrectData_attributeMissing_defaultToDimensionResourcePresent_usingSpUnits() {
+		final AttributeSet attrs = fromXml(context, R.xml.dimension_handler_without_attr);
+
+		final DimensionHandlerTestTargetBase target = new WithDefaultToSpDimensionResource(context, attrs);
+
+		final int expectedValue = (int) context.getResources().getDimension(R.dimen.DimensionForTestingSp);
+		assertThat(target.getReceivedValue(), is(ReceivedValue.of(expectedValue)));
+	}
+}
---
+++ integration tests/src/main/java/com/matthewtamlin/spyglass/integration_tests/dimension_handler_test_target/WithDefaultToPxDimension.java
@@ -0,0 +1,63 @@
+package com.matthewtamlin.spyglass.integration_tests.dimension_handler_test_target;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+
+import com.matthewtamlin.spyglass.annotations.default_annotations.DefaultToDimension;
+import com.matthewtamlin.spyglass.annotations.units.DimensionUnit;
+import com.matthewtamlin.spyglass.annotations.value_handler_annotations.DimensionHandler;
+import com.matthewtamlin.spyglass.consumer.Spyglass;
+import com.matthewtamlin.spyglass.integration_tests.R;
+import com.matthewtamlin.spyglass.integration_tests.ReceivedValue;
+
+public class WithDefaultToPxDimension extends DimensionHandlerTestTargetBase {
+	public static final int DEFAULT_VALUE_PX = 44;
+
+	public WithDefaultToPxDimension(final Context context) {
+		super(context);
+		init(null, 0, 0);
+	}
+
+	public WithDefaultToPxDimension(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs, 0, 0);
+	}
+
+	public WithDefaultToPxDimension(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init(attrs, defStyleAttr, 0);
+	}
+
+	@TargetApi(21)
+	@RequiresApi(21)
+	public WithDefaultToPxDimension(
+			final Context context,
+			final AttributeSet attrs,
+			final int defStyleAttr,
+			final int defStyleRes) {
+
+		super(context, attrs, defStyleAttr, defStyleRes);
+		init(attrs, defStyleAttr, defStyleRes);
+	}
+
+	@DimensionHandler(attributeId = R.styleable.DimensionHandlerTestTargetBase_dimensionHandlerAttr)
+	@DefaultToDimension(value = DEFAULT_VALUE_PX, unit = DimensionUnit.PX)
+	public void handlerMethod(final int i) {
+		setReceivedValue(ReceivedValue.of(i));
+	}
+
+	private void init(final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
+		Spyglass.builder()
+				.withTarget(this)
+				.withAnnotationSource(WithDefaultToPxDimension.class)
+				.withContext(getContext())
+				.withAttributeSet(attrs)
+				.withDefStyleAttr(defStyleAttr)
+				.withDefStyleRes(defStyleRes)
+				.withStyleableResource(R.styleable.DimensionHandlerTestTargetBase)
+				.build()
+				.passDataToMethods();
+	}
+}
