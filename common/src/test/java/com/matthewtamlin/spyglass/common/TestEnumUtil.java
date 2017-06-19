375f485a4f1a1f91e27974acc1d44e4ee4458f61
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-06-19 12:29:01 +0930

    Deleted unnecessary throws declarations

--- common/src/test/java/com/matthewtamlin/spyglass/common/TestEnumUtil.java
+++ common/src/test/java/com/matthewtamlin/spyglass/common/TestEnumUtil.java
@@ -41,54 +41,52 @@ public class TestEnumUtil {
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testGetEnumConstant_classnameAndOrdinalVariant_nullClassname() throws ClassNotFoundException {
+	public void testGetEnumConstant_classnameAndOrdinalVariant_nullClassname() {
 		EnumUtil.getEnumConstant((String) null, 0);
 	}
 
 	@Test(expected = EnumInstantiationException.class)
-	public void testGetEnumConstant_classnameAndOrdinalVariant_classDoesNotExist() throws ClassNotFoundException {
+	public void testGetEnumConstant_classnameAndOrdinalVariant_classDoesNotExist() {
 		EnumUtil.getEnumConstant("not.a.class", 0);
 	}
 
 	@Test(expected = EnumInstantiationException.class)
-	public void testGetEnumConstant_classnameAndOrdinalVariant_classIsNotAnEnum() throws ClassNotFoundException {
+	public void testGetEnumConstant_classnameAndOrdinalVariant_classIsNotAnEnum() {
 		EnumUtil.getEnumConstant(Object.class.getCanonicalName(), 0);
 	}
 
 	@Test(expected = EnumInstantiationException.class)
-	public void testGetEnumConstant_classnameAndOrdinalVariant_negativeOrdinal() throws ClassNotFoundException {
+	public void testGetEnumConstant_classnameAndOrdinalVariant_negativeOrdinal() {
 		EnumUtil.getEnumConstant(Vehicle.class.getCanonicalName(), -1);
 	}
 
 	@Test(expected = EnumInstantiationException.class)
-	public void testGetEnumConstant_classnameAndOrdinalVariant_excessiveOrdinal() throws ClassNotFoundException {
+	public void testGetEnumConstant_classnameAndOrdinalVariant_excessiveOrdinal() {
 		EnumUtil.getEnumConstant(Vehicle.class.getCanonicalName(), Vehicle.values().length);
 	}
 
 	@Test
-	public void testGetEnumConstant_classnameAndOrdinalVariant_validClassAndOrdinal() throws ClassNotFoundException {
+	public void testGetEnumConstant_classnameAndOrdinalVariant_validClassAndOrdinal() {
 		EnumUtil.getEnumConstant(Vehicle.class.getCanonicalName(), 0);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void testGetEnumConstant_fullyQualifiedConstantNameVariant_constantIsNull() throws ClassNotFoundException {
+	public void testGetEnumConstant_fullyQualifiedConstantNameVariant_constantIsNull() {
 		EnumUtil.getEnumConstant(null);
 	}
 
 	@Test(expected = EnumInstantiationException.class)
-	public void testGetEnumConstant_fullyQualifiedConstantNameVariant_classDoesNotExist()
-			throws ClassNotFoundException {
-
+	public void testGetEnumConstant_fullyQualifiedConstantNameVariant_classDoesNotExist() {
 		EnumUtil.getEnumConstant("not.a.class");
 	}
 
 	@Test(expected = EnumInstantiationException.class)
-	public void testGetEnumConstant_fullyQualifiedConstantNameVariant_classIsNotAnEnum() throws ClassNotFoundException {
+	public void testGetEnumConstant_fullyQualifiedConstantNameVariant_classIsNotAnEnum() {
 		EnumUtil.getEnumConstant(String.class.getCanonicalName());
 	}
 
 	@Test
-	public void testGetEnumConstant_fullyQualifiedConstantNameVariant_validConstant() throws ClassNotFoundException {
+	public void testGetEnumConstant_fullyQualifiedConstantNameVariant_validConstant() {
 		EnumUtil.getEnumConstant(Vehicle.class.getCanonicalName() + ".BUS");
 	}
 }
