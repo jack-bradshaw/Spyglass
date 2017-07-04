faea6ef692f4f81527bebcff1e2e43c36334e668
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-07-04 15:32:59 +0930

    Renamed class

--- consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithCompanionThrowsTargetException_SpyglassCompanion.java
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithCompanionThrowsTargetException_SpyglassCompanion.java
@@ -3,7 +3,7 @@ package com.matthewtamlin.spyglass.consumer_tests;
 import android.content.Context;
 import android.content.res.TypedArray;
 
-public class ViewWithExceptionThrowingCompanion_SpyglassCompanion {
+public class ViewWithCompanionThrowsTargetException_SpyglassCompanion {
 	public static void activateCallers(
 			final ViewWithCompanionThrowsTargetException target,
 			final Context context,
