68ee3baa41854e56c3c8eed5e76ee6fbe6c71239
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-07-04 11:49:01 +0930

    Renamed package
    - Renamed package
    - Updated references

--- integration tests/src/main/java/com/matthewtamlin/spyglass/integration_tests/annotation_combination_tests/enum_ordinal_handler_combinations/WithDefaultToInteger.java
+++ integration tests/src/main/java/com/matthewtamlin/spyglass/integration_tests/annotation_combination_tests/enum_ordinal_handler_combinations/WithDefaultToInteger.java
@@ -1,40 +1,41 @@
-package com.matthewtamlin.spyglass.integration_tests.annotation_combination_tests.enum_ordinal_handler;
+package com.matthewtamlin.spyglass.integration_tests.annotation_combination_tests.enum_ordinal_handler_combinations;
 
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 
-import com.matthewtamlin.spyglass.common.annotations.default_annotations.DefaultToNull;
-import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.EnumConstantHandler;
+import com.matthewtamlin.spyglass.common.annotations.default_annotations.DefaultToInteger;
 import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.EnumOrdinalHandler;
 import com.matthewtamlin.spyglass.consumer.Spyglass;
 import com.matthewtamlin.spyglass.integration_tests.R;
-import com.matthewtamlin.spyglass.integration_tests.enum_constant_handler.EnumConstantHandlerTestTargetBase;
-import com.matthewtamlin.spyglass.integration_tests.enum_constant_handler.EnumForTesting.Fruit;
 import com.matthewtamlin.spyglass.integration_tests.framework.ReceivedValue;
 
-import static com.matthewtamlin.spyglass.integration_tests.R.attr.fruit;
+public class WithDefaultToInteger extends EnumOrdinalHandlerTestTargetBase {
+	public static final int DEFAULT_VALUE = 9849;
 
-public class WithDefaultToNull extends EnumOrdinalHandlerTestTargetBase {
-	public WithDefaultToNull(final Context context) {
+	public WithDefaultToInteger(final Context context) {
 		super(context);
 		init(null, 0, 0);
 	}
 
-	public WithDefaultToNull(final Context context, final AttributeSet attrs) {
+	public WithDefaultToInteger(final Context context, final AttributeSet attrs) {
 		super(context, attrs);
 		init(attrs, 0, 0);
 	}
 
-	public WithDefaultToNull(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+	public WithDefaultToInteger(
+			final Context context,
+			final AttributeSet attrs,
+			final int defStyleAttr) {
+
 		super(context, attrs, defStyleAttr);
 		init(attrs, defStyleAttr, 0);
 	}
 
 	@TargetApi(21)
 	@RequiresApi(21)
-	public WithDefaultToNull(
+	public WithDefaultToInteger(
 			final Context context,
 			final AttributeSet attrs,
 			final int defStyleAttr,
@@ -45,15 +46,15 @@ public class WithDefaultToNull extends EnumOrdinalHandlerTestTargetBase {
 	}
 
 	@EnumOrdinalHandler(attributeId = R.styleable.EnumOrdinalHandlerTestTargetBase_language)
-	@DefaultToNull
-	public void handlerMethod(final Integer i) {
+	@DefaultToInteger(DEFAULT_VALUE)
+	public void handlerMethod(final int i) {
 		setReceivedValue(ReceivedValue.of(i));
 	}
 
 	private void init(final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
 		Spyglass.builder()
 				.withTarget(this)
-				.withAnnotationSource(WithDefaultToNull.class)
+				.withAnnotationSource(WithDefaultToInteger.class)
 				.withStyleableResource(R.styleable.EnumOrdinalHandlerTestTargetBase)
 				.withContext(getContext())
 				.withAttributeSet(attrs)
--- integration tests/src/main/java/com/matthewtamlin/spyglass/integration_tests/annotation_combination_tests/enum_ordinal_handler_combinations/WithoutDefault.java
+++ integration tests/src/main/java/com/matthewtamlin/spyglass/integration_tests/annotation_combination_tests/enum_ordinal_handler_combinations/WithoutDefault.java
@@ -1,30 +1,27 @@
-package com.matthewtamlin.spyglass.integration_tests.annotation_combination_tests.enum_ordinal_handler;
+package com.matthewtamlin.spyglass.integration_tests.annotation_combination_tests.enum_ordinal_handler_combinations;
 
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 
-import com.matthewtamlin.spyglass.common.annotations.default_annotations.DefaultToInteger;
 import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.EnumOrdinalHandler;
 import com.matthewtamlin.spyglass.consumer.Spyglass;
 import com.matthewtamlin.spyglass.integration_tests.R;
 import com.matthewtamlin.spyglass.integration_tests.framework.ReceivedValue;
 
-public class WithDefaultToInteger extends EnumOrdinalHandlerTestTargetBase {
-	public static final int DEFAULT_VALUE = 9849;
-
-	public WithDefaultToInteger(final Context context) {
+public class WithoutDefault extends EnumOrdinalHandlerTestTargetBase {
+	public WithoutDefault(final Context context) {
 		super(context);
 		init(null, 0, 0);
 	}
 
-	public WithDefaultToInteger(final Context context, final AttributeSet attrs) {
+	public WithoutDefault(final Context context, final AttributeSet attrs) {
 		super(context, attrs);
 		init(attrs, 0, 0);
 	}
 
-	public WithDefaultToInteger(
+	public WithoutDefault(
 			final Context context,
 			final AttributeSet attrs,
 			final int defStyleAttr) {
@@ -35,7 +32,7 @@ public class WithDefaultToInteger extends EnumOrdinalHandlerTestTargetBase {
 
 	@TargetApi(21)
 	@RequiresApi(21)
-	public WithDefaultToInteger(
+	public WithoutDefault(
 			final Context context,
 			final AttributeSet attrs,
 			final int defStyleAttr,
@@ -46,7 +43,6 @@ public class WithDefaultToInteger extends EnumOrdinalHandlerTestTargetBase {
 	}
 
 	@EnumOrdinalHandler(attributeId = R.styleable.EnumOrdinalHandlerTestTargetBase_language)
-	@DefaultToInteger(DEFAULT_VALUE)
 	public void handlerMethod(final int i) {
 		setReceivedValue(ReceivedValue.of(i));
 	}
@@ -54,7 +50,7 @@ public class WithDefaultToInteger extends EnumOrdinalHandlerTestTargetBase {
 	private void init(final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
 		Spyglass.builder()
 				.withTarget(this)
-				.withAnnotationSource(WithDefaultToInteger.class)
+				.withAnnotationSource(WithoutDefault.class)
 				.withStyleableResource(R.styleable.EnumOrdinalHandlerTestTargetBase)
 				.withContext(getContext())
 				.withAttributeSet(attrs)
