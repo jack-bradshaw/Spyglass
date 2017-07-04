2d53ebc30b04ac8aa3e9c8897e1cdb3ad96033d3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-07-04 15:33:48 +0930

    Created multiple classes

---
+++ consumer tests/src/main/java/com/matthewtamlin/spyglass/consumer_tests/ViewWithCompanionThrowsSpyglassRuntimeException_SpyglassCompanion.java
@@ -0,0 +1,16 @@
+package com.matthewtamlin.spyglass.consumer_tests;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+
+import com.matthewtamlin.spyglass.common.exception.SpyglassRuntimeException;
+
+public class ViewWithCompanionThrowsSpyglassRuntimeException_SpyglassCompanion {
+	public static void activateCallers(
+			final ViewWithCompanionThrowsTargetException target,
+			final Context context,
+			final TypedArray attrs) {
+
+		throw new SpyglassRuntimeException();
+	}
+}
