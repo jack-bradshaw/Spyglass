dc3eb413534a8469f51318ce6cc5dfe12acc8f1a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-07-04 11:45:08 +0930

    Renamed package
    - Renamed package
    - Updated references

--- integration tests/src/main/java/com/matthewtamlin/spyglass/integration_tests/annotation_combination_tests/boolean_handler_combinations/WithoutDefault.java
+++ integration tests/src/main/java/com/matthewtamlin/spyglass/integration_tests/annotation_combination_tests/boolean_handler_combinations/WithoutDefault.java
@@ -1,35 +1,37 @@
-package com.matthewtamlin.spyglass.integration_tests.annotation_combination_tests.boolean_handler;
+package com.matthewtamlin.spyglass.integration_tests.annotation_combination_tests.boolean_handler_combinations;
 
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 
-import com.matthewtamlin.spyglass.common.annotations.default_annotations.DefaultToBooleanResource;
 import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.BooleanHandler;
 import com.matthewtamlin.spyglass.consumer.Spyglass;
 import com.matthewtamlin.spyglass.integration_tests.R;
 import com.matthewtamlin.spyglass.integration_tests.framework.ReceivedValue;
 
-public class WithDefaultToBooleanResource extends BooleanHandlerTestTargetBase {
-	public WithDefaultToBooleanResource(final Context context) {
+public class WithoutDefault extends BooleanHandlerTestTargetBase {
+	public WithoutDefault(final Context context) {
 		super(context);
 		init(null, 0, 0);
 	}
 
-	public WithDefaultToBooleanResource(final Context context, final AttributeSet attrs) {
+	public WithoutDefault(final Context context, final AttributeSet attrs) {
 		super(context, attrs);
 		init(attrs, 0, 0);
 	}
 
-	public WithDefaultToBooleanResource(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+	public WithoutDefault(
+			final Context context,
+			final AttributeSet attrs,
+			final int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		init(attrs, defStyleAttr, 0);
 	}
 
-	@TargetApi(21)
 	@RequiresApi(21)
-	public WithDefaultToBooleanResource(
+	@TargetApi(21)
+	public WithoutDefault(
 			final Context context,
 			final AttributeSet attrs,
 			final int defStyleAttr,
@@ -40,7 +42,6 @@ public class WithDefaultToBooleanResource extends BooleanHandlerTestTargetBase {
 	}
 
 	@BooleanHandler(attributeId = R.styleable.BooleanHandlerTestTargetBase_booleanHandlerAttr)
-	@DefaultToBooleanResource(resId = R.bool.BooleanForTesting)
 	public void handlerMethod(final boolean b) {
 		setReceivedValue(ReceivedValue.of(b));
 	}
@@ -48,7 +49,7 @@ public class WithDefaultToBooleanResource extends BooleanHandlerTestTargetBase {
 	private void init(final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
 		Spyglass.builder()
 				.withTarget(this)
-				.withAnnotationSource(WithDefaultToBooleanResource.class)
+				.withAnnotationSource(WithoutDefault.class)
 				.withStyleableResource(R.styleable.BooleanHandlerTestTargetBase)
 				.withContext(getContext())
 				.withAttributeSet(attrs)
