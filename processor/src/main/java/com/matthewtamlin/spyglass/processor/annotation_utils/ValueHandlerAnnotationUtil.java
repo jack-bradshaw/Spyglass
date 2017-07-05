ea3b7474e0e9bd22a62c7c80cdbc4ac6fa86e95f
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-07-05 11:48:26 +0930

    Added method getValueHandlerAnnotationClasses
    - Added method
    - Removed external class dependence

--- processor/src/main/java/com/matthewtamlin/spyglass/processor/annotation_utils/ValueHandlerAnnotationUtil.java
+++ processor/src/main/java/com/matthewtamlin/spyglass/processor/annotation_utils/ValueHandlerAnnotationUtil.java
@@ -1,22 +1,45 @@
 package com.matthewtamlin.spyglass.processor.annotation_utils;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseBoolean;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseByte;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseChar;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseDouble;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseFloat;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseInt;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseLong;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseNull;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseShort;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseString;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.BooleanHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.ColorHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.ColorStateListHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.DimensionHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.DrawableHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.EnumConstantHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.EnumOrdinalHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.FloatHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.FractionHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.IntegerHandler;
+import com.matthewtamlin.spyglass.common.annotations.value_handler_annotations.StringHandler;
 import com.matthewtamlin.spyglass.processor.mirror_utils.AnnotationMirrorUtil;
 
 import java.lang.annotation.Annotation;
+import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 import static com.matthewtamlin.spyglass.processor.core.AnnotationRegistry.VALUE_HANDLER_ANNOTATIONS;
+import static com.matthewtamlin.spyglass.processor.util.SetUtil.unmodifiableSetOf;
 
 @Tested(testMethod = "automated")
 public class ValueHandlerAnnotationUtil {
 	public static AnnotationMirror getValueHandlerAnnotationMirror(final ExecutableElement element) {
 		checkNotNull(element, "Argument \'element\' cannot be null.");
 
-		for (final Class<? extends Annotation> annotationClass : VALUE_HANDLER_ANNOTATIONS) {
+		for (final Class<? extends Annotation> annotationClass : getValueHandlerAnnotationClasses()) {
 			final AnnotationMirror mirror = AnnotationMirrorUtil.getAnnotationMirror(element, annotationClass);
 
 			if (mirror != null) {
@@ -29,6 +52,21 @@ public class ValueHandlerAnnotationUtil {
 
 	public static boolean hasValueHandlerAnnotation(final ExecutableElement element) {
 		return getValueHandlerAnnotationMirror(element) != null;
+	}
+
+	public static Set<Class<? extends Annotation>> getValueHandlerAnnotationClasses() {
+		return unmodifiableSetOf(
+				BooleanHandler.class,
+				ColorHandler.class,
+				ColorStateListHandler.class,
+				DimensionHandler.class,
+				DrawableHandler.class,
+				EnumConstantHandler.class,
+				EnumOrdinalHandler.class,
+				FloatHandler.class,
+				FractionHandler.class,
+				IntegerHandler.class,
+				StringHandler.class);
 	}
 
 	private ValueHandlerAnnotationUtil() {
