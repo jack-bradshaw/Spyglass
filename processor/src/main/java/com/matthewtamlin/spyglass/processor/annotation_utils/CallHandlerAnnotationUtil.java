be9b5d2b3b825b05e6b90b199b75b6391086786a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-07-05 11:42:22 +0930

    Added method getCallHandlerAnnotationClasses()
    - Added method
    - Removed external class dependence

--- processor/src/main/java/com/matthewtamlin/spyglass/processor/annotation_utils/CallHandlerAnnotationUtil.java
+++ processor/src/main/java/com/matthewtamlin/spyglass/processor/annotation_utils/CallHandlerAnnotationUtil.java
@@ -1,22 +1,25 @@
 package com.matthewtamlin.spyglass.processor.annotation_utils;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
+import com.matthewtamlin.spyglass.common.annotations.call_handler_annotations.SpecificEnumHandler;
+import com.matthewtamlin.spyglass.common.annotations.call_handler_annotations.SpecificFlagHandler;
 import com.matthewtamlin.spyglass.processor.mirror_utils.AnnotationMirrorUtil;
 
 import java.lang.annotation.Annotation;
+import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
-import static com.matthewtamlin.spyglass.processor.core.AnnotationRegistry.CALL_HANDLER_ANNOTATIONS;
+import static com.matthewtamlin.spyglass.processor.util.SetUtil.unmodifiableSetOf;
 
 @Tested(testMethod = "automated")
 public class CallHandlerAnnotationUtil {
 	public static AnnotationMirror getCallHandlerAnnotationMirror(final ExecutableElement element) {
 		checkNotNull(element, "Argument \'element\' cannot be null.");
 
-		for (final Class<? extends Annotation> annotationClass : CALL_HANDLER_ANNOTATIONS) {
+		for (final Class<? extends Annotation> annotationClass : getCallHandlerAnnotationClasses()) {
 			final AnnotationMirror mirror = AnnotationMirrorUtil.getAnnotationMirror(element, annotationClass);
 
 			if (mirror != null) {
@@ -29,6 +32,12 @@ public class CallHandlerAnnotationUtil {
 
 	public static boolean hasCallHandlerAnnotation(final ExecutableElement element) {
 		return getCallHandlerAnnotationMirror(element) != null;
+	}
+
+	public static Set<Class<? extends Annotation>> getCallHandlerAnnotationClasses() {
+		return unmodifiableSetOf(
+				SpecificEnumHandler.class,
+				SpecificFlagHandler.class);
 	}
 
 	private CallHandlerAnnotationUtil() {
