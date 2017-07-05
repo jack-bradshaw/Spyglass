9ba1cf75b43be21cd5cdde985c01e9b1d7646b23
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-07-05 11:46:04 +0930

    Added method getUseAnnotationClasses
    - Added method
    - Removed external class dependence

--- processor/src/main/java/com/matthewtamlin/spyglass/processor/annotation_utils/UseAnnotationUtil.java
+++ processor/src/main/java/com/matthewtamlin/spyglass/processor/annotation_utils/UseAnnotationUtil.java
@@ -1,22 +1,33 @@
 package com.matthewtamlin.spyglass.processor.annotation_utils;
 
 import com.matthewtamlin.java_utilities.testing.Tested;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseBoolean;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseByte;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseChar;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseDouble;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseFloat;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseInt;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseLong;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseNull;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseShort;
+import com.matthewtamlin.spyglass.common.annotations.use_annotations.UseString;
 import com.matthewtamlin.spyglass.processor.mirror_utils.AnnotationMirrorUtil;
 
 import java.lang.annotation.Annotation;
+import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.VariableElement;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
-import static com.matthewtamlin.spyglass.processor.core.AnnotationRegistry.USE_ANNOTATIONS;
+import static com.matthewtamlin.spyglass.processor.util.SetUtil.unmodifiableSetOf;
 
 @Tested(testMethod = "automated")
 public class UseAnnotationUtil {
 	public static AnnotationMirror getUseAnnotationMirror(final VariableElement element) {
 		checkNotNull(element, "Argument \'element\' cannot be null.");
 
-		for (final Class<? extends Annotation> annotationClass : USE_ANNOTATIONS) {
+		for (final Class<? extends Annotation> annotationClass : getUseAnnotationClasses()) {
 			final AnnotationMirror mirror = AnnotationMirrorUtil.getAnnotationMirror(element, annotationClass);
 
 			if (mirror != null) {
@@ -29,6 +40,20 @@ public class UseAnnotationUtil {
 
 	public static boolean hasUseAnnotation(final VariableElement element) {
 		return getUseAnnotationMirror(element) != null;
+	}
+
+	public static Set<Class<? extends Annotation>> getUseAnnotationClasses() {
+		return unmodifiableSetOf(
+				UseBoolean.class,
+				UseByte.class,
+				UseChar.class,
+				UseDouble.class,
+				UseFloat.class,
+				UseInt.class,
+				UseLong.class,
+				UseNull.class,
+				UseShort.class,
+				UseString.class);
 	}
 
 	private UseAnnotationUtil() {
