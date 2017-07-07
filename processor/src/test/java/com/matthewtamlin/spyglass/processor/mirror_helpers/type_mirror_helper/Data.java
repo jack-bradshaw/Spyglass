5fb733ea5fa3c9f3fd10f414f6b211b539b3e675
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-07-07 15:37:07 +0930

    Created tests for TypeMirrorHelper class

---
+++ processor/src/test/java/com/matthewtamlin/spyglass/processor/mirror_helpers/type_mirror_helper/Data.java
@@ -0,0 +1,84 @@
+package com.matthewtamlin.spyglass.processor.mirror_helpers.type_mirror_helper;
+
+import com.matthewtamlin.avatar.element_supplier.ElementId;
+
+public class Data {
+	@ElementId("with primitive boolean")
+	public void method(boolean b) {}
+
+	@ElementId("with primitive byte")
+	public void method(byte b) {}
+
+	@ElementId("with primitive char")
+	public void method(char c) {}
+
+	@ElementId("with primitive double")
+	public void method(double d) {}
+
+	@ElementId("with primitive float")
+	public void method(float f) {}
+
+	@ElementId("with primitive int")
+	public void method(int i) {}
+
+	@ElementId("with primitive long")
+	public void method(long l) {}
+
+	@ElementId("with primitive short")
+	public void method(short s) {}
+
+	@ElementId("with boxed boolean")
+	public void method(Boolean b) {}
+
+	@ElementId("with boxed byte")
+	public void method(Byte b) {}
+
+	@ElementId("with boxed char")
+	public void method(Character c) {}
+
+	@ElementId("with boxed double")
+	public void method(Double d) {}
+
+	@ElementId("with boxed float")
+	public void method(Float f) {}
+
+	@ElementId("with boxed int")
+	public void method(Integer i) {}
+
+	@ElementId("with boxed long")
+	public void method(Long l) {}
+
+	@ElementId("with boxed short")
+	public void method(Short s) {}
+
+	@ElementId("with object")
+	public void method(Object o) {}
+
+	@ElementId("with number")
+	public void method(Number n) {}
+
+	@ElementId("with custom number implementation")
+	public void method(NumberImplementation n) {}
+
+	private class NumberImplementation extends Number {
+		@Override
+		public int intValue() {
+			return 0;
+		}
+
+		@Override
+		public long longValue() {
+			return 0;
+		}
+
+		@Override
+		public float floatValue() {
+			return 0;
+		}
+
+		@Override
+		public double doubleValue() {
+			return 0;
+		}
+	}
+}
