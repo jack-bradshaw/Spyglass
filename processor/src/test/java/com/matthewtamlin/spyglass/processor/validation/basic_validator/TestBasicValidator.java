1bb35944ec2c32b78023442ac42f0e4b3e0dce16
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-09-05 08:13:42 +0930

    test(validation): updated TestBasicValidator to use Result instead of ValidationException

--- processor/src/test/java/com/matthewtamlin/spyglass/processor/validation/basic_validator/TestBasicValidator.java
+++ processor/src/test/java/com/matthewtamlin/spyglass/processor/validation/basic_validator/TestBasicValidator.java
@@ -1,9 +1,8 @@
 package com.matthewtamlin.spyglass.processor.validation.basic_validator;
 
 import com.matthewtamlin.avatar.rules.AvatarRule;
-import com.matthewtamlin.spyglass.processor.core.CoreHelpers;
 import com.matthewtamlin.spyglass.processor.validation.BasicValidator;
-import com.matthewtamlin.spyglass.processor.validation.ValidationException;
+import com.matthewtamlin.spyglass.processor.validation.Result;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -17,12 +16,17 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.ExecutableElement;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
 @RunWith(JUnit4.class)
 public class TestBasicValidator {
 	@Rule
 	public final AvatarRule avatarRule = AvatarRule
 			.builder()
-			.withSourcesAt("processor/src/test/java/com/matthewtamlin/spyglass/processor/validation/Data.java")
+			.withSourcesAt(
+					"processor/src/test/java/com/matthewtamlin/spyglass/processor/validation/basic_validator/Data.java")
 			.build();
 
 	private Set<Element> elements;
@@ -33,43 +37,21 @@ public class TestBasicValidator {
 	public void setup() {
 		elements = avatarRule.getElementsWithAnnotation(Target.class);
 
-		validator = new BasicValidator(new CoreHelpers(avatarRule.getElementUtils(), avatarRule.getTypeUtils()));
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testConstructor_nullTypeMirrorHelper() {
-		new BasicValidator(null);
+		validator = new BasicValidator();
 	}
 
 	@Test
 	public void testValidateElement_usingDataFileElements() {
 		for (final Element element : elements) {
-			final Target targetAnnotation = element.getAnnotation(Target.class);
-			final boolean shouldPassValidation = targetAnnotation.isValid();
-
 			if (element.getKind() != ElementKind.METHOD) {
 				throw new RuntimeException("All test elements must be executable elements (e.g. methods).");
 			}
 
-			try {
-				validator.validate((ExecutableElement) element);
+			final Target targetAnnotation = element.getAnnotation(Target.class);
+			final boolean shouldPassValidation = targetAnnotation.isValid();
+			final Result validationResult = validator.validate((ExecutableElement) element);
 
-				if (!shouldPassValidation) {
-					throw new RuntimeException(
-							String.format(
-									"Element %1$s should have failed validation but passed.",
-									element.getSimpleName()));
-				}
-			} catch (final ValidationException exception) {
-				if (shouldPassValidation) {
-					throw new RuntimeException(
-							String.format(
-									"Element %1$s should have passed validation but failed with error message: %2$s",
-									element.getSimpleName(),
-									exception.getMessage())
-					);
-				}
-			}
+			assertThat(validationResult.isSuccessful(), is(shouldPassValidation));
 		}
 	}
 }
