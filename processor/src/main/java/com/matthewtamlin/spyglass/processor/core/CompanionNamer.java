7b0dcb475a477f8fb737da85b29a0e0aedb6a7f3
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-09-09 16:39:26 +0930

    fix(processor): fixed errors in recursion

--- processor/src/main/java/com/matthewtamlin/spyglass/processor/core/CompanionNamer.java
+++ processor/src/main/java/com/matthewtamlin/spyglass/processor/core/CompanionNamer.java
@@ -1,21 +1,29 @@
 package com.matthewtamlin.spyglass.processor.core;
 
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.NestingKind;
 import javax.lang.model.element.TypeElement;
 
 public class CompanionNamer {
 	public static String getCompanionNameFor(final TypeElement targetClass) {
-		return getPrefix(targetClass) + "_SpyglassCompanion";
+		return getParentChain(targetClass) + "_SpyglassCompanion";
 	}
 
-	private static String getPrefix(final TypeElement targetClass) {
+	private static String getParentChain(final TypeElement targetClass) {
+		// if input is top level class return it
+		// otherwise return the parent chain plus it
+
 		if (targetClass.getNestingKind() == NestingKind.TOP_LEVEL) {
 			return targetClass.getSimpleName().toString();
-
 		} else {
-			final TypeElement parent = (TypeElement) targetClass.getEnclosedElements();
+			final Element parent = targetClass.getEnclosingElement();
 
-			return getPrefix(parent) + "_" + targetClass.getSimpleName();
+			if (parent.getKind() != ElementKind.CLASS) {
+				throw new RuntimeException("Cannot create parent chain. Non-class parent found.");
+			}
+
+			return (getParentChain((TypeElement) parent)) + "_" + targetClass.getSimpleName().toString();
 		}
 	}
 }
