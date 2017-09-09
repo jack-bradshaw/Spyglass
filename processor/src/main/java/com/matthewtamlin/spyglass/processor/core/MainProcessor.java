a4fbc4189de92f056b60ca1ffe3c60e42adc2aa8
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-09-09 16:57:38 +0930

    feat(processor): added more information to error loggin

--- processor/src/main/java/com/matthewtamlin/spyglass/processor/core/MainProcessor.java
+++ processor/src/main/java/com/matthewtamlin/spyglass/processor/core/MainProcessor.java
@@ -119,9 +119,12 @@ public class MainProcessor extends AbstractProcessor {
 		} catch (final Throwable t) {
 			messager.printMessage(
 					ERROR,
-					"An unknown error occurred while processing Spyglass annotations. Please update to the latest " +
-							"version of Spyglass, or report the issue if a newer version does not exist. Stacktrace: " +
-							Arrays.toString(t.getStackTrace()));
+					String.format(
+							"An unknown error occurred while processing Spyglass annotations. Please update to the " +
+									"latest version of Spyglass, or report the issue if a newer version does not " +
+									"exist. Exception message: \'%1$s\'. Stacktrace: \'%2$s\'.",
+							t.getMessage(),
+							Arrays.toString(t.getStackTrace())));
 		}
 
 		return false;
