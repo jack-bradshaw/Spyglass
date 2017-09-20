79eb769d0024779721f472df26458c4cfc612865
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-09-20 20:36:20 +0930

    feat(common): updated Javadoc for value handler annotations

--- common/src/main/java/com/matthewtamlin/spyglass/common/annotations/value_handler_annotations/IntegerHandler.java
+++ common/src/main/java/com/matthewtamlin/spyglass/common/annotations/value_handler_annotations/IntegerHandler.java
@@ -6,14 +6,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Declares a method capable of handling an integer attribute. If the Spyglass framework finds
- * an integer value mapped to the specified attribute, it will invoke the method and pass in the
- * value. This annotation should only be applied to methods which satisfy all of the following
- * criteria:
+ * Declares a method capable of handling an integer attribute. If the Spyglass framework finds an integer value
+ * mapped to the attribute ID, it will invoke the method and pass in the value. This annotation should only be
+ * applied to methods which satisfy all of the following criteria:
  * <ul>
  * <li>The method has no other handler annotations.</li>
- * <li>The method has at least one integer parameter.</li>
- * <li>Except for one integer parameter, every parameter has a use annotation.</li>
+ * <li>The method has at least one {@code Number} parameter.</li>
+ * <li>Except for one {@code Number} parameter, every parameter has a use annotation.</li>
  * </ul>
  */
 @Retention(RetentionPolicy.SOURCE)
