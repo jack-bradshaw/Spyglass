af81d0e5b34382aa9628613e81a96c823a5d5e90
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-09-20 20:02:55 +0930

    feat(common): updated Javadoc for use annotations

--- common/src/main/java/com/matthewtamlin/spyglass/common/annotations/use_annotations/UseByte.java
+++ common/src/main/java/com/matthewtamlin/spyglass/common/annotations/use_annotations/UseByte.java
@@ -6,13 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Defines the value to supply to the annotated parameter when the Spyglass framework invokes the
- * method. This annotation should only be applied to parameters which satisfy all of the following
- * criteria:
+ * Defines the value to supply to the annotated parameter when the Spyglass framework invokes the method. This
+ * annotation should only be applied to parameters which satisfy all of the following criteria:
  * <ul>
  * <li>The parameter is part of a method which has a handler annotation.</li>
  * <li>The parameter has no other use annotations.</li>
- * <li>The parameter accepts a byte value.</li>
+ * <li>The parameter accepts a {@code Number} value.</li>
  * </ul>
  */
 @Retention(RetentionPolicy.SOURCE)
