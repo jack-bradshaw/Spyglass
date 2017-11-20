6a3f258e31385e419e216af69621defbd45c1653
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-11-20 20:07:02 +1030

    test(integration): created integration tests for the SpecificBooleanHandler annotation

---
+++ integration tests/src/main/java/com/matthewtamlin/spyglass/integration_tests/annotation_combination_tests/specific_boolean_handler_combinations/SpecificBooleanHandlerTestTarget.java
@@ -0,0 +1,67 @@
+package com.matthewtamlin.spyglass.integration_tests.annotation_combination_tests.specific_boolean_handler_combinations;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.matthewtamlin.spyglass.integration_tests.R;
+import com.matthewtamlin.spyglass.integration_tests.annotation_combination_tests.specific_flag_handler_combinations.SpecificFlagHandlerTestTarget_SpyglassCompanion;
+import com.matthewtamlin.spyglass.markers.annotations.call_handler_annotations.SpecificBooleanHandler;
+import com.matthewtamlin.spyglass.markers.annotations.call_handler_annotations.SpecificFlagHandler;
+
+public class SpecificBooleanHandlerTestTarget extends View {
+	private boolean handlerCalled = false;
+
+	public SpecificBooleanHandlerTestTarget(final Context context) {
+		super(context);
+		init(null, 0, 0);
+	}
+
+	public SpecificBooleanHandlerTestTarget(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+		init(attrs, 0, 0);
+	}
+
+	public SpecificBooleanHandlerTestTarget(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init(attrs, defStyleAttr, 0);
+	}
+
+	@RequiresApi(21)
+	@TargetApi(21)
+	public SpecificBooleanHandlerTestTarget(
+			final Context context,
+			final AttributeSet attrs,
+			final int defStyleAttr,
+			final int defStyleRes) {
+
+		super(context, attrs, defStyleAttr, defStyleRes);
+		init(attrs, defStyleAttr, defStyleRes);
+	}
+
+	@SpecificBooleanHandler(
+			attributeId = R.styleable.SpecificFlagHandlerTestTarget_specificFlagHandlerAttr,
+			handledBoolean = true)
+	public void handlerMethod() {
+		handlerCalled = true;
+	}
+
+	public boolean wasHandlerCalled() {
+		return handlerCalled;
+	}
+
+	private void init(final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
+		SpecificBooleanHandlerTestTarget_SpyglassCompanion
+				.builder()
+				.withTarget(this)
+				.withContext(getContext())
+				.withStyleableResource(R.styleable.SpecificBooleanHandlerTestTarget)
+				.withAttributeSet(attrs)
+				.withDefaultStyleAttribute(defStyleAttr)
+				.withDefaultStyleResource(defStyleRes)
+				.build()
+				.passDataToMethods();
+	}
+}
